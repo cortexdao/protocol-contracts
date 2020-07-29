4ebe1a11de7bc0114628c39ac936d75660f95f79
Chan-Ho Suh <chanhosuh@gmail.com>
2020-07-28 23:52:51 -0400

    start tests; make stubs

--- contracts/APYLiquidityPool.sol
+++ contracts/APYLiquidityPool.sol
@@ -35,9 +35,9 @@ contract APYLiquidityPool is Ownable, ReentrancyGuard {
     function mint() external payable nonReentrant {
         require(msg.value > 0, "Pool/insufficient-value");
 
-        // uint256 totalValue = address(this).balance.sub(msg.value);
-        // uint256 mintAmount = _calculateMintAmount(msg.value, totalValue);
-        // apt.mint(msg.sender, mintAmount);
+        uint256 totalValue = address(this).balance.sub(msg.value);
+        uint256 mintAmount = _calculateMintAmount(msg.value, totalValue);
+        // _mint(msg.sender, mintAmount);
 
         // emit MintAPT(msg.sender, mintAmount, msg.value);
     }
--- test/APYLiquidityPool.js
+++ test/APYLiquidityPool.js
@@ -6,6 +6,7 @@ const {
   deployContract,
 } = require("ethereum-waffle");
 const APYLiquidityPool = require("../artifacts/APYLiquidityPool.json");
+const { it } = require("ethers/wordlists");
 
 describe("APYLiquidityPool", () => {
   const provider = waffle.provider;
@@ -30,5 +31,17 @@ describe("APYLiquidityPool", () => {
 
   it("mint reverts if 0 ETH sent", async () => {
     await expect(apiLiquidityPool.mint({ value: "0" })).to.be.reverted;
+  });
+
+  it("mint amount to supply equals ETH deposit to total ETH value", async () => {
+    // start with non-zero token supply and non-zero ETH value
+  });
+
+  it("mint amount is constant multiple of deposit if total ETH value is zero", async () => {
+    // start with zero total ETH value and non-zero total supply
+  });
+
+  it("mint amount is constant multiple of deposit if total supply is zero ", async () => {
+    // start with non-zero total ETH value and zero total supply
   });
 });
