80d5f369d5167c061ad3c8f18a90f4f98f7a6369
Chan-Ho Suh <chanhosuh@gmail.com>
2020-08-12 09:16:21 -0400

    Allow Infura as node in Ganache script
    Cloudflare will be fallback only if needed env var is missing.
    Infura mainnet will be used if INFURA_KEY_MAINNET is found
    in the .env file.

--- ganache/ganache_start.sh
+++ ganache/ganache_start.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
+source .env
+
+if [ -n "${INFURA_KEY_MAINNET}" ]; then
+    NODE_URL="https://mainnet.infura.io/v3/${INFURA_KEY_MAINNET}";
+else
+    # an ok fallback, but somewhat flakey behavior
+    NODE_URL="https://cloudflare-eth.com";
+fi
+
+echo "Using node: ${NODE_URL}"
+echo ""
 
 mkdir -p .ganache/logs
 
 ganache-cli --networkId=10312008 \
-    --deterministic --fork="https://cloudflare-eth.com" \
+    --deterministic --fork="${NODE_URL}" \
     2>&1 | tee .ganache/logs/$(date +%Y-%m-%d-%H:%M:%S).log
 
