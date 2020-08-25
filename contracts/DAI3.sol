2b0110b0368bcd6eb03f3faa2a3d08b4e3f9671e
Chan-Ho Suh <chanhosuh@gmail.com>
2020-08-25 09:57:05 -0400

    Lint contract

--- contracts/DAI3.sol
+++ contracts/DAI3.sol
@@ -17,11 +17,11 @@ interface CErc20 {
 
     function borrow(uint256) external returns (uint256);
 
-    function borrowRatePerBlock() external view returns (uint256);
-
     function borrowBalanceCurrent(address) external returns (uint256);
 
     function repayBorrow(uint256) external returns (uint256);
+
+    function borrowRatePerBlock() external view returns (uint256);
 }
 
 interface CEth {
@@ -41,6 +41,8 @@ interface Comptroller {
         external
         returns (uint256[] memory);
 
+    function claimComp(address holder) external;
+
     function getAccountLiquidity(address)
         external
         view
@@ -49,8 +51,6 @@ interface Comptroller {
             uint256,
             uint256
         );
-
-    function claimComp(address holder) external;
 }
 
 interface PriceOracle {
@@ -63,26 +63,29 @@ contract DAI3Strategy {
 
     // Mainnet Dai
     // https://etherscan.io/address/0x6b175474e89094c44da98b954eedeac495271d0f#readContract
-    address daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    Erc20 dai = Erc20(daiAddress);
+    address private _daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    Erc20 private _dai = Erc20(_daiAddress);
 
     // Mainnet cDai
     // https://etherscan.io/address/0x5d3a536e4d6dbd6114cc1ead35777bab948e3643#readProxyContract
-    address cDaiAddress = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
-    CErc20 cDai = CErc20(cDaiAddress);
+    address private _cDaiAddress = 0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643;
+    CErc20 private _cDai = CErc20(_cDaiAddress);
 
     // Mainnet Comptroller
     // https://etherscan.io/address/0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b#readProxyContract
-    address comptrollerAddress = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
-    Comptroller comptroller = Comptroller(comptrollerAddress);
+    address
+        private _comptrollerAddress = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
+    Comptroller private _comptroller = Comptroller(_comptrollerAddress);
 
     // COMP ERC-20 token
     // https://etherscan.io/token/0xc00e94cb662c3520282e6f5717214004a7f26888
-    Erc20 compToken = Erc20(0xc00e94Cb662C3520282E6f5717214004A7f26888);
+    Erc20 private _compToken = Erc20(
+        0xc00e94Cb662C3520282E6f5717214004A7f26888
+    );
 
-    uint256 constant DAYS_IN_PERIOD = 7;
-    uint256 constant ETH_MANTISSA = 10**18;
-    uint256 constant BLOCKS_PER_DAY = 4 * 60 * 24;
+    uint256 private constant _DAYS_IN_PERIOD = 7;
+    uint256 private constant _ETH_MANTISSA = 10**18;
+    uint256 private constant _BLOCKS_PER_DAY = 4 * 60 * 24;
 
     receive() external payable {
         revert("Should not be receiving ETH.");
@@ -90,8 +93,8 @@ contract DAI3Strategy {
 
     constructor() public {
         address[] memory cTokens = new address[](1);
-        cTokens[0] = cDaiAddress;
-        uint256[] memory errors = comptroller.enterMarkets(cTokens);
+        cTokens[0] = _cDaiAddress;
+        uint256[] memory errors = _comptroller.enterMarkets(cTokens);
         require(errors[0] == 0, "Comptroller.enterMarkets failed.");
     }
 
@@ -100,12 +103,12 @@ contract DAI3Strategy {
         external
         returns (uint256)
     {
-        dai.approve(cDaiAddress, 0);
-        dai.approve(cDaiAddress, depositAmount);
-        cDai.mint(depositAmount);
+        _dai.approve(_cDaiAddress, 0);
+        _dai.approve(_cDaiAddress, depositAmount);
+        _cDai.mint(depositAmount);
 
-        cDai.borrow(borrowAmount);
-        uint256 borrows = cDai.borrowBalanceCurrent(address(this));
+        _cDai.borrow(borrowAmount);
+        uint256 borrows = _cDai.borrowBalanceCurrent(address(this));
         return borrows;
     }
 
@@ -120,14 +123,17 @@ contract DAI3Strategy {
         int128 borrowFactor = _calculateBorrowFactor();
 
         for (uint256 i = 0; i < numBorrows; i++) {
-            dai.approve(cDaiAddress, 0);
-            dai.approve(cDaiAddress, amount);
+            _dai.approve(_cDaiAddress, 0);
+            _dai.approve(_cDaiAddress, amount);
 
-            uint256 error = cDai.mint(amount);
+            uint256 error = _cDai.mint(amount);
             require(error == 0, "CErc20.mint Error");
 
-            (uint256 error2, uint256 liquidity, uint256 shortfall) = comptroller
-                .getAccountLiquidity(address(this));
+            (
+                uint256 error2,
+                uint256 liquidity,
+                uint256 shortfall
+            ) = _comptroller.getAccountLiquidity(address(this));
             require(error2 == 0, "could not get account liquidity");
             require(shortfall == 0, "account underwater");
             require(liquidity > 0, "account has excess collateral");
@@ -135,7 +141,7 @@ contract DAI3Strategy {
             // uint256 borrowAmount = borrowFactor.mulu(liquidity);
             uint256 borrowAmount = borrowFactor.mulu(amount);
 
-            uint256 error3 = cDai.borrow(borrowAmount);
+            uint256 error3 = _cDai.borrow(borrowAmount);
             require(error3 == 0, "could not borrow");
 
             emit BorrowedDai(borrowFactor, liquidity, shortfall, borrowAmount);
@@ -145,21 +151,23 @@ contract DAI3Strategy {
     }
 
     function _calculateBorrowFactor() internal returns (int128) {
-        (, uint256 collateralFactorMantissa) = comptroller.markets(cDaiAddress);
+        (, uint256 collateralFactorMantissa) = _comptroller.markets(
+            _cDaiAddress
+        );
         int128 collateralFactor = collateralFactorMantissa.fromUInt().div(
-            ETH_MANTISSA.fromUInt()
+            _ETH_MANTISSA.fromUInt()
         );
 
-        uint256 borrowRateMantissa = cDai.borrowRatePerBlock();
+        uint256 borrowRateMantissa = _cDai.borrowRatePerBlock();
         int128 borrowRate = borrowRateMantissa.fromUInt().div(
-            ETH_MANTISSA.fromUInt()
+            _ETH_MANTISSA.fromUInt()
         );
 
         int128 interestFactorPerDay = borrowRate
-            .mul(BLOCKS_PER_DAY.fromUInt())
+            .mul(_BLOCKS_PER_DAY.fromUInt())
             .add(1.fromUInt());
         int128 interestFactorPerPeriod = interestFactorPerDay
-            .pow(DAYS_IN_PERIOD)
+            .pow(_DAYS_IN_PERIOD)
             .sub(1.fromUInt());
 
         int128 borrowFactor = collateralFactor.sub(interestFactorPerPeriod);
@@ -167,8 +175,8 @@ contract DAI3Strategy {
     }
 
     function repayBorrow(uint256 amount) public returns (bool) {
-        dai.approve(cDaiAddress, amount);
-        uint256 error = cDai.repayBorrow(amount);
+        _dai.approve(_cDaiAddress, amount);
+        uint256 error = _cDai.repayBorrow(amount);
 
         require(error == 0, "CErc20.repayBorrow Error");
         return true;
