e2ebc679fd53acb77e3440cc082915a514547c06
Chan-Ho Suh <chanhosuh@gmail.com>
2020-08-27 23:49:02 -0400

    Update yield farming test with claim COMP

--- integration/LeveragedYieldFarm.js
+++ integration/LeveragedYieldFarm.js
@@ -7,6 +7,7 @@ const {
   constants,
   expectEvent,
   expectRevert,
+  time,
 } = require("@openzeppelin/test-helpers");
 const { expect } = require("chai");
 const {
@@ -16,15 +17,18 @@ const {
   getERC20Balance,
   undoErc20,
 } = require("./utils");
+const { advanceBlock } = require("@openzeppelin/test-helpers/src/time");
 
 const LeveragedYieldFarm = artifacts.require("LeveragedYieldFarm");
 const IMintableERC20 = artifacts.require("IMintableERC20");
+const IERC20 = artifacts.require("IERC20");
 const CErc20 = artifacts.require("CErc20");
 
 // https://changelog.makerdao.com/releases/mainnet/latest/contracts.json
 const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // MCD_DAI
 const DAI_MINTER_ADDRESS = "0x9759A6Ac90977b93B58547b4A71c78317f391A28"; // MCD_JOIN_DAI
 const CDAI_ADDRESS = "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643";
+const COMP_ADDRESS = "0xc00e94Cb662C3520282E6f5717214004A7f26888";
 
 const timeout = 120000; // in millis
 
@@ -34,11 +38,13 @@ contract("LeveragedYieldFarm", async (accounts) => {
   let farm;
   let daiToken;
   let cDaiToken;
+  let compToken;
 
   beforeEach(async () => {
     daiToken = await IMintableERC20.at(DAI_ADDRESS);
     farm = await LeveragedYieldFarm.new();
     cDaiToken = await CErc20.at(CDAI_ADDRESS);
+    compToken = await IERC20.at(COMP_ADDRESS);
 
     await mintERC20Tokens(
       DAI_ADDRESS,
@@ -51,7 +57,7 @@ contract("LeveragedYieldFarm", async (accounts) => {
     console.log("       --->  DAI balance:", daiBalance.toString() / 1e18);
   });
 
-  it("deposit DAI with flash loan", async () => {
+  it("farm COMP with DAI flash loan", async () => {
     const amount = dai("100");
     console.log("       --->  DAI deposited:", amount.toString() / 1e18);
 
@@ -74,25 +80,18 @@ contract("LeveragedYieldFarm", async (accounts) => {
       "       --->  total DAI locked:",
       cDaiBalance.mul(exchangeRate).toString() / 1e36
     );
+    console.log("");
+
+    blocksPerDay = 4 * 60 * 24;
+    blocksPerWeek = 7 * blocksPerDay;
+    for (i = 0; i < blocksPerDay; i++) {
+      await time.advanceBlock();
+    }
+    console.log("       --->  Number of blocks waited:", blocksPerDay);
+
+    await farm.withdrawDai(amount, { from: deployer, gas: 2000000 });
+
+    const compBalance = await compToken.balanceOf(deployer);
+    console.log("       --->  COMP balance:", compBalance.toString() / 1e18);
   }).timeout(timeout);
-
-  advanceBlock = () => {
-    return new Promise((resolve, reject) => {
-      web3.currentProvider.sendAsync(
-        {
-          jsonrpc: "2.0",
-          method: "evm_mine",
-          id: new Date().getTime(),
-        },
-        (err, result) => {
-          if (err) {
-            return reject(err);
-          }
-          const newBlockHash = web3.eth.getBlock("latest").hash;
-
-          return resolve(newBlockHash);
-        }
-      );
-    });
-  };
 });
