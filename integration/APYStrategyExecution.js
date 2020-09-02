4ce89774f923878cb1fe6b94ff51851e7c64f2d7
Chan-Ho Suh <chanhosuh@gmail.com>
2020-09-02 14:39:32 -0400

    Fix syntax errors in strategy execution tests
    Errors were being thrown during test runs, so I looked
    into them and fixed them:
    
    "Setup" for APYStrategyExecution used "toNumber" instead
    of "toString" (gave 53 bits error).
    
    expectEvent.inTransaction is async, so needs "await".  This
    was causing unresolved promise errors.   After adding "await",
    one of the asserts now fails.

--- integration/APYStrategyExecution.js
+++ integration/APYStrategyExecution.js
@@ -1,7 +1,7 @@
 const { ethers, web3, artifacts, contract } = require("@nomiclabs/buidler");
-const { defaultAbiCoder: abiCoder } = ethers.utils
-const BigNumber = ethers.BigNumber
-const { mintERC20Tokens } = require("./utils.js")
+const { defaultAbiCoder: abiCoder } = ethers.utils;
+const BigNumber = ethers.BigNumber;
+const { mintERC20Tokens } = require("./utils.js");
 const {
   BN,
   ether,
@@ -36,7 +36,7 @@ const borrowRatePerBlock = IcDAI.getSighash("borrowRatePerBlock");
 const borrow = IcDAI.getSighash("borrow");
 const repayBorrow = IcDAI.getSighash("repayBorrow");
 const balanceOf = IcDAI.getSighash("balanceOf");
-const comp_approve = ICOMP.getSighash("approve")
+const comp_approve = ICOMP.getSighash("approve");
 const enterMarkets = IComptroller.getSighash("enterMarkets");
 const getAccountLiquidity = IComptroller.getSighash("getAccountLiquidity");
 const claimComp = IComptroller.getSighash("claimComp");
@@ -51,67 +51,72 @@ const claimComp = IComptroller.getSighash("claimComp");
 //   0
 // ]
 
-
 contract("APYStrategyExecution", async (accounts) => {
-  const [owner] = accounts
-  const DAI_MINTER = '0x9759A6Ac90977b93B58547b4A71c78317f391A28'
-  const amount = 1000
-  let dai_contract
-  let cDAI_contract
-  let comp_contract
-  let comptroller_contract
-  let e_cDAI_address
-  let e_amount
-  let borrowAmount
+  const [owner] = accounts;
+  const DAI_MINTER = "0x9759A6Ac90977b93B58547b4A71c78317f391A28";
+  const amount = new BN("1000");
+  let dai_contract;
+  let cDAI_contract;
+  let comp_contract;
+  let comptroller_contract;
+  let e_cDAI_address;
+  let e_amount;
+  let borrowAmount;
 
   before("Setup", async () => {
     // Contracts
-    dai_contract = await DAI.at('0x6b175474e89094c44da98b954eedeac495271d0f')
-    cDAI_contract = await cDAI.at('0x5d3a536e4d6dbd6114cc1ead35777bab948e3643')
-    comp_contract = await COMP.at('0xc00e94cb662c3520282e6f5717214004a7f26888')
-    comptroller_contract = await Comptroller.at('0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b')
+    dai_contract = await DAI.at("0x6b175474e89094c44da98b954eedeac495271d0f");
+    cDAI_contract = await cDAI.at("0x5d3a536e4d6dbd6114cc1ead35777bab948e3643");
+    comp_contract = await COMP.at("0xc00e94cb662c3520282e6f5717214004a7f26888");
+    comptroller_contract = await Comptroller.at(
+      "0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b"
+    );
 
     // Function Encodings
-    e_cDAI_address = abiCoder.encode(['address'], [cDAI_contract.address]);
-    e_amount = abiCoder.encode(['uint256'], [amount]);
-    e_owner = abiCoder.encode(['address'], [owner]);
-    borrowAmount = abiCoder.encode(['uint256'], [1]);
+    e_cDAI_address = abiCoder.encode(["address"], [cDAI_contract.address]);
+    e_amount = abiCoder.encode(["uint256"], [amount]);
+    e_owner = abiCoder.encode(["address"], [owner]);
+    borrowAmount = abiCoder.encode(["uint256"], [1]);
 
     // mint ourselves DAI
     await mintERC20Tokens(dai_contract.address, owner, DAI_MINTER, amount);
 
-    let dai_val = await dai_contract.balanceOf.call(owner)
-    console.log(dai_val.toNumber());
+    let dai_val = await dai_contract.balanceOf.call(owner);
+    console.log(dai_val.toString());
 
-    let cdai_val = await cDAI_contract.balanceOf.call(owner)
-    console.log(cdai_val.toNumber());
+    let cdai_val = await cDAI_contract.balanceOf.call(owner);
+    console.log(cdai_val.toString());
+  });
 
-  })
-
-  describe('Example Execution', async () => {
-    it('Execute Steps', async () => {
-
+  describe("Example Execution", async () => {
+    it("Execute Steps", async () => {
       // execute steps
-      const exec = await APYStrategyExecutor.new()
+      const exec = await APYStrategyExecutor.new();
       const trx = await exec.execute(
         [
-          [dai_contract.address, dai_approve, [], [e_cDAI_address, e_amount], []],
+          [
+            dai_contract.address,
+            dai_approve,
+            [],
+            [e_cDAI_address, e_amount],
+            [],
+          ],
           [cDAI_contract.address, balanceOf, [], [e_owner], []],
           [cDAI_contract.address, mint, [], [e_amount], []],
-          [cDAI_contract.address, borrow, [], [borrowAmount], []]
+          [cDAI_contract.address, borrow, [], [borrowAmount], []],
         ],
         { from: owner }
-      )
+      );
 
-      // expectEvent.inTransaction(trx.tx, exec, 'InitialCall', { a: '0x0000000000000000000000000000000000000000000000000000000000000001' })
-      // expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAUint256', { a: '1' })
-      // expectEvent.inTransaction(trx.tx, contractA, 'ExecuteABytes32', { a: '0x0000000000000000000000000000000000000000000000000000000000000001' })
-      // expectEvent.inTransaction(trx.tx, contractA, 'MultiParam', { a: '1', b: '100', c: '1' })
-      // expectEvent.inTransaction(trx.tx, contractA, 'MultiParam', { a: '1', b: '1', c: '100' })
-      // expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAReturnArray', { a: ['1000', '500'] })
-      // expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAArrayParam', { a: '1000' })
-      //expectEvent.inTransaction(trx.tx, exec, 'Params', { params: '3' })
-      //expectEvent.inTransaction(trx.tx, exec, 'EncodeCallData', { length: '3' })
-    })
-  })
-})
+      // await expectEvent.inTransaction(trx.tx, exec, 'InitialCall', { a: '0x0000000000000000000000000000000000000000000000000000000000000001' })
+      // await expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAUint256', { a: '1' })
+      // await expectEvent.inTransaction(trx.tx, contractA, 'ExecuteABytes32', { a: '0x0000000000000000000000000000000000000000000000000000000000000001' })
+      // await expectEvent.inTransaction(trx.tx, contractA, 'MultiParam', { a: '1', b: '100', c: '1' })
+      // await expectEvent.inTransaction(trx.tx, contractA, 'MultiParam', { a: '1', b: '1', c: '100' })
+      // await expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAReturnArray', { a: ['1000', '500'] })
+      // await expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAArrayParam', { a: '1000' })
+      // await expectEvent.inTransaction(trx.tx, exec, 'Params', { params: '3' })
+      // await expectEvent.inTransaction(trx.tx, exec, 'EncodeCallData', { length: '3' })
+    });
+  });
+});
--- integration/APYStrategyExecutionExamples.js
+++ integration/APYStrategyExecutionExamples.js
@@ -1,6 +1,6 @@
 const { ethers, web3, artifacts, contract } = require("@nomiclabs/buidler");
-const { defaultAbiCoder: abiCoder } = ethers.utils
-const BigNumber = ethers.BigNumber
+const { defaultAbiCoder: abiCoder } = ethers.utils;
+const BigNumber = ethers.BigNumber;
 const {
   BN,
   ether,
@@ -13,42 +13,77 @@ const {
 const { expect } = require("chai");
 const APYStrategyExecutor = artifacts.require("APYStrategyExecutor");
 const APYContractA = artifacts.require("APYContractA");
-const AInterface = new ethers.utils.Interface(APYContractA.abi)
-const executeASelector = AInterface.getSighash("executeA")
-const executeAMultiParamSelector = AInterface.getSighash("executeAMultiParam")
-const executeAReturnArraySelector = AInterface.getSighash("executeAReturnArray")
-const executeAArrayParam = AInterface.getSighash("executeAArrayParam")
+const AInterface = new ethers.utils.Interface(APYContractA.abi);
+const executeASelector = AInterface.getSighash("executeA");
+const executeAMultiParamSelector = AInterface.getSighash("executeAMultiParam");
+const executeAReturnArraySelector = AInterface.getSighash(
+  "executeAReturnArray"
+);
+const executeAArrayParam = AInterface.getSighash("executeAArrayParam");
 
-const e_0 = abiCoder.encode(['uint256'], [0])
-const e_1 = abiCoder.encode(['uint256'], [1])
+const e_0 = abiCoder.encode(["uint256"], [0]);
+const e_1 = abiCoder.encode(["uint256"], [1]);
 
-console.log(e_0)
-console.log(e_1)
+console.log(e_0);
+console.log(e_1);
 
 contract("APYStrategyExecution", async (accounts) => {
-  describe('Example Execution', async () => {
-    it('Basic Calls', async () => {
-      const contractA = await APYContractA.new()
+  describe("Example Execution", async () => {
+    it("Basic Calls", async () => {
+      const contractA = await APYContractA.new();
 
       // execute on data
-      const exec = await APYStrategyExecutor.new()
-      const trx = await exec.execute(
+      const exec = await APYStrategyExecutor.new();
+      const trx = await exec.execute([
+        [contractA.address, executeASelector, [], [e_1], []],
         [
-          [contractA.address, executeASelector, [], [e_1], []],
-          [contractA.address, executeAMultiParamSelector, [0], [e_1, e_1, e_1], [1]],
-          [contractA.address, executeAMultiParamSelector, [0, 0, 0], [e_1, e_1, e_1], [constants.MAX_UINT256, 2, constants.MAX_UINT256]],
-          [contractA.address, executeAReturnArraySelector, [0, 0, 0], [e_1], [constants.MAX_UINT256, constants.MAX_UINT256, 0]]
-          // [contractA.address, executeAArrayParam, [0, 0, 0], [e_1], [constants.MAX_UINT256, constants.MAX_UINT256, 0]] // does not work
-          // NOTE: 0 is cheaper in gas
-        ]
-      )
+          contractA.address,
+          executeAMultiParamSelector,
+          [0],
+          [e_1, e_1, e_1],
+          [1],
+        ],
+        [
+          contractA.address,
+          executeAMultiParamSelector,
+          [0, 0, 0],
+          [e_1, e_1, e_1],
+          [constants.MAX_UINT256, 2, constants.MAX_UINT256],
+        ],
+        [
+          contractA.address,
+          executeAReturnArraySelector,
+          [0, 0, 0],
+          [e_1],
+          [constants.MAX_UINT256, constants.MAX_UINT256, 0],
+        ],
+        // [contractA.address, executeAArrayParam, [0, 0, 0], [e_1], [constants.MAX_UINT256, constants.MAX_UINT256, 0]] // does not work
+        // NOTE: 0 is cheaper in gas
+      ]);
 
-      expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAUint256', { a: '1' })
-      expectEvent.inTransaction(trx.tx, contractA, 'MultiParam', { a: '1', b: '100', c: '1' })
-      expectEvent.inTransaction(trx.tx, contractA, 'MultiParam', { a: '1', b: '1', c: '100' })
-      expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAReturnArray', { a: ['1000', '500'] })
-      // expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAArrayParam', { a: '1000' })
-      //expectEvent.inTransaction(trx.tx, exec, 'Params', { params: '3' })
-    })
-  })
-})
+      await expectEvent.inTransaction(trx.tx, contractA, "ExecuteAUint256", {
+        a: "1",
+      });
+      await expectEvent.inTransaction(trx.tx, contractA, "MultiParam", {
+        a: "1",
+        b: "100",
+        c: "1",
+      });
+      await expectEvent.inTransaction(trx.tx, contractA, "MultiParam", {
+        a: "1",
+        b: "1",
+        c: "100",
+      });
+      await expectEvent.inTransaction(
+        trx.tx,
+        contractA,
+        "ExecuteAReturnArray",
+        {
+          a: ["1000", "500"],
+        }
+      );
+      // await expectEvent.inTransaction(trx.tx, contractA, 'ExecuteAArrayParam', { a: '1000' })
+      // await expectEvent.inTransaction(trx.tx, exec, 'Params', { params: '3' })
+    });
+  });
+});
