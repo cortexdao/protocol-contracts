0007356bacd1377341b9c7c4a6bf29edd463b3bc
The Architect <email@example.com>
2020-09-06 11:36:08 -0700

    update naming

--- contracts/APYStrategyGeneralExecutor.sol
+++ contracts/APYStrategyGeneralExecutor.sol
@@ -3,7 +3,7 @@ pragma experimental ABIEncoderV2;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract APYStrategyExecutor is Ownable {
+contract APYStrategyGeneralExecutor is Ownable {
     struct Data {
         address target;
         bytes data;
@@ -12,30 +12,6 @@ contract APYStrategyExecutor is Ownable {
     function execute(Data[] calldata executionSteps) external payable {
         for (uint256 i = 0; i < executionSteps.length; i++) {
             _call(executionSteps[i].target, executionSteps[i].data);
-        }
-    }
-
-    function _delegate(address target, bytes memory data)
-        private
-        returns (bytes memory)
-    {
-        // solhint-disable-next-line avoid-low-level-calls
-        (bool success, bytes memory returndata) = target.delegatecall(data);
-        if (success) {
-            return returndata;
-        } else {
-            // Look for revert reason and bubble it up if present
-            if (returndata.length > 0) {
-                // The easiest way to bubble the revert reason is using memory via assembly
-
-                // solhint-disable-next-line no-inline-assembly
-                assembly {
-                    let returndata_size := mload(returndata)
-                    revert(add(32, returndata), returndata_size)
-                }
-            } else {
-                revert("DELEGATECALL_FAILED");
-            }
         }
     }
 
