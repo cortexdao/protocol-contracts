06517bd569b4e946541bf144863307c4a8e0d68e
Chan-Ho Suh <chanhosuh@gmail.com>
2020-09-05 23:36:49 -0400

    Add missing DAI minter address for CI
    Removed reading this address from .env as it is
    not a secret and we need it for the CI build.

--- scripts/unlocked_addresses.js
+++ scripts/unlocked_addresses.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+require("dotenv").config();
+const { DAI_MINTER_ADDRESS } = require("../integration/constants");
 // comma-separated list of unlock addresses
-const unlockAddresses = process.env.DAI_MINTER || ''
-process.stdout.write(unlockAddresses)
+const unlockAddresses = DAI_MINTER_ADDRESS || "";
+process.stdout.write(unlockAddresses);
