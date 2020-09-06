ea904b9260c14ef4206b0a59461d4b39e2596142
Chan-Ho Suh <chanhosuh@gmail.com>
2020-09-06 19:01:11 -0400

    Fix typo in script :(

--- scripts/infura.js
+++ scripts/infura.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 let apiKey = process.env.INFURA_API_KEY || ''
 if (process.env.GITHUB_ACTIONS) {
     apiKey = '77c0d857bd4447b49816158a51a5e115'
+}
 if (apiKey) {
     process.stdout.write('https://mainnet.infura.io/v3/' + apiKey)
 }
