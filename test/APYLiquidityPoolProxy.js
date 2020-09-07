065b7e760fb967b61b573ee2eae8076bde6d11db
Chan-Ho Suh <chanhosuh@gmail.com>
2020-09-07 00:51:59 -0400

    Add unit tests for pool proxy deposit

--- test/APYLiquidityPoolProxy.js
+++ test/APYLiquidityPoolProxy.js
@@ -18,6 +18,7 @@ const APYLiquidityPoolImplementation = artifacts.require(
 );
 const APT = artifacts.require("APT");
 const MockContract = artifacts.require("MockContract");
+const dai = ether;
 
 contract("APYLiquidityPoolProxy", async (accounts) => {
   const [deployer, admin, wallet, other] = accounts;
@@ -27,9 +28,15 @@ contract("APYLiquidityPoolProxy", async (accounts) => {
   let poolImpl;
   let poolProxy;
 
-  // deploy pool and APT contracts before each test
+  // we need to be careful to use EVM snapshots,
+  // as we are messing with the proxy and so our
+  // test failures can impact other test suites
+  let snapshotId;
+
+  // deploy pool contracts before each test
   beforeEach(async () => {
-    apt = await APT.new({ from: deployer });
+    let snapshot = await timeMachine.takeSnapshot();
+    snapshotId = snapshot["result"];
 
     poolImpl = await APYLiquidityPoolImplementation.new({ from: deployer });
     poolProxy = await APYLiquidityPoolProxy.new(poolImpl.address, admin, [], {
@@ -41,6 +48,51 @@ contract("APYLiquidityPoolProxy", async (accounts) => {
     pool = await APYLiquidityPoolImplementation.at(poolProxy.address);
   });
 
+  afterEach(async () => {
+    await timeMachine.revertToSnapshot(snapshotId);
+  });
+
+  describe("user gets right amount of APT from DAI deposit", async () => {
+    beforeEach(async () => {
+      apt = await APT.new({ from: deployer });
+      pool.setAptAddress(apt.address, { from: deployer });
+      apt.setPoolAddress(pool.address, { from: deployer });
+    });
+
+    it("addLiquidity creates APT for user", async () => {
+      let balanceOf = await apt.balanceOf(wallet);
+      expect(balanceOf).to.bignumber.equal("0");
+
+      const daiDeposit = dai("1");
+      await mockDaiTransfer(pool, daiDeposit);
+
+      await pool.addLiquidity(daiDeposit, {
+        from: wallet,
+      });
+      balanceOf = await apt.balanceOf(wallet);
+      expect(balanceOf).to.bignumber.gt("0");
+    });
+
+    it("addLiquidity creates correctly calculated amount of APT", async () => {
+      await mockDaiTransfer(pool, dai("10"));
+      // use another account to call addLiquidity and create non-zero
+      // token and DAI supplies in contract
+      await pool.addLiquidity(dai("10"), {
+        from: other,
+      });
+
+      // now we can check the expected mint amount based on the DAI ratio
+      const daiDeposit = ether("2");
+      const expectedMintAmount = await pool.calculateMintAmount(daiDeposit, {
+        from: wallet,
+      });
+
+      await pool.addLiquidity(daiDeposit, { from: wallet });
+      const mintAmount = await apt.balanceOf(wallet);
+      expect(mintAmount).to.bignumber.equal(expectedMintAmount);
+    });
+  });
+
   describe("proxy delegates to implementation", async () => {
     it("owner is deployer", async () => {
       expect(await pool.owner()).to.equal(deployer);
@@ -48,7 +100,7 @@ contract("APYLiquidityPoolProxy", async (accounts) => {
 
     it("owner can set APT address", async () => {
       try {
-        await pool.setAptAddress(apt.address, { from: deployer });
+        await pool.setAptAddress(DUMMY_ADDRESS, { from: deployer });
       } catch {
         assert.fail("Cannot set APT address.");
       }
@@ -56,7 +108,7 @@ contract("APYLiquidityPoolProxy", async (accounts) => {
 
     it("reverts if non-owner tries setting APT address", async () => {
       await expectRevert(
-        pool.setAptAddress(apt.address, { from: other }),
+        pool.setAptAddress(DUMMY_ADDRESS, { from: other }),
         "Ownable: caller is not the owner"
       );
     });
@@ -78,20 +130,6 @@ contract("APYLiquidityPoolProxy", async (accounts) => {
   });
 
   describe("proxy has admin functionality", async () => {
-    // we need to be careful to use EVM snapshots,
-    // as we are messing with the proxy and so our
-    // test failures can impact other test suites
-    let snapshotId;
-
-    beforeEach(async () => {
-      let snapshot = await timeMachine.takeSnapshot();
-      snapshotId = snapshot["result"];
-    });
-
-    afterEach(async () => {
-      await timeMachine.revertToSnapshot(snapshotId);
-    });
-
     it("admin can call admin functions (non-upgrade)", async () => {
       expect(await poolProxy.admin.call({ from: admin })).to.equal(admin);
       expect(await poolProxy.implementation.call({ from: admin })).to.equal(
@@ -139,4 +177,24 @@ contract("APYLiquidityPoolProxy", async (accounts) => {
       );
     });
   });
+
+  // test helper to mock ERC20 functions on underlyer token
+  const mockDaiTransfer = async (liquidityPoolContract, amount) => {
+    const mock = await MockContract.new();
+    await liquidityPoolContract.setUnderlyerAddress(mock.address, {
+      from: deployer,
+    });
+    const allowanceAbi = apt.contract.methods
+      .allowance(ZERO_ADDRESS, ZERO_ADDRESS)
+      .encodeABI();
+    const transferFromAbi = apt.contract.methods
+      .transferFrom(ZERO_ADDRESS, ZERO_ADDRESS, 0)
+      .encodeABI();
+    const transferAbi = apt.contract.methods
+      .transfer(ZERO_ADDRESS, 0)
+      .encodeABI();
+    await mock.givenMethodReturnUint(allowanceAbi, amount);
+    await mock.givenMethodReturnBool(transferAbi, true);
+    await mock.givenMethodReturnBool(transferFromAbi, true);
+  };
 });
