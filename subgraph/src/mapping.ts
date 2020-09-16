4b552b1f562215aa5a10eee9f39628b309f82507
opz <will.shahda@gmail.com>
2020-09-15 22:13:04 -0400

    Compute unique ID for TVL subgraph entities

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,14 +1,24 @@
-import { DepositAPT, RedeemedAPT } from '../generated/APYLiquidityPoolImplementation/APYLiquidityPoolImplementation'
+import { DepositedAPT, RedeemedAPT } from '../generated/APYLiquidityPoolImplementation/APYLiquidityPoolImplementation'
 import { TotalValueLocked } from '../generated/schema'
 
-export function handleDepositAPT(event: DepositAPT): void {
-  let tvl = new TotalValueLocked(event.params.id.toHex())
+export function handleDepositedAPT(event: DepositedAPT): void {
+  let tvl = new TotalValueLocked(
+    event.params.sender.toHex()
+    + event.block.timestamp.toString()
+    + event.logIndex.toString()
+    + event.transaction.hash.toString()
+  )
   tvl.totalValueLocked = event.params.totalValueLocked
   tvl.save()
 }
 
 export function handleRedeemedAPT(event: RedeemedAPT): void {
-  let tvl = new TotalValueLocked(event.params.id.toHex())
+  let tvl = new TotalValueLocked(
+    event.params.sender.toHex()
+    + event.block.timestamp.toString()
+    + event.logIndex.toString()
+    + event.transaction.hash.toString()
+  )
   tvl.totalValueLocked = event.params.totalValueLocked
   tvl.save()
 }
