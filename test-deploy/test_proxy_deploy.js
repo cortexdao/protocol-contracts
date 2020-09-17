4cc7d09e4d8762db9bbabd0d3199544a29ae5311
Chan-Ho Suh <chanhosuh@gmail.com>
2020-09-17 13:01:47 -0400

    Add test to check supported tokens

--- test-deploy/test_proxy_deploy.js
+++ test-deploy/test_proxy_deploy.js
@@ -1,11 +1,16 @@
+/*
+ * Command to run test script:
+ *
+ * $ yarn buidler --network kovan run test-deploy/test_proxy_deploy.js
+ */
 require("dotenv").config();
 const APYLiquidityPoolImplementationArtifact = require("../deployments/kovan/APYLiquidityPoolImplementation.json");
 const APYLiquidityPoolProxyArtifact = require("../deployments/kovan/APYLiquidityPoolProxy.json");
 const ProxyAdminArtifact = require("../deployments/kovan/ProxyAdmin.json");
-const { ethers, web3, artifacts, contract } = require("@nomiclabs/buidler");
-const { defaultAbiCoder: abiCoder } = ethers.utils;
-const IERC20 = new ethers.utils.Interface(artifacts.require("IERC20").abi);
-const ERC20 = new ethers.utils.Interface(artifacts.require("ERC20").abi);
+const { ethers, web3, artifacts } = require("@nomiclabs/buidler");
+const { abiCoder } = ethers.utils;
+const IERC20 = artifacts.require("IERC20");
+const ERC20 = artifacts.require("ERC20");
 
 const { erc20 } = require("../utils/helpers");
 const { expectRevert } = require("@openzeppelin/test-helpers");
@@ -16,9 +21,6 @@ console.log("Endpoint:", endpoint);
 
 const main = async () => {
   const provider = new ethers.providers.JsonRpcProvider(endpoint);
-  provider.getBlockNumber().then((result) => {
-    console.log("Current block number: " + result);
-  });
 
   const logicAddress = APYLiquidityPoolImplementationArtifact.address;
   const logicAbi = APYLiquidityPoolImplementationArtifact.abi;
@@ -31,13 +33,13 @@ const main = async () => {
   let admin = new ethers.Contract(adminAddress, adminAbi);
 
   const deployerWallet = ethers.Wallet.fromMnemonic(mnemonic).connect(provider);
-  console.log("Account 0:", deployerWallet.address);
+  console.log("Account 0 (deployer):", deployerWallet.address);
 
   path = "m/44'/60'/0'/0/2";
   const userWallet = ethers.Wallet.fromMnemonic(mnemonic, path).connect(
     provider
   );
-  console.log("Account 2:", userWallet.address);
+  console.log("Account 2 (test user):", userWallet.address);
 
   // check logic is accessible through the proxy
   pool = pool.connect(userWallet);
@@ -50,11 +52,21 @@ const main = async () => {
   //    b. set in logic contract's portion of storage, to protect the initializer
   // 2. check logic address is set on the proxy
   admin = admin.connect(userWallet);
+  expect(await admin.getProxyAdmin(pool.address)).to.equal(admin.address);
+  expect(await pool.proxyAdmin()).to.equal(admin.address);
   expect(await admin.getProxyImplementation(pool.address)).to.equal(
     logic.address
   );
-  expect(await admin.getProxyAdmin(pool.address)).to.equal(admin.address);
-  expect(await pool.proxyAdmin()).to.equal(admin.address);
+
+  // check tokens and chainlink is setup
+  const tokenAddresses = await pool.getSupportedTokens();
+  const tokenSymbols = [];
+  for (const address of tokenAddresses) {
+    const token = new ethers.Contract(address, ERC20.abi).connect(userWallet);
+    const symbol = await token.symbol();
+    tokenSymbols.push(symbol);
+  }
+  expect(new Set(tokenSymbols)).to.eql(new Set(["DAI", "USDC", "USDT"]));
 };
 
 main()
