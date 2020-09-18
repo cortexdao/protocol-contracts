50765de07e9bd2a61ed36755111ed645e4969b7b
The Architect <email@example.com>
2020-09-17 20:31:50 -0700

    Initial Deployment Script

--- buidler.config.js
+++ buidler.config.js
@@ -3,41 +3,9 @@ require("dotenv").config();
 usePlugin("solidity-coverage");
 usePlugin("@nomiclabs/buidler-ethers");
 usePlugin("@nomiclabs/buidler-truffle5");
-usePlugin("buidler-deploy");
-
-function getEnv(env) {
-  let value = process.env[env];
-
-  if (typeof value == "undefined") {
-    value = "";
-    console.error(`WARNING: ${env} environment variable has not been set`);
-  }
-
-  return value;
-}
-
-const mainnetEndpoint = getEnv("MAINNET_ENDPOINT");
-const mainnetMnemonic = getEnv("MAINNET_MNEMONIC");
-
-const kovanEndpoint = getEnv("KOVAN_ENDPOINT");
-const kovanMnemonic = getEnv("KOVAN_MNEMONIC");
 
 module.exports = {
   networks: {
-    mainnet: {
-      url: mainnetEndpoint,
-      chainId: 1,
-      accounts: {
-        mnemonic: mainnetMnemonic,
-      },
-    },
-    kovan: {
-      url: kovanEndpoint,
-      chainId: 42,
-      accounts: {
-        mnemonic: kovanMnemonic,
-      },
-    },
     localhost: {
       url: "http://127.0.0.1:8545",
       timeout: 1000000,
@@ -45,6 +13,19 @@ module.exports = {
     coverage: {
       url: "http://localhost:8555",
     },
+    mainnet: {
+      url: 'https://mainnet.infura.io/v3/' + process.env.INFURA_API_KEY,
+      gasPrice: 500e9,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    },
+    kovan: {
+      url: 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY,
+      accounts: {
+        mnemonic: process.env.MNEMONIC,
+      },
+    }
   },
   solc: {
     version: "0.6.6",
@@ -55,15 +36,5 @@ module.exports = {
   },
   mocha: {
     timeout: 1000000,
-  },
-  paths: {
-    deploy: "deploy",
-    deployments: "deployments",
-    imports: `imports`,
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0,
-    },
-  },
+  }
 };
---
+++ scripts/1_initial_deployment.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const ProxyAdmin = artifacts.require("ProxyAdmin");
+const APYLiquidityPoolProxy = artifacts.require("APYLiquidityPoolProxy");
+const APYLiquidityPoolImplementation = artifacts.require(
+  "APYLiquidityPoolImplementation"
+);
+const chainIdToAggregators = require("../config/addresses.json");
+
+async function main() {
+  const networkID = network.name.toUpperCase()
+  console.log(`${networkID} selected`)
+
+  const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin")
+  const APYLiquidityPoolImplementation = await ethers.getContractFactory("APYLiquidityPoolImplementation")
+  const APYLiquidityPoolProxy = await ethers.getContractFactory("APYLiquidityPoolProxy")
+
+  const proxyAdmin = await ProxyAdmin.deploy()
+  await proxyAdmin.deployed()
+  console.log(`ProxyAdmin: ${proxyAdmin.address}`)
+
+  const logic = await APYLiquidityPoolImplementation.deploy()
+  await logic.deployed()
+  console.log(`Implementation Logic: ${logic.address}`)
+
+  const proxy = await APYLiquidityPoolProxy.deploy(logic.address, proxyAdmin.address)
+  await proxy.deployed()
+  console.log(`Proxy: ${proxy.address}`)
+
+  const instance = await APYLiquidityPoolImplementation.attach(proxy.address)
+
+  await instance.setAdminAddress(proxyAdmin.address)
+  console.log("admin address set")
+
+  for ({ symbol, token, aggregator } of chainIdToAggregators[networkID]) {
+    await instance.addTokenSupport(token, aggregator)
+    console.log(`${symbol}: ${token} -> ${aggregator}`)
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
