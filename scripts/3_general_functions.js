91e89d4a7bf72d7a10b039a5a25fc6a2b3f435da
The Architect <email@example.com>
2020-09-17 21:02:02 -0700

    general functions

---
+++ scripts/3_general_functions.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const chainIdToAggregators = require("../config/addresses.json");
+
+async function main() {
+  const networkID = network.name.toUpperCase()
+  console.log(`${networkID} selected`)
+
+  const APYLiquidityPoolImplementation = await ethers.getContractFactory("APYLiquidityPoolImplementation")
+  const APYLiquidityPoolProxy = await ethers.getContractFactory("APYLiquidityPoolProxy")
+
+  const proxy = await APYLiquidityPoolProxy.attach('')
+  const instance = await APYLiquidityPoolImplementation.attach(proxy.address)
+
+  // NOTE: pick what you want to do
+  // await instance.lock()
+  // await instance.unlock()
+
+  // await instance.lockAddLiquidity()
+  // await instance.unlockAddLiquidity()
+
+  // await instance.lockRedeem()
+  // await instance.unlockRedeem()
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
