8d03642e3edeb6e05b342c693adfbfa98df08bd8
The Architect <email@example.com>
2020-09-17 21:44:57 -0700

    capture deployed addresse

--- scripts/1_initial_deployment.js
+++ scripts/1_initial_deployment.js
@@ -1,5 +1,38 @@
 require('dotenv').config();
 const chainIdToAggregators = require("../config/addresses.json");
+const fs = require('fs')
+
+const chainIds = {
+  MAINNET: '1',
+  RINKEBY: '4',
+  GOERLI: '5',
+  KOVAN: '42',
+}
+
+const chainNames = {
+  '1': 'MAINNET',
+  '4': 'RINKEBY',
+  '5': 'GOERLI',
+  '42': 'KOVAN',
+}
+
+DEPLOYS_JSON = {
+  ProxyAdmin: '../deployed_addresses/ProxyAdminAddresses.json',
+  APYLiquidityPoolImplementation: '../deployed_addresses/APYLiquidityPoolImplementationAddresses.json',
+  APYLiquidityPoolProxy: '../deployed_addresses/APYLiquidityPoolProxyAddresses.json'
+}
+
+async function updateDeployJsons(network, deploy_data) {
+  for (let [contract_name, file_path] of Object.entries(DEPLOYS_JSON)) {
+    // go through all deploys json and update them
+    address_json = require(file_path)
+    address_json[chainIds[network]] = deploy_data[contract_name]
+    address_json_string = JSON.stringify(address_json, null, '  ')
+    fs.writeFileSync(__dirname + '/' + file_path, address_json_string, err => {
+      if (err) throw err
+    })
+  }
+}
 
 async function main() {
   const networkID = network.name.toUpperCase()
@@ -30,6 +63,13 @@ async function main() {
     await instance.addTokenSupport(token, aggregator)
     console.log(`${symbol} -> ${aggregator} Chainlink Oracle Agg`)
   }
+
+  //Update Jsons
+  let deploy_data = {}
+  deploy_data['ProxyAdmin'] = proxyAdmin.address
+  deploy_data['APYLiquidityPoolImplementation'] = logic.address
+  deploy_data['APYLiquidityPoolProxy'] = proxy.address
+  await updateDeployJsons(networkID, deploy_data)
 }
 
 main()
