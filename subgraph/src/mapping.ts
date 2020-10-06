7cd39b1d19dbfb0025e70d006a422757a59d0a85
opz <will.shahda@gmail.com>
2020-10-06 18:18:21 -0400

    WIP - add Transfer entity to subgraph
    Used for finding all possible APT token holders.

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,5 +1,5 @@
-import { DepositedAPT, RedeemedAPT } from '../generated/APYLiquidityPoolImplementation/APYLiquidityPoolImplementation'
-import { TotalEthValueLocked } from '../generated/schema'
+import { DepositedAPT, RedeemedAPT, Transfer } from '../generated/APYLiquidityPoolImplementation/APYLiquidityPoolImplementation'
+import { TotalEthValueLocked, Transfer } from '../generated/schema'
 
 export function handleDepositedAPT(event: DepositedAPT): void {
   let tvl = new TotalEthValueLocked(
@@ -25,4 +25,19 @@ export function handleRedeemedAPT(event: RedeemedAPT): void {
   tvl.poolAddress = event.address
   tvl.totalEthValueLocked = event.params.totalEthValueLocked
   tvl.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  let transfer = new Transfer(
+    event.params.from.toHexString()
+    + event.params.to.toHexString()
+    + event.block.timestamp.toString()
+    + event.logIndex.toString()
+    + event.transaction.hash.toHexString()
+  )
+  transfer.poolAddress = event.address
+  transfer.from = event.params.from
+  transfer.to = event.params.to
+  transfer.value = event.params.value
+  transfer.save()
 }
