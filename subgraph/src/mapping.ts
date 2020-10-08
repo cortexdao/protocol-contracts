01014145a1fd9d03c8d931539bc737c5597989bb
opz <will.shahda@gmail.com>
2020-10-08 03:15:03 -0400

    Handlers for updating subgraph User entities

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,9 +1,11 @@
 import {
+  APYPoolToken,
   DepositedAPT,
   RedeemedAPT,
   Transfer as TransferEvent,
 } from "../generated/DAI_APYPoolToken/APYPoolToken";
-import { TotalEthValueLocked, Transfer } from "../generated/schema";
+import { TotalEthValueLocked, Transfer, User } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
 export function handleDepositedAPT(event: DepositedAPT): void {
   let tvl = new TotalEthValueLocked(
@@ -29,6 +31,27 @@ export function handleRedeemedAPT(event: RedeemedAPT): void {
   tvl.poolAddress = event.address;
   tvl.totalEthValueLocked = event.params.totalEthValueLocked;
   tvl.save();
+
+  let userId = event.params.sender.toHexString() + event.address.toHexString();
+
+  let user = User.load(userId);
+  if (user === null) {
+    user = new User(userId);
+  }
+
+  user.poolAddress = event.address;
+  user.address = event.params.sender;
+
+  let contract = APYPoolToken.bind(event.address);
+  let balance = contract.balanceOf(event.params.sender);
+  let result = contract.try_getAPTEthValue(balance);
+
+  let ethValue = BigInt.fromI32(0);
+  if (!result.reverted) {
+    ethValue = result.value;
+  }
+
+  user.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
@@ -44,4 +67,27 @@ export function handleTransfer(event: TransferEvent): void {
   transfer.to = event.params.to;
   transfer.value = event.params.value;
   transfer.save();
+
+  let userId = event.params.to.toHexString() + event.address.toHexString();
+
+  let user = User.load(userId);
+  if (user === null) {
+    user = new User(userId);
+  }
+
+  user.poolAddress = event.address;
+  user.address = event.params.to;
+
+  let contract = APYPoolToken.bind(event.address);
+  let balance = contract.balanceOf(event.params.to);
+  let result = contract.try_getAPTEthValue(balance);
+
+  let ethValue = BigInt.fromI32(0);
+  if (!result.reverted) {
+    ethValue = result.value;
+  }
+
+  user.accountValue = ethValue;
+
+  user.save();
 }
