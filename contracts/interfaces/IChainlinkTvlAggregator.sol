6306fa355ae47c1cb0fc4b1bc2c867af3fc8044b
Chan-Ho Suh <chanhosuh@gmail.com>
2020-12-05 23:31:45 -0500

    Update with comments, rename contract

--- contracts/interfaces/IChainlinkTvlAggregator.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.6.11;
-
-interface IChainlinkTvlAggregator {
-
-    function getTokenAddresses() external view returns(address[] memory);
-    function balanceOf(address token) external view returns (uint256);
-    function symbolOf(address token) external view returns (string memory);
-}
