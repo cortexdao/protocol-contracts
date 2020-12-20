1f4623285bff10b8558b1041e7d0275b1fc4b594
Chan-Ho Suh <chanhosuh@gmail.com>
2020-12-20 13:27:48 -0500

    Remove unused imports

--- contracts/interfaces/IAddressRegistry.sol
+++ contracts/interfaces/IAddressRegistry.sol
@@ -1,9 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.6.11;
-pragma experimental ABIEncoderV2;
-
-import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 
 interface IAddressRegistry {
     function getIds() external view returns (bytes32[] memory);
