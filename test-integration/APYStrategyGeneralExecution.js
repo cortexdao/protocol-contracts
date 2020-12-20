365dda02dddefbf4cad23b3f595933bc715409c1
The Architect <email@example.com>
2020-12-19 22:04:22 -0800

    checkpoint with trx

--- test-integration/APYStrategyGeneralExecution.js
+++ test-integration/APYStrategyGeneralExecution.js
@@ -1,10 +1,11 @@
 const { ethers, artifacts, contract } = require("hardhat");
-const { waffle } = require("ethereum-waffle");
-const { expect } = require("chai");
-
 const BN = ethers.BigNumber
+const { expect, use } = require("chai");
+const { solidity } = require('ethereum-waffle');
+use(solidity)
 
 const GenericExecutor = artifacts.require("APYGenericExecutor");
+const { expectEvent } = require("@openzeppelin/test-helpers");
 const legos = require('defi-legos')
 
 contract("Test GenericExecutor", async () => {
@@ -12,10 +13,11 @@ contract("Test GenericExecutor", async () => {
 
     const [signer1, signer2] = await ethers.getSigners()
 
-    const DAI = await ethers.getContractFactory("MockContract")
-    const cDAI = await ethers.getContractFactory("MockContract")
+    const DAI = await ethers.getContractAt(legos.maker.abis.DAI, legos.maker.addresses.DAI)
+    const cDAI = await ethers.getContractAt(legos.compound.abis.cDAI, legos.compound.addresses.cDAI)
 
     const signer1Address = await signer1.getAddress();
+    const signer2Address = await signer2.getAddress();
 
     const ethBal = await ethers.provider.getBalance(signer1Address)
     console.log(`ETH Balance: ${ethBal.toString()}`)
@@ -32,9 +34,13 @@ contract("Test GenericExecutor", async () => {
     const cDaiBal2 = await cDAI.balanceOf(signer1.getAddress())
     console.log(`cDAI Balance: ${cDaiBal2.toString()}`)
 
-    const exec = await GenericExecutor.new()
+    const trans = DAI.approve(signer2Address, "999")
+    // await expect(trans).to.emit(DAI, 'Approval')
+    // console.log("first check passed")
+    // console.log(trans)
 
-    const trx = await exec.execute(
+    const exec = await GenericExecutor.new()
+    const trx = exec.execute(
       [
         [legos.maker.addresses.DAI, legos.maker.codecs.DAI.encodeApprove(signer1Address, BN.from('999'))],
         [legos.compound.addresses.cDAI, legos.compound.codecs.cDAI.encodeMint('999')]
@@ -42,9 +48,12 @@ contract("Test GenericExecutor", async () => {
       ]
     )
 
+    // console.log(trx)
+    // console.log(JSON.parse(JSON.stringify(trx.receipt.rawLogs)))
+
     await expect(trx).to.emit(DAI, ' Approval')
 
-    // await expectEvent.inTransaction(trx.tx, legos.maker, 'Approval', { owner: exec.address, spender: account1, value: '999' })
+    // await expectEvent.inTransaction(trx.tx, DAI, 'Approval', { owner: exec.address, spender: signer1Address, value: '999' })
 
     // await expectEvent.inTransaction(trx.tx, A, 'ExecuteAUint256', { a: '100' })
     // await expectEvent.inTransaction(trx.tx, A, 'ExecuteABytes32', { a: '0x0000000000000000000000000000000000000000000000000000000000000064' })
