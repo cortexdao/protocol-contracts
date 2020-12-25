98dc9f41200ee8a2a379d43104a25add22798636
opz <will.shahda@gmail.com>
2020-12-25 03:36:13 -0500

    Test Curve Y Pool add liquidity sequence

--- test-integration/APYStrategyGeneralExecution.js
+++ test-integration/APYStrategyGeneralExecution.js
@@ -1,49 +1,69 @@
-const { ethers, artifacts, contract, web3 } = require("hardhat");
-const BN = ethers.BigNumber;
+const hre = require("hardhat");
+const { artifacts, contract, web3 } = require("hardhat");
+const { DAI_WHALE } = require("../utils/constants");
 
+const IDetailedERC20 = artifacts.require("IDetailedERC20");
 const GenericExecutor = artifacts.require("APYGenericExecutor");
-const { expectEvent } = require("@openzeppelin/test-helpers");
-// const legos = require("defi-legos");
+const {
+  BN,
+  expectEvent, // Assertions for emitted events
+} = require("@openzeppelin/test-helpers");
+const legos = require("defi-legos");
 
-contract("Test GenericExecutor", async () => {
-  it.skip("Execution Test", async () => {
-    const [signer1] = await ethers.getSigners();
-    const signer1Address = await signer1.getAddress();
+async function formattedAmount(token, value) {
+  const decimals = await token.decimals.call();
+  return new BN("10").pow(decimals).mul(new BN(value)).toString();
+}
 
-    const DAI = new web3.eth.Contract(
-      legos.maker.abis.DAI,
-      legos.maker.addresses.DAI
-    );
-    const cDAI = new web3.eth.Contract(
-      legos.compound.abis.cDAI,
-      legos.compound.addresses.cDAI
+async function acquireToken(fundAccount, receiver, token, amount) {
+  /* NOTE: Ganache is setup to control "whale" addresses. This method moves
+  requested funds out of the fund account and into the specified wallet */
+
+  const funds = await formattedAmount(token, amount);
+
+  await token.transfer(receiver, funds, { from: fundAccount });
+  const tokenBal = await token.balanceOf(receiver);
+  console.log(`${token.address} Balance: ${tokenBal.toString()}`);
+}
+
+contract("Test GenericExecutor", async (accounts) => {
+  it.only("Execution Test", async () => {
+    const stableSwapY = new web3.eth.Contract(
+      legos.curvefi.abis.yDAI_yUSDC_yUSDT_ytUSD,
+      legos.curvefi.addresses.yDAI_yUSDC_yUSDT_ytUSD
     );
 
     const exec = await GenericExecutor.new();
+
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: [DAI_WHALE],
+    });
+
+    await web3.eth.sendTransaction({
+      from: accounts[0],
+      to: DAI_WHALE,
+      value: 1e18,
+    });
+
+    const DAI = await IDetailedERC20.at(legos.maker.addresses.DAI);
+
+    await acquireToken(DAI_WHALE, exec.address, DAI, "1000000");
+
     const trx = await exec.execute([
       [
         legos.maker.addresses.DAI,
         legos.maker.codecs.DAI.encodeApprove(
-          legos.compound.addresses.cDAI,
-          BN.from("999")
+          legos.curvefi.addresses.DEPOSIT_Y,
+          100000
         ),
       ],
       [
-        legos.compound.addresses.cDAI,
-        legos.compound.codecs.cDAI.encodeMint("999"),
+        legos.curvefi.addresses.DEPOSIT_Y,
+        legos.curvefi.codecs.DEPOSIT_Y.encodeAddLiquidity([100000, 0, 0, 0], 0),
       ],
-      // [legos.compound.addresses.cDAI, legos.maker.codecs.cDAI.encodeApprove(account1, BN.from('999'))],
     ]);
 
-    await expectEvent.inTransaction(trx.tx, DAI, "Approval", {
-      src: exec.address,
-      guy: legos.compound.addresses.cDAI,
-      wad: "999",
-    });
-    await expectEvent.inTransaction(trx.tx, cDAI, "Mint", {
-      minter: exec.address,
-      mintAmount: "999",
-      mintTokens: "999",
-    });
+    await expectEvent.inTransaction(trx.tx, stableSwapY, "AddLiquidity");
   });
 });
