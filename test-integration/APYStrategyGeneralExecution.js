dced1356172f5ef86b8435b1e570e24598f9e921
opz <will.shahda@gmail.com>
2020-12-27 14:28:37 -0500

    Start to test curve sequence with APY

--- test-integration/APYStrategyGeneralExecution.js
+++ test-integration/APYStrategyGeneralExecution.js
@@ -1,32 +1,18 @@
 const hre = require("hardhat");
 const { artifacts, contract, web3 } = require("hardhat");
-const { DAI_WHALE } = require("../utils/constants");
+const APYManagerAddresses = require("../deployed_addresses/APYManagerProxy.json");
+const DAI_APYPoolTokenAddresses = require("../deployed_addresses/DAI_APYPoolTokenProxy.json");
+const USDC_APYPoolTokenAddresses = require("../deployed_addresses/USDC_APYPoolTokenProxy.json");
+const USDT_APYPoolTokenAddresses = require("../deployed_addresses/USDC_APYPoolTokenProxy.json");
 
 const IDetailedERC20 = artifacts.require("IDetailedERC20");
 const GenericExecutor = artifacts.require("APYGenericExecutor");
-const {
-  BN,
-  expectEvent, // Assertions for emitted events
-} = require("@openzeppelin/test-helpers");
+const APYManager = artifacts.require("APYManager");
+const APYPoolToken = artifacts.require("APYPoolToken");
+const { expectEvent } = require("@openzeppelin/test-helpers");
 const legos = require("defi-legos");
 
-async function formattedAmount(token, value) {
-  const decimals = await token.decimals.call();
-  return new BN("10").pow(decimals).mul(new BN(value)).toString();
-}
-
-async function acquireToken(fundAccount, receiver, token, amount) {
-  /* NOTE: Ganache is setup to control "whale" addresses. This method moves
-  requested funds out of the fund account and into the specified wallet */
-
-  const funds = await formattedAmount(token, amount);
-
-  await token.transfer(receiver, funds, { from: fundAccount });
-  const tokenBal = await token.balanceOf(receiver);
-  console.log(`${token.address} Balance: ${tokenBal.toString()}`);
-}
-
-contract("Test GenericExecutor", async (accounts) => {
+contract("Test GenericExecutor", async () => {
   it.only("Execution Test", async () => {
     const stableSwapY = new web3.eth.Contract(
       legos.curvefi.abis.yDAI_yUSDC_yUSDT_ytUSD,
@@ -35,20 +21,30 @@ contract("Test GenericExecutor", async (accounts) => {
 
     const exec = await GenericExecutor.new();
 
+    const manager = await APYManager.at(APYManagerAddresses["1"]);
+
+    const daiPool = await APYPoolToken.at(DAI_APYPoolTokenAddresses["1"]);
+    const poolOwner = await daiPool.owner();
     await hre.network.provider.request({
       method: "hardhat_impersonateAccount",
-      params: [DAI_WHALE],
+      params: [poolOwner],
     });
+    await daiPool.infiniteApprove(manager.address, { from: poolOwner });
+    const DAI = await IDetailedERC20.at(await daiPool.underlyer());
+    const daiBalance = await DAI.balanceOf(daiPool.address);
+    console.log(daiBalance);
 
-    await web3.eth.sendTransaction({
-      from: accounts[0],
-      to: DAI_WHALE,
-      value: 1e18,
-    });
+    const usdcPool = await APYPoolToken.at(USDC_APYPoolTokenAddresses["1"]);
+    await usdcPool.infiniteApprove(manager.address, { from: poolOwner });
+    const USDC = await IDetailedERC20.at(await usdcPool.underlyer());
+    const usdcBalance = await USDC.balanceOf(usdcPool.address);
+    console.log(usdcBalance);
 
-    const DAI = await IDetailedERC20.at(legos.maker.addresses.DAI);
-
-    await acquireToken(DAI_WHALE, exec.address, DAI, "1000000");
+    const usdtPool = await APYPoolToken.at(USDT_APYPoolTokenAddresses["1"]);
+    await usdtPool.infiniteApprove(manager.address, { from: poolOwner });
+    const USDT = await IDetailedERC20.at(await usdtPool.underlyer());
+    const usdtBalance = await USDT.balanceOf(usdtPool.address);
+    console.log(usdtBalance);
 
     const trx = await exec.execute([
       [
