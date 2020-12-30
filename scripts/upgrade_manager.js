bdc7e8df0c1e5db9fa78d2c4db00ba11aa005ef9
Chan-Ho Suh <chanhosuh@gmail.com>
2020-12-30 12:50:10 -0500

    Cleanup upgrade manager script

--- scripts/upgrade_manager.js
+++ scripts/upgrade_manager.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const hre = require("hardhat");
-const { artifacts, ethers, network } = require("hardhat");
-const { CHAIN_IDS, DEPLOYS_JSON } = require("../utils/constants.js");
-const { updateDeployJsons } = require("../utils/helpers.js");
+const {
+  // artifacts,
+  ethers,
+  network,
+} = hre;
+const {
+  updateDeployJsons,
+  getDeployedAddress,
+} = require("../utils/helpers.js");
 
-const PROXY_ADMIN_ADDRESSES = require(DEPLOYS_JSON["APYManagerProxyAdmin"]);
-const MANAGER_PROXY_ADDRESSES = require(DEPLOYS_JSON["APYManagerProxy"]);
-
-const ContractData = artifacts.require("APYManager");
+// const ContractData = artifacts.require("APYManager");
 
 async function main() {
   await hre.run("compile");
@@ -21,29 +24,31 @@ async function main() {
   console.log("Deployer address:", deployer);
   console.log("");
 
-  const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
-  const APYManager = await ethers.getContractFactory("APYManager");
-
-  const proxyAdmin = await ProxyAdmin.attach(
-    PROXY_ADMIN_ADDRESSES[CHAIN_IDS[NETWORK_NAME]]
+  const proxyAdminAddress = getDeployedAddress(
+    "APYManagerProxyAdmin",
+    NETWORK_NAME
   );
+  const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
+  const proxyAdmin = await ProxyAdmin.attach(proxyAdminAddress);
 
+  const APYManager = await ethers.getContractFactory("APYManager");
   const newLogic = await APYManager.deploy();
   await newLogic.deployed();
   console.log(`New Implementation Logic: ${newLogic.address}`);
 
-  const proxyAddress = MANAGER_PROXY_ADDRESSES[CHAIN_IDS[NETWORK_NAME]];
+  const proxyAddress = getDeployedAddress("APYManagerProxy", NETWORK_NAME);
 
-  const iImplementation = new ethers.utils.Interface(ContractData.abi);
-  const initData = iImplementation.encodeFunctionData("initializeUpgrade", []);
+  // const iImplementation = new ethers.utils.Interface(ContractData.abi);
+  // const initData = iImplementation.encodeFunctionData("initializeUpgrade", []);
 
   // NOTE: Select 1 of the following
-  await proxyAdmin.upgradeAndCall(proxyAddress, newLogic.address, initData);
-  // await proxyAdmin.upgrade(proxyAddress, newLogic.address)
+  // await proxyAdmin.upgradeAndCall(proxyAddress, newLogic.address, initData);
+  await proxyAdmin.upgrade(proxyAddress, newLogic.address);
 
-  let deploy_data = {};
-  deploy_data["APYManager"] = newLogic.address;
-  await updateDeployJsons(NETWORK_NAME, deploy_data);
+  const deploy_data = {
+    APYManager: newLogic.address,
+  };
+  updateDeployJsons(NETWORK_NAME, deploy_data);
 }
 
 main()
