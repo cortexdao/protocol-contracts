57a16f8177893f7766c348497485a3083974fdfb
The Architect <email@example.com>
2021-02-06 14:58:53 -0800

    utility script deploy generic executor

---
+++ scripts/deploy_generic_executor.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { ethers, network } = require("hardhat");
+const chalk = require("chalk");
+
+async function main() {
+  const NETWORK_NAME = network.name.toUpperCase();
+  console.log(`${NETWORK_NAME} selected`);
+  const signers = await ethers.getSigners();
+  const deployer = await signers[0].getAddress();
+  console.log(`Deployer: ${chalk.green(deployer)}`);
+
+  const genericExecutor = await ethers.getContractFactory("APYGenericExecutor");
+  const genericExecutorContract = await genericExecutor.deploy();
+  console.log(
+    `New Generic Executor: ${chalk.green(genericExecutorContract.address)}`
+  );
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
