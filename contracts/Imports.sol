8b7310003be434b2d7c9c152fcaada207697ca05
The Architect <email@example.com>
2021-02-07 11:51:38 -0800

    remove comment

--- contracts/Imports.sol
+++ contracts/Imports.sol
@@ -5,5 +5,4 @@ pragma solidity 0.6.11;
 // available when working from truffle console.
 import "@gnosis.pm/mock-contract/contracts/MockContract.sol";
 import "@openzeppelin/contracts/proxy/ProxyAdmin.sol";
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./interfaces/IMintableERC20.sol";
