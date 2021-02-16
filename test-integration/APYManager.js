866d6214d63778c9854d21911a196ea5f0897ec6
The Architect <email@example.com>
2021-02-16 08:33:39 -0800

    setup remaining integration tests

--- test-integration/APYManager.js
+++ test-integration/APYManager.js
@@ -291,17 +291,74 @@ contract("APYManager", async (accounts) => {
 
   describe.only("Execute and Withdraw", async () => {
     it("Test executeAndWithdraw by non owner", async () => {
+      const bad_signer = await ethers.provider.getSigner(_)
+      const bad_MANAGER = await ethers.getContractAt(APYManagerV2.abi, legos.apy.addresses.APY_MANAGER, bad_signer)
+
+      await expectRevert(bad_MANAGER.executeAndWithdraw(strategy.address,
+        [
+          ['0x75CE0E501e2E6776FcAAa514f394a88a772A8970', '0xe18b0365D5D09F394f84eE56ed29DD2d8D6Fba5f'],
+          ['100', '140']
+        ],
+        [
+          [
+            '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+            '0x095ea7b30000000000000000000000006b175474e89094c44da98b954eedeac495271d0f0000000000000000000000000000000000000000000000000000000000000064'
+          ],
+          [
+            '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
+            '0x095ea7b30000000000000000000000005d3a536e4d6dbd6114cc1ead35777bab948e36430000000000000000000000000000000000000000000000000000000000000064'
+          ]
+        ]
+      ), "revert Ownable: caller is not the owner")
     })
 
-    it("Test executeAndWithdraw by owner", async () => {
+    it.skip("Test executeAndWithdraw by owner", async () => {
+      const DAI_Contract = await ethers.getContractAt(legos.maker.abis.DAI, legos.maker.addresses.DAI)
+      const USDC_Contract = await ethers.getContractAt(legos.centre.abis.USDC_Logic, legos.centre.addresses.USDC)
+      const USDT_Contract = await ethers.getContractAt(legos.tether.abis.USDT, legos.tether.addresses.USDT)
+
+      //TODO: need to give the manager access to move funds
+
+      await Manager.executeAndWithdraw(strategy.address,
+        [
+          ['0x75CE0E501e2E6776FcAAa514f394a88a772A8970', '0xe18b0365D5D09F394f84eE56ed29DD2d8D6Fba5f'],
+          ['10', '10']
+        ],
+        [
+          [
+            '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+            '0x095ea7b30000000000000000000000006b175474e89094c44da98b954eedeac495271d0f0000000000000000000000000000000000000000000000000000000000000064'
+          ],
+          [
+            '0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643',
+            '0x095ea7b30000000000000000000000005d3a536e4d6dbd6114cc1ead35777bab948e36430000000000000000000000000000000000000000000000000000000000000064'
+          ]
+        ]
+      )
+      const stratDaiBal = await DAI_Contract.balanceOf(strategy.address)
+      const stratUsdcBal = await USDC_Contract.balanceOf(strategy.address)
+      const stratUsdtBal = await USDT_Contract.balanceOf(strategy.address)
+
+      // NOTE: DAI, USDC, and USDT funded to the account before with 10
+      assert.equal(stratDaiBal.toString(), "100")
+      assert.equal(stratUsdcBal.toString(), "140")
+      assert.equal(stratUsdtBal.toString(), "10")
     })
   })
 
   describe.only("Withdraw from Strategy", async () => {
     it("Test withdrawing from strategy by non owner", async () => {
+      const bad_signer = await ethers.provider.getSigner(_)
+      const bad_MANAGER = await ethers.getContractAt(APYManagerV2.abi, legos.apy.addresses.APY_MANAGER, bad_signer)
+      await expectRevert(bad_MANAGER.withdrawFromStrategy(strategy.address,
+        [
+          [legos.maker.addresses.DAI, legos.centre.addresses.USDC, legos.tether.addresses.USDT],
+          ['10', '10', '10']
+        ]
+      ), "revert Ownable: caller is not the owner")
     })
 
-    it("Test withdrawing from strategy by owner", async () => {
+    it.skip("Test withdrawing from strategy by owner", async () => {
     })
   })
 });
