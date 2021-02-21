d11bebb118336b2086d4604957a1e37cc36acded
The Architect <email@example.com>
2021-02-21 08:55:33 -0800

    remove receive() from APYPoolTokenV2 not V1

--- contracts/APYPoolToken.sol
+++ contracts/APYPoolToken.sol
@@ -91,6 +91,10 @@ contract APYPoolToken is
         _unpause();
     }
 
+    receive() external payable {
+        revert("DONT_SEND_ETHER");
+    }
+
     /**
      * @notice Mint corresponding amount of APT tokens for sent token amount.
      * @dev If no APT tokens have been minted yet, fallback to a fixed ratio.
