a09a9fe6dfb1d5a17751837c0d378d1172cf48a4
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-02 11:20:08 -0500

    Add natspecs

--- contracts/SequenceRegistry.sol
+++ contracts/SequenceRegistry.sol
@@ -43,6 +43,10 @@ contract SequenceRegistry is Ownable, ISequenceRegistry, IAssetAllocation {
         emit ExecutorChanged(executorAddress);
     }
 
+    /**
+     * @notice Registers a sequence for use with the `balanceOf` functionality.
+     * @dev Has O(n) time complexity, where n is the total size of `data`.
+     */
     function addSequence(
         bytes32 sequenceId,
         APYViewExecutor.Data[] memory data,
@@ -56,12 +60,20 @@ contract SequenceRegistry is Ownable, ISequenceRegistry, IAssetAllocation {
         }
     }
 
+    /**
+     * @notice Deregisters a sequence for use with the `balanceOf` functionality.
+     * @dev Has O(n) time complexity, where n is the total size of sequence data.
+     */
     function removeSequence(bytes32 sequenceId) external override onlyOwner {
         delete _sequenceData[sequenceId];
         delete _sequenceSymbols[sequenceId];
         _sequenceIds.remove(sequenceId);
     }
 
+    /**
+     * @notice Returns true/false indicating if sequence is registered.
+     * @dev Operation is O(1) in time complexity.
+     */
     function isSequenceRegistered(bytes32 sequenceId)
         public
         view
@@ -72,7 +84,18 @@ contract SequenceRegistry is Ownable, ISequenceRegistry, IAssetAllocation {
     }
 
     /**
-     * @notice Returns the list of sequenceIds.
+     * @notice Returns the list of identifiers used by the other functions
+     *         to pull asset info.
+     *
+     *         Each identifier represents a token and information on
+     *         how it is placed within the system.
+     *
+     *         Note that the list has no duplicates, but a token may have
+     *         multiplier identifiers since it may be placed in different
+     *         parts of the system.
+     *
+     * @dev Identifiers are added during strategy deployments.
+     * @return List of identifiers
      */
     function getSequenceIds()
         external
@@ -88,9 +111,13 @@ contract SequenceRegistry is Ownable, ISequenceRegistry, IAssetAllocation {
         return sequenceIds;
     }
 
-    /** @notice Returns the total balance in the system for given token.
-     *  @dev The balance is possibly aggregated from multiple contracts
-     *       holding the token.
+    /**
+     * @notice Returns the balance represented by the identifier, i.e.
+     *         the token balance held in a specific part of the system.
+     * @dev The balance may be aggregated from multiple contracts holding
+     *      the token and also may result from a series of calculations.
+     * @param sequenceId identifier for a token placed in the system
+     * @return token balance represented by the identifer
      */
     function balanceOf(bytes32 sequenceId)
         external
@@ -110,7 +137,11 @@ contract SequenceRegistry is Ownable, ISequenceRegistry, IAssetAllocation {
         return _balance;
     }
 
-    /// @notice Returns the symbol of the given token.
+    /**
+     * @notice Returns the symbol of the token represented by the identifier.
+     * @param sequenceId identifier for a token placed in the system
+     * @return the token symbol
+     */
     function symbolOf(bytes32 sequenceId)
         external
         view
