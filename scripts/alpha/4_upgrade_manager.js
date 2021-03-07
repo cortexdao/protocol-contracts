9d651f8060c389121ff1de950bf145bbad473c9f
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-07 16:04:08 -0500

    Wrap up manager upgrade

--- scripts/alpha/4_upgrade_manager.js
+++ scripts/alpha/4_upgrade_manager.js
@@ -1,3 +1,4 @@
+require("dotenv").config({ path: "./alpha.env" });
 const { argv } = require("yargs").option("gasPrice", {
   type: "number",
   description: "Gas price in gwei; omitting uses EthGasStation value",
@@ -8,14 +9,38 @@ const { ethers, network } = hre;
 const chalk = require("chalk");
 const { getDeployedAddress, getGasPrice } = require("../../utils/helpers");
 
+// eslint-disable-next-line no-unused-vars
 async function main(argv) {
   await hre.run("compile");
   const NETWORK_NAME = network.name.toUpperCase();
   console.log("");
   console.log(`${NETWORK_NAME} selected`);
   console.log("");
-  const [deployer] = await ethers.getSigners();
-  console.log("Deployer address:", deployer.address);
+
+  const MANAGER_MNEMONIC = process.env.MANAGER_MNEMONIC;
+  const managerDeployer = ethers.Wallet.fromMnemonic(MANAGER_MNEMONIC).connect(
+    ethers.provider
+  );
+  console.log("Deployer address:", managerDeployer.address);
+  /* TESTING on localhost only
+   * may need to fund the deployer while testing
+   */
+  // const [funder] = await ethers.getSigners();
+  // const fundingTrx = await funder.sendTransaction({
+  //   to: managerDeployer.address,
+  //   value: ethers.utils.parseEther("1.0"),
+  // });
+  // await fundingTrx.wait();
+
+  const balance =
+    (await ethers.provider.getBalance(managerDeployer.address)).toString() /
+    1e18;
+  console.log("ETH balance:", balance.toString());
+  console.log("");
+
+  console.log("");
+  console.log("Upgrading manager ...");
+  console.log("");
 
   const proxyAdminAddress = getDeployedAddress(
     "APYManagerProxyAdmin",
@@ -23,53 +48,64 @@ async function main(argv) {
   );
   const proxyAdmin = await ethers.getContractAt(
     "ProxyAdmin",
-    proxyAdminAddress
+    proxyAdminAddress,
+    managerDeployer
   );
   assert.strictEqual(
     await proxyAdmin.owner(),
-    deployer.address,
-    "MNEMONIC needs to be set to pool deployer."
+    managerDeployer.address,
+    "MNEMONIC needs to be set to manager deployer."
   );
 
   const proxyAddress = getDeployedAddress("APYManagerProxy", NETWORK_NAME);
-  let proxy = await ethers.getContractAt("APYManager", proxyAddress);
+  const managerV1 = await ethers.getContractAt(
+    "APYManager",
+    proxyAddress,
+    managerDeployer
+  );
+  console.log("Manager (proxy):", proxyAddress);
 
-  // Delete unneeded V1 storage
+  console.log("Deleting deprecated storage ...");
   let gasPrice = await getGasPrice(argv.gasPrice);
-  let trx = await proxy.deleteTokenAddress({ gasPrice });
-  console.log("Etherscan:", `https://etherscan.io/tx/${trx.hash}`);
+  let trx = await managerV1.deleteTokenAddresses({ gasPrice });
+  console.log("Delete token addresses:", `https://etherscan.io/tx/${trx.hash}`);
   await trx.wait();
   gasPrice = await getGasPrice(argv.gasPrice);
-  trx = await proxy.deletePoolIds({ gasPrice });
-  console.log("Etherscan:", `https://etherscan.io/tx/${trx.hash}`);
+  trx = await managerV1.deletePoolIds({ gasPrice });
+  console.log("Delete pool IDs:", `https://etherscan.io/tx/${trx.hash}`);
   await trx.wait();
+  console.log("... done.");
+  console.log("");
 
-  const APYManagerV2 = await ethers.getContractFactory("APYManagerV2");
+  console.log("Starting upgrade ...");
+  const APYManagerV2 = await ethers.getContractFactory(
+    "APYManagerV2",
+    managerDeployer
+  );
   const logicV2 = await APYManagerV2.deploy();
   console.log(
-    "Etherscan:",
+    "V2 deploy:",
     `https://etherscan.io/tx/${logicV2.deployTransaction.hash}`
   );
   await logicV2.deployed();
-  console.log(
-    `New Implementation Logic for Manager: ${chalk.green(logicV2.address)}`
-  );
+  console.log(`Manager logic V2: ${chalk.green(logicV2.address)}`);
 
   gasPrice = await getGasPrice(argv.gasPrice);
   trx = await proxyAdmin.upgrade(proxyAddress, logicV2.address, { gasPrice });
-  console.log("Etherscan:", `https://etherscan.io/tx/${trx.hash}`);
+  console.log("Upgrade:", `https://etherscan.io/tx/${trx.hash}`);
   await trx.wait();
-  console.log(
-    `${chalk.yellow("Manager")}: ${chalk.green(
-      proxyAddress
-    )}, Logic: ${chalk.green(logicV2.address)}`
-  );
+  console.log("Upgraded manager to V2.");
+  console.log("");
 
   const mAPTAddress = getDeployedAddress("APYMetaPoolToken", NETWORK_NAME);
-  proxy = await ethers.getContractAt("APYManagerV2", proxyAddress);
+  const managerV2 = await ethers.getContractAt(
+    "APYManagerV2",
+    proxyAddress,
+    managerDeployer
+  );
   gasPrice = await getGasPrice(argv.gasPrice);
-  trx = await proxy.setMetaPoolToken(mAPTAddress, { gasPrice });
-  console.log("Etherscan:", `https://etherscan.io/tx/${trx.hash}`);
+  trx = await managerV2.setMetaPoolToken(mAPTAddress, { gasPrice });
+  console.log("Set mAPT address:", `https://etherscan.io/tx/${trx.hash}`);
   await trx.wait();
 }
 
