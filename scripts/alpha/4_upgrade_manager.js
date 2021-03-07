a69380cba36f73809b41a2b89800f4a5f584e516
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-07 00:24:57 -0500

    Fixes to manager upgrade

--- scripts/alpha/4_upgrade_manager.js
+++ scripts/alpha/4_upgrade_manager.js
@@ -1,45 +1,72 @@
-require("dotenv").config();
-const { ethers, network } = require("hardhat");
+const { argv } = require("yargs").option("gasPrice", {
+  type: "number",
+  description: "Gas price in gwei; omitting uses EthGasStation value",
+});
+const hre = require("hardhat");
+const { ethers, network } = hre;
 const chalk = require("chalk");
-const legos = require("@apy-finance/defi-legos");
+const { getDeployedAddress, getGasPrice } = require("../../utils/helpers");
 
-async function main() {
+async function main(argv) {
+  await hre.run("compile");
   const NETWORK_NAME = network.name.toUpperCase();
+  console.log("");
   console.log(`${NETWORK_NAME} selected`);
-  const signers = await ethers.getSigners();
-  const deployer = await signers[0].getAddress();
-  console.log(`Deployer: ${chalk.green(deployer)}`);
+  console.log("");
+  const [deployer] = await ethers.getSigners();
+  console.log("Deployer address:", deployer.address);
 
-  const newManagerLogic = await ethers.getContractFactory("APYManager");
-  const newManagerLogicContract = await newManagerLogic.deploy();
+  const proxyAddress = getDeployedAddress("APYManagerProxy", NETWORK_NAME);
+  const proxyAdminAddress = getDeployedAddress(
+    "APYManagerProxyAdmin",
+    NETWORK_NAME
+  );
+  const proxy = await ethers.getContractAt("APYManager", proxyAddress);
 
+  // Delete unneeded V1 storage
+  let gasPrice = await getGasPrice(argv.gasPrice);
+  let trx = await proxy.deleteTokenAddress({ gasPrice });
+  console.log("Etherscan:", `https://etherscan.io/tx/${trx.hash}`);
+  await trx.wait();
+  gasPrice = await getGasPrice(argv.gasPrice);
+  trx = await proxy.deletePoolIds({ gasPrice });
+  console.log("Etherscan:", `https://etherscan.io/tx/${trx.hash}`);
+  await trx.wait();
+
+  const APYManagerV2 = await ethers.getContractFactory("APYManagerV2");
+  const logicV2 = await APYManagerV2.deploy();
   console.log(
-    `New Implementation Logic for Manager: ${chalk.green(
-      newManagerLogicContract.address
-    )}`
+    "Etherscan:",
+    `https://etherscan.io/tx/${logicV2.deployTransaction.hash}`
+  );
+  await logicV2.deployed();
+  console.log(
+    `New Implementation Logic for Manager: ${chalk.green(logicV2.address)}`
   );
 
   const ManagerAdmin = await ethers.getContractAt(
-    legos.apy.abis.APY_MANAGER_Admin,
-    legos.apy.addresses.APY_MANAGER_Admin
+    "APYManagerProxyAdmin",
+    proxyAdminAddress
   );
-
-  await ManagerAdmin.upgrade(
-    legos.apy.addresses.APY_MANAGER,
-    newManagerLogicContract.address
-  );
+  await ManagerAdmin.upgrade(proxyAddress, logicV2.address);
   console.log(
     `${chalk.yellow("Manager")}: ${chalk.green(
-      legos.apy.addresses.APY_MANAGER
-    )}, Logic: ${chalk.green(newManagerLogicContract.address)}`
+      proxyAddress
+    )}, Logic: ${chalk.green(logicV2.address)}`
   );
 }
 
 if (!module.parent) {
-  main()
-    .then(() => process.exit(0))
+  main(argv)
+    .then(() => {
+      console.log("");
+      console.log("Manager upgrade successful.");
+      console.log("");
+      process.exit(0);
+    })
     .catch((error) => {
       console.error(error);
+      console.log("");
       process.exit(1);
     });
 } else {
