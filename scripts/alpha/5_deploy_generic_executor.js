10c946aa7f5f1d816bbe7f46c0f9fa1b843607ba
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-07 16:16:34 -0500

    Wrap up generic executor deploy

--- scripts/alpha/5_deploy_generic_executor.js
+++ scripts/alpha/5_deploy_generic_executor.js
@@ -1,26 +1,71 @@
-require("dotenv").config();
-const { ethers, network } = require("hardhat");
+require("dotenv").config({ path: "./alpha.env" });
+const { argv } = require("yargs").option("gasPrice", {
+  type: "number",
+  description: "Gas price in gwei; omitting uses EthGasStation value",
+});
+const hre = require("hardhat");
+const { ethers, network } = hre;
 const chalk = require("chalk");
+const { getGasPrice } = require("../../utils/helpers");
 
-async function main() {
+// eslint-disable-next-line no-unused-vars
+async function main(argv) {
+  await hre.run("compile");
   const NETWORK_NAME = network.name.toUpperCase();
+  console.log("");
   console.log(`${NETWORK_NAME} selected`);
-  const signers = await ethers.getSigners();
-  const deployer = await signers[0].getAddress();
-  console.log(`Deployer: ${chalk.green(deployer)}`);
+  console.log("");
 
-  const genericExecutor = await ethers.getContractFactory("APYGenericExecutor");
-  const genericExecutorContract = await genericExecutor.deploy();
-  console.log(
-    `New Generic Executor: ${chalk.green(genericExecutorContract.address)}`
+  const MANAGER_MNEMONIC = process.env.MANAGER_MNEMONIC;
+  const managerDeployer = ethers.Wallet.fromMnemonic(MANAGER_MNEMONIC).connect(
+    ethers.provider
   );
+  console.log("Deployer address:", managerDeployer.address);
+  /* TESTING on localhost only
+   * may need to fund the deployer while testing
+   */
+  // const [funder] = await ethers.getSigners();
+  // const fundingTrx = await funder.sendTransaction({
+  //   to: managerDeployer.address,
+  //   value: ethers.utils.parseEther("1.0"),
+  // });
+  // await fundingTrx.wait();
+
+  const balance =
+    (await ethers.provider.getBalance(managerDeployer.address)).toString() /
+    1e18;
+  console.log("ETH balance:", balance.toString());
+  console.log("");
+
+  console.log("");
+  console.log("Deploying generic executor ...");
+  console.log("");
+  const APYGenericExecutor = await ethers.getContractFactory(
+    "APYGenericExecutor",
+    managerDeployer
+  );
+  let gasPrice = getGasPrice(argv.gasPrice);
+  const genericExecutor = await APYGenericExecutor.deploy({ gasPrice });
+  console.log(
+    "Deploy:",
+    `https://etherscan.io/tx/${genericExecutor.deployTransaction.hash}`
+  );
+  await genericExecutor.deployed();
+  console.log("Generic Executor", chalk.green(genericExecutor.address));
+  console.log("");
 }
 
 if (!module.parent) {
-  main()
-    .then(() => process.exit(0))
+  main(argv)
+    .then(() => {
+      console.log("");
+      console.log("Executor deployment successful.");
+      console.log("");
+      process.exit(0);
+    })
     .catch((error) => {
       console.error(error);
+      console.log("");
       process.exit(1);
     });
 } else {
