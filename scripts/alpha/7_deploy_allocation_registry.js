1a78aed5368233a217cc570028e55cfe5379ee85
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-07 17:14:38 -0500

    Wrap up alloc registry deploy

--- scripts/alpha/7_deploy_allocation_registry.js
+++ scripts/alpha/7_deploy_allocation_registry.js
@@ -8,17 +8,20 @@
  *
  * $ HARDHAT_NETWORK=<network name> node run scripts/<script filename> --arg1=val1 --arg2=val2
  */
+require("dotenv").config({ path: "./alpha.env" });
 const { argv } = require("yargs").option("gasPrice", {
   type: "number",
   description: "Gas price in gwei; omitting uses EthGasStation value",
 });
 const hre = require("hardhat");
 const { ethers, network } = require("hardhat");
+const assert = require("assert");
+const chalk = require("chalk");
 const {
   getGasPrice,
   updateDeployJsons,
-  getAggregatorAddress,
-} = require("../utils/helpers");
+  getDeployedAddress,
+} = require("../../utils/helpers");
 
 // eslint-disable-next-line no-unused-vars
 async function main(argv) {
@@ -28,60 +31,65 @@ async function main(argv) {
   console.log(`${NETWORK_NAME} selected`);
   console.log("");
 
-  const signers = await ethers.getSigners();
-  const deployer = await signers[0].getAddress();
-  console.log("Deployer address:", deployer);
+  const ALLOCATION_REGISTRY_MNEMONIC = process.env.ALLOCATION_REGISTRY_MNEMONIC;
+  const registryDeployer = ethers.Wallet.fromMnemonic(
+    ALLOCATION_REGISTRY_MNEMONIC
+  ).connect(ethers.provider);
+  console.log("Deployer address:", registryDeployer.address);
+  /* TESTING on localhost only
+   * need to fund as there is no ETH on Mainnet for the deployer
+   */
+  const [funder] = await ethers.getSigners();
+  const fundingTrx = await funder.sendTransaction({
+    to: registryDeployer.address,
+    value: ethers.utils.parseEther("1.0"),
+  });
+  await fundingTrx.wait();
 
   const balance =
-    (await ethers.provider.getBalance(deployer)).toString() / 1e18;
+    (await ethers.provider.getBalance(registryDeployer.address)).toString() /
+    1e18;
   console.log("ETH balance:", balance.toString());
   console.log("");
 
   console.log("");
-  console.log("Deploying ...");
+  console.log("Deploying AssetAllocationRegistry ...");
   console.log("");
 
   const AssetAllocationRegistry = await ethers.getContractFactory(
-    "AssetAllocationRegistry"
+    "AssetAllocationRegistry",
+    registryDeployer
   );
 
-  let deploy_data = {};
-
+  const managerAddress = getDeployedAddress("APYManagerProxy", NETWORK_NAME);
   let gasPrice = await getGasPrice(argv.gasPrice);
-  const registry = await AssetAllocationRegistry.deploy({ gasPrice });
-  await registry.deployed();
-  deploy_data["AssetAllocationRegistry"] = registry.address;
-  console.log(`AssetAllocationRegistry: ${registry.address}`);
+  const registry = await AssetAllocationRegistry.deploy(managerAddress, {
+    gasPrice,
+  });
   console.log(
-    "Etherscan:",
+    "Deploy:",
     `https://etherscan.io/tx/${registry.deployTransaction.hash}`
   );
+  await registry.deployed();
+  console.log("AssetAllocationRegistry:", chalk.green(registry.address));
   console.log("");
+  assert.strictEqual(await registry.owner(), registryDeployer.address);
 
+  const deploy_data = {
+    AssetAllocationRegistry: registry.address,
+  };
   updateDeployJsons(NETWORK_NAME, deploy_data);
 
   if (["KOVAN", "MAINNET"].includes(NETWORK_NAME)) {
     console.log("");
     console.log("Verifying on Etherscan ...");
-    await ethers.provider.waitForTransaction(proxy.deployTransaction.hash, 5); // wait for Etherscan to catch up
-    await hre.run("verify:verify", {
-      address: proxy.address,
-      constructorArguments: [
-        logic.address,
-        registry.address,
-        tvlAggAddress,
-        ethUsdAggAddress,
-        aggStalePeriod.toString(),
-      ],
-      // to avoid the "More than one contract was found to match the deployed bytecode."
-      // with proxy contracts that only differ in constructors but have the same bytecode
-      contract: "contracts/APYMetaPoolTokenProxy.sol:APYMetaPoolTokenProxy",
-    });
-    await hre.run("verify:verify", {
-      address: logic.address,
-    });
+    await ethers.provider.waitForTransaction(
+      registry.deployTransaction.hash,
+      5
+    ); // wait for Etherscan to catch up
     await hre.run("verify:verify", {
       address: registry.address,
+      constructorArguments: [managerAddress],
     });
     console.log("");
   }
