7a85cfd94ac42711cfcb2bb919b70daa7cd364f1
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-12 09:37:01 -0500

    Add CashflowPoint entity

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -10,6 +10,7 @@ import {
   TotalEthValueLocked,
   PoolTotalValue,
   Cashflow,
+  CashflowPoint,
   Transfer,
   User,
   Pool,
@@ -18,26 +19,28 @@ import {
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 export function handleDepositedAPT(event: DepositedAPT): void {
-  let tvl = new TotalEthValueLocked(
-    event.params.sender.toHexString() +
-      event.block.timestamp.toString() +
-      event.logIndex.toString() +
-      event.transaction.hash.toHexString()
+  const sender = event.params.sender;
+  const timestamp = event.block.timestamp;
+  const poolAddress = event.address;
+  const logIndex = event.logIndex;
+  const transactionHash = event.transaction.hash;
+
+  const tvl = new TotalEthValueLocked(
+    sender.toHexString() +
+      timestamp.toString() +
+      logIndex.toString() +
+      transactionHash.toHexString()
   );
-  tvl.timestamp = event.block.timestamp;
-  tvl.sequenceNumber =
-    event.block.timestamp * BigInt.fromI32(100000000) + event.logIndex;
-  tvl.poolAddress = event.address;
+  tvl.timestamp = timestamp;
+  tvl.sequenceNumber = timestamp * BigInt.fromI32(100000000) + logIndex;
+  tvl.poolAddress = poolAddress;
   tvl.totalEthValueLocked = event.params.totalEthValueLocked;
   tvl.save();
 
-  const poolAddress = event.address;
   const contract = APYPoolToken.bind(poolAddress);
 
-  let ptv = new PoolTotalValue(
-    poolAddress.toHexString() + event.block.timestamp.toString()
-  );
-  ptv.timestamp = event.block.timestamp;
+  const ptv = new PoolTotalValue(poolAddress.toHexString() + timestamp.toString());
+  ptv.timestamp = timestamp;
   ptv.poolAddress = poolAddress;
   ptv.totalEthValueLocked = event.params.totalEthValueLocked;
   ptv.aptSupply = contract.totalSupply();
@@ -47,72 +50,90 @@ export function handleDepositedAPT(event: DepositedAPT): void {
   ptv.save();
 
   // Cashflow entity
-  const user = event.params.sender;
-  const cashflowId =
+  const user = sender;
+  const cashflowId = poolAddress.toHexString() + user.toHexString();
+  const cashflowPointId =
     poolAddress.toHexString() +
     user.toHexString() +
-    event.block.timestamp.toString();
-  const cashflow = Cashflow.load(cashflowId) || new Cashflow(cashflowId);
-
-  cashflow.timestamp = event.block.timestamp;
-  cashflow.userAddress = user;
-  cashflow.poolAddress = poolAddress;
-  cashflow.userAptBalance = contract.balanceOf(user);
-
-  const previousTotal = cashflow.total || BigInt.fromI32(0);
-  cashflow.total = previousTotal.minus(event.params.tokenAmount);
-
-  cashflow.save();
-}
-
-export function handleRedeemedAPT(event: RedeemedAPT): void {
-  let tvl = new TotalEthValueLocked(
-    event.params.sender.toHexString() +
-      event.block.timestamp.toString() +
-      event.logIndex.toString() +
-      event.transaction.hash.toHexString()
-  );
-  tvl.timestamp = event.block.timestamp;
-  tvl.sequenceNumber =
-    event.block.timestamp * BigInt.fromI32(100000000) + event.logIndex;
-  tvl.poolAddress = event.address;
-  tvl.totalEthValueLocked = event.params.totalEthValueLocked;
-  tvl.save();
-
-  const poolAddress = event.address;
-  const contract = APYPoolToken.bind(poolAddress);
-
-  let ptv = new PoolTotalValue(
-    poolAddress.toHexString() + event.block.timestamp.toString()
-  );
-  ptv.timestamp = event.block.timestamp;
-  ptv.poolAddress = poolAddress;
-  ptv.totalEthValueLocked = event.params.totalEthValueLocked;
-  ptv.aptSupply = contract.totalSupply();
-  if (!ptv.aptSupply.isZero())
-    ptv.valuePerShare =
-      new BigDecimal(ptv.totalEthValueLocked) / new BigDecimal(ptv.aptSupply);
-  ptv.save();
-
-  // Cashflow entity
-  const user = event.params.sender;
-  const cashflowId =
-    poolAddress.toHexString() +
-    user.toHexString() +
-    event.block.timestamp.toString();
+    timestamp.toString();
   let cashflow = Cashflow.load(cashflowId);
   if (cashflow == null) {
     cashflow = new Cashflow(cashflowId);
     cashflow.total = BigInt.fromI32(0);
-    cashflow.timestamp = event.block.timestamp;
     cashflow.userAddress = user;
     cashflow.poolAddress = poolAddress;
   }
 
-  cashflow.userAptBalance = contract.balanceOf(user);
-  cashflow.total = cashflow.total.plus(event.params.redeemedTokenAmount);
+  const cashflowPoint = new CashflowPoint(cashflowPointId);
+  cashflowPoint.userAddress = user;
+  cashflowPoint.poolAddress = poolAddress;
+  cashflowPoint.timestamp = timestamp;
+  cashflowPoint.userAptBalance = contract.balanceOf(user);
+
+  cashflow.total = cashflow.total.minus(event.params.tokenAmount);
+  cashflowPoint.total = cashflow.total;
 
   cashflow.save();
+  cashflowPoint.save();
+}
+
+export function handleRedeemedAPT(event: RedeemedAPT): void {
+  const sender = event.params.sender;
+  const timestamp = event.block.timestamp;
+  const poolAddress = event.address;
+  const logIndex = event.logIndex;
+  const transactionHash = event.transaction.hash;
+
+  const tvl = new TotalEthValueLocked(
+    event.params.sender.toHexString() +
+      timestamp.toString() +
+      logIndex.toString() +
+      transactionHash.toHexString()
+  );
+  tvl.timestamp = event.block.timestamp;
+  tvl.sequenceNumber = timestamp * BigInt.fromI32(100000000) + logIndex;
+  tvl.poolAddress = poolAddress;
+  tvl.totalEthValueLocked = event.params.totalEthValueLocked;
+  tvl.save();
+
+  const contract = APYPoolToken.bind(poolAddress);
+
+  const ptv = new PoolTotalValue(poolAddress.toHexString() + timestamp.toString());
+  ptv.timestamp = timestamp;
+  ptv.poolAddress = poolAddress;
+  ptv.totalEthValueLocked = event.params.totalEthValueLocked;
+  ptv.aptSupply = contract.totalSupply();
+  if (!ptv.aptSupply.isZero())
+    ptv.valuePerShare =
+      new BigDecimal(ptv.totalEthValueLocked) / new BigDecimal(ptv.aptSupply);
+  ptv.save();
+
+  // Cashflow entity
+  const user = event.params.sender;
+  const cashflowId =poolAddress.toHexString() + user.toHexString();
+  const cashflowPointId =
+    poolAddress.toHexString() +
+    user.toHexString() +
+    timestamp.toString();
+  let cashflow = Cashflow.load(cashflowId);
+  if (cashflow == null) {
+    cashflow = new Cashflow(cashflowId);
+    cashflow.total = BigInt.fromI32(0);
+    cashflow.userAddress = user;
+    cashflow.poolAddress = poolAddress;
+  }
+
+  const cashflowPoint = new CashflowPoint(cashflowPointId);
+  cashflowPoint.userAddress = user;
+  cashflowPoint.poolAddress = poolAddress;
+  cashflowPoint.timestamp = timestamp;
+  cashflowPoint.userAptBalance = contract.balanceOf(user);
+
+  cashflow.total = cashflow.total.plus(event.params.redeemedTokenAmount);
+  cashflowPoint.total = cashflow.total;
+
+  cashflow.save();
+  cashflowPoint.save();
 }
 
 export function handleTransfer(event: TransferEvent): void {
