d04d749a720b5f9cc6983367d2acff5778788a54
Chan-Ho Suh <chanhosuh@gmail.com>
2021-03-17 18:26:42 -0400

    Add allowance check; refactor a bit, natspecs

--- contracts/PoolManager.sol
+++ contracts/PoolManager.sol
@@ -136,11 +136,49 @@ contract PoolManager is Initializable, OwnableUpgradeSafe, IAccountFunder {
     function initializeUpgrade() external virtual onlyAdmin {}
 
     /**
+     * @dev Throws if called by any account other than the proxy admin.
+     */
+    modifier onlyAdmin() {
+        require(msg.sender == proxyAdmin, "ADMIN_ONLY");
+        _;
+    }
+
+    function setAdminAddress(address adminAddress) public onlyOwner {
+        require(adminAddress != address(0), "INVALID_ADMIN");
+        proxyAdmin = adminAddress;
+        emit AdminChanged(adminAddress);
+    }
+
+    /// @dev Allow contract to receive Ether.
+    receive() external payable {} // solhint-disable-line no-empty-blocks
+
+    function setMetaPoolToken(address payable _mApt) public onlyOwner {
+        require(Address.isContract(_mApt), "INVALID_ADDRESS");
+        mApt = MetaPoolToken(_mApt);
+    }
+
+    function setAddressRegistry(address _addressRegistry) public onlyOwner {
+        require(Address.isContract(_addressRegistry), "INVALID_ADDRESS");
+        addressRegistry = IAddressRegistry(_addressRegistry);
+    }
+
+    function setAccountFactory(address _accountFactory) public onlyOwner {
+        require(Address.isContract(_accountFactory), "INVALID_ADDRESS");
+        accountFactory = IAccountFactory(_accountFactory);
+    }
+
+    /**
      * @notice Fund Account contract and register an asset allocation
      * @param accountId The Account contract ID
      * @param poolAmounts Specifies the PoolToken contracts to pull from and
      * the amounts to pull.
-     * See APYManager._fundAccount.
+     *
+     * @notice PoolAmount example (pulls ~$1 from each pool to the Account):
+     *      [
+     *          { poolId: "daiPool", amount: "1000000000000" },
+     *          { poolId: "usdcPool", amount: "1000000" },
+     *          { poolId: "usdtPool", amount: "1000000" },
+     *      ]
      */
     function fundAccount(
         bytes32 accountId,
@@ -171,114 +209,10 @@ contract PoolManager is Initializable, OwnableUpgradeSafe, IAccountFunder {
     }
 
     /**
-     * @notice Move capital from an Account to the PoolToken contracts
-     * @param accountId The Account contract ID
-     * @param poolAmounts Specifies the PoolToken contracts to push to and
-     * the amounts to push.
-     * See APYManager._withdrawFromAccount.
-     */
-    function withdrawFromAccount(
-        bytes32 accountId,
-        IAccountFunder.PoolAmount[] memory poolAmounts
-    ) external override onlyOwner {
-        address accountAddress = accountFactory.getAccount(accountId);
-        require(accountAddress != address(0), "INVALID_ACCOUNT");
-        _withdrawFromAccount(accountAddress, poolAmounts);
-    }
-
-    /**
-     * @notice Move capital from PoolTokenV2 contracts to an Account
-     * @param account The Account contract ID
-     * @param pools the pools to pull from
-     * @param amounts the amounts to pull from pools
-     *
-     * @notice AccountAllocation example (pulls ~$1 from each pool):
-     *      {
-     *          poolIds: ["daiPool", "usdcPool", "usdtPool"],
-     *          amounts: ["1000000000000", "1000000", "1000000"]
-     *      }
-     */
-    function _fundAccount(
-        address account,
-        PoolTokenV2[] memory pools,
-        uint256[] memory amounts
-    ) internal {
-        uint256[] memory mintAmounts = new uint256[](pools.length);
-        for (uint256 i = 0; i < pools.length; i++) {
-            PoolTokenV2 pool = pools[i];
-            uint256 poolAmount = amounts[i];
-            IDetailedERC20 underlyer = pool.underlyer();
-
-            uint256 tokenPrice = pool.getUnderlyerPrice();
-            uint8 decimals = underlyer.decimals();
-            uint256 mintAmount =
-                mApt.calculateMintAmount(poolAmount, tokenPrice, decimals);
-            mintAmounts[i] = mintAmount;
-
-            underlyer.safeTransferFrom(address(pool), account, poolAmount);
-        }
-        // MUST do the actual minting after calculating *all* mint amounts,
-        // otherwise due to Chainlink not updating during a transaction,
-        // the totalSupply will change while TVL doesn't.
-        //
-        // Using the pre-mint TVL and totalSupply gives the same answer
-        // as using post-mint values.
-        for (uint256 i = 0; i < pools.length; i++) {
-            mApt.mint(address(pools[i]), mintAmounts[i]);
-        }
-    }
-
-    /**
-     * @notice Move capital from an Account to the PoolToken contracts
-     * @param account The Account contract ID
-     * @param poolAmounts Specifies the PoolToken contracts to push to and
-     * the amounts to push.
-     *
-     * @notice AccountAllocation example (pushes ~$1 to each pool from the Account):
-     *      {
-     *          poolIds: ["daiPool", "usdcPool", "usdtPool"],
-     *          amounts: ["1000000000000", "1000000", "1000000"]
-     *      }
-     */
-    function _withdrawFromAccount(
-        address account,
-        IAccountFunder.PoolAmount[] memory poolAmounts
-    ) internal {
-        (PoolTokenV2[] memory pools, uint256[] memory amounts) =
-            _getPoolsAndAmounts(poolAmounts);
-        uint256[] memory burnAmounts = new uint256[](pools.length);
-        for (uint256 i = 0; i < pools.length; i++) {
-            PoolTokenV2 pool = pools[i];
-            uint256 amountToSend = amounts[i];
-            IDetailedERC20 underlyer = pool.underlyer();
-
-            uint256 tokenPrice = pool.getUnderlyerPrice();
-            uint8 decimals = underlyer.decimals();
-            uint256 burnAmount =
-                mApt.calculateMintAmount(amountToSend, tokenPrice, decimals);
-            burnAmounts[i] = burnAmount;
-
-            underlyer.safeTransferFrom(account, address(pool), amountToSend);
-        }
-        // MUST do the actual burning after calculating *all* burn amounts,
-        // otherwise due to Chainlink not updating during a transaction,
-        // the totalSupply will change while TVL doesn't.
-        //
-        // Using the pre-burn TVL and totalSupply gives the same answer
-        // as using post-burn values.
-        for (uint256 i = 0; i < pools.length; i++) {
-            mApt.burn(address(pools[i]), burnAmounts[i]);
-        }
-    }
-
-    /**
-     * @notice Register a new asset allocation
-     * @notice When capital is routed to new protocols, asset allocations are
-     * registered to return the balances of underlying assets so the TVL can be
-     * computed.
-     * @param pools The array of asset allocations to calculate the TVL of
-     * new assets stored in the Account contract.
-     *
+     * @notice Register each pool underlyer's allocation for the given account
+     *         so that the TVL computation will pick up the account's balance.
+     * @param account the address holding the transferred underlyers
+     * @param pools pools whose underlyers need to be registered
      * @notice AssetAllocation example (gets the DAI balance of the account):
      *      {
      *          sequenceId: "daiBalance",
@@ -316,36 +250,118 @@ contract PoolManager is Initializable, OwnableUpgradeSafe, IAccountFunder {
         }
     }
 
-    function setAdminAddress(address adminAddress) public onlyOwner {
-        require(adminAddress != address(0), "INVALID_ADMIN");
-        proxyAdmin = adminAddress;
-        emit AdminChanged(adminAddress);
+    /**
+     * @notice Move capital from PoolTokenV2 contracts to an Account
+     * @param account The Account contract ID
+     * @param pools the pools to pull from
+     * @param amounts the amounts to pull from pools
+     */
+    function _fundAccount(
+        address account,
+        PoolTokenV2[] memory pools,
+        uint256[] memory amounts
+    ) internal {
+        uint256[] memory mintAmounts = new uint256[](pools.length);
+        for (uint256 i = 0; i < pools.length; i++) {
+            PoolTokenV2 pool = pools[i];
+            uint256 poolAmount = amounts[i];
+            IDetailedERC20 underlyer = pool.underlyer();
+
+            uint256 tokenPrice = pool.getUnderlyerPrice();
+            uint8 decimals = underlyer.decimals();
+            uint256 mintAmount =
+                mApt.calculateMintAmount(poolAmount, tokenPrice, decimals);
+            mintAmounts[i] = mintAmount;
+
+            underlyer.safeTransferFrom(address(pool), account, poolAmount);
+        }
+        // MUST do the actual minting after calculating *all* mint amounts,
+        // otherwise due to Chainlink not updating during a transaction,
+        // the totalSupply will change while TVL doesn't.
+        //
+        // Using the pre-mint TVL and totalSupply gives the same answer
+        // as using post-mint values.
+        for (uint256 i = 0; i < pools.length; i++) {
+            mApt.mint(address(pools[i]), mintAmounts[i]);
+        }
     }
 
     /**
-     * @dev Throws if called by any account other than the proxy admin.
+     * @notice Move capital from an Account to the PoolToken contracts
+     * @param accountId The Account contract ID
+     * @param poolAmounts Specifies the PoolToken contracts to push to and
+     * the amounts to push.
+     *
+     * @notice PoolAmount example (pushes ~$1 to each pool from the Account):
+     *      [
+     *          { poolId: "daiPool", amount: "1000000000000" },
+     *          { poolId: "usdcPool", amount: "1000000" },
+     *          { poolId: "usdtPool", amount: "1000000" },
+     *      ]
      */
-    modifier onlyAdmin() {
-        require(msg.sender == proxyAdmin, "ADMIN_ONLY");
-        _;
+    function withdrawFromAccount(
+        bytes32 accountId,
+        IAccountFunder.PoolAmount[] memory poolAmounts
+    ) external override onlyOwner {
+        address accountAddress = accountFactory.getAccount(accountId);
+        require(accountAddress != address(0), "INVALID_ACCOUNT");
+        (PoolTokenV2[] memory pools, uint256[] memory amounts) =
+            _getPoolsAndAmounts(poolAmounts);
+        _checkManagerAllowances(accountAddress, pools, amounts);
+        _withdrawFromAccount(accountAddress, pools, amounts);
     }
 
-    /// @dev Allow contract to receive Ether.
-    receive() external payable {} // solhint-disable-line no-empty-blocks
-
-    function setMetaPoolToken(address payable _mApt) public onlyOwner {
-        require(Address.isContract(_mApt), "INVALID_ADDRESS");
-        mApt = MetaPoolToken(_mApt);
+    /**
+     * @dev Revert if pool manager doesn't have enough allowance to transfer
+     *      the pool's underlyer from the given account.
+     */
+    function _checkManagerAllowances(
+        address account,
+        PoolTokenV2[] memory pools,
+        uint256[] memory amounts
+    ) internal view {
+        for (uint256 i = 0; i < pools.length; i++) {
+            IDetailedERC20 underlyer = pools[i].underlyer();
+            uint256 allowance = underlyer.allowance(account, address(this));
+            require(amounts[i] <= allowance, "INSUFFICIENT_ALLOWANCE");
+        }
     }
 
-    function setAddressRegistry(address _addressRegistry) public onlyOwner {
-        require(Address.isContract(_addressRegistry), "INVALID_ADDRESS");
-        addressRegistry = IAddressRegistry(_addressRegistry);
-    }
+    /**
+     * @notice Move capital from an Account to the PoolToken contracts
+     * @param account The Account contract ID
+     * @param pools the pools to pull from
+     * @param amounts the amounts to pull from pools
+     *
+     */
+    function _withdrawFromAccount(
+        address account,
+        PoolTokenV2[] memory pools,
+        uint256[] memory amounts
+    ) internal {
+        uint256[] memory burnAmounts = new uint256[](pools.length);
+        for (uint256 i = 0; i < pools.length; i++) {
+            PoolTokenV2 pool = pools[i];
+            uint256 amountToSend = amounts[i];
+            IDetailedERC20 underlyer = pool.underlyer();
 
-    function setAccountFactory(address _accountFactory) public onlyOwner {
-        require(Address.isContract(_accountFactory), "INVALID_ADDRESS");
-        accountFactory = IAccountFactory(_accountFactory);
+            uint256 tokenPrice = pool.getUnderlyerPrice();
+            uint8 decimals = underlyer.decimals();
+            uint256 burnAmount =
+                mApt.calculateMintAmount(amountToSend, tokenPrice, decimals);
+            burnAmounts[i] = burnAmount;
+
+            underlyer.safeTransferFrom(account, address(pool), amountToSend);
+        }
+        // MUST do the actual burning after calculating *all* burn amounts,
+        // otherwise due to Chainlink not updating during a transaction,
+        // the totalSupply will change while TVL doesn't.
+        //
+        // Using the pre-burn TVL and totalSupply gives the same answer
+        // as using post-burn values.
+        for (uint256 i = 0; i < pools.length; i++) {
+            mApt.burn(address(pools[i]), burnAmounts[i]);
+        }
     }
 
     /// @notice Return the list of pool IDs.
