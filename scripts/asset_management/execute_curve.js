616eecfb26a96c6a7d6c41e769aa6db4c54e31a4
The Architect <email@example.com>
2021-04-07 16:23:25 -0700

    clean asset manager curve

--- scripts/asset_management/execute_curve.js
+++ scripts/asset_management/execute_curve.js
@@ -1,52 +1,27 @@
 #!/usr/bin/env node
-/**
- * Command to run script:
- *
- * $ HARDHAT_NETWORK=localhost node scripts/1_deployments.js
- *
- * You can modify the script to handle command-line args and retrieve them
- * through the `argv` object.  Values are passed like so:
- *
- * $ HARDHAT_NETWORK=localhost node scripts/1_deployments.js --arg1=val1 --arg2=val2
- *
- * Remember, you should have started the forked mainnet locally in another terminal:
- *
- * $ MNEMONIC='' yarn fork:mainnet
- */
-const { argv } = require("yargs");
 const hre = require("hardhat");
 const { ethers, network, artifacts } = hre;
 const { MAX_UINT256 } = require("../../utils/helpers");
+const { program } = require("commander");
+
 const {
   getAccountManager,
   getStrategyAccountInfo,
   getStablecoins,
 } = require("./utils");
-const { console } = require("./utils");
+
+program.requiredOption(
+  "-s, --stableSwap <string>",
+  "3Pool Address",
+  "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7"
+);
 
 // eslint-disable-next-line no-unused-vars
-async function main(argv) {
-  await hre.run("compile");
+async function main(options) {
   const networkName = network.name.toUpperCase();
-  console.log("");
-  console.log(`${networkName} selected`);
-  console.log("");
-
-  const [deployer] = await ethers.getSigners();
-  console.log("Deployer address:", deployer.address);
-
   const accountManager = await getAccountManager(networkName);
-  console.logAddress("AccountManager", accountManager.address);
-
   const [accountId, accountAddress] = await getStrategyAccountInfo(networkName);
-  console.logAddress("Strategy account", accountAddress);
-
   const stablecoins = await getStablecoins(networkName);
-
-  console.log("");
-  console.log("Executing ...");
-  console.log("");
-
   const ifaceERC20 = new ethers.utils.Interface(
     artifacts.require("IDetailedERC20").abi
   );
@@ -55,16 +30,9 @@ async function main(argv) {
   );
 
   // 3Pool addresses:
-  const STABLE_SWAP_ADDRESS = "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7";
-  const LP_TOKEN_ADDRESS = "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490";
+  const STABLE_SWAP_ADDRESS = options.stableSwap;
 
   // deposit into liquidity pool
-  const lpToken = await ethers.getContractAt(
-    "IDetailedERC20",
-    LP_TOKEN_ADDRESS
-  );
-  let lpBalance = await lpToken.balanceOf(accountAddress);
-  console.log("LP balance (before):", lpBalance.toString());
   const approveStableSwap = ifaceERC20.encodeFunctionData(
     "approve(address,uint256)",
     [STABLE_SWAP_ADDRESS, MAX_UINT256]
@@ -82,28 +50,23 @@ async function main(argv) {
     [STABLE_SWAP_ADDRESS, stableSwapAddLiquidity], // deposit DAI into Curve 3pool
   ];
   await accountManager.execute(accountId, executionSteps, []);
-
-  lpBalance = await lpToken.balanceOf(accountAddress);
-  console.log("LP balance (after):", lpBalance.toString());
-
-  console.logDone();
-
-  /****************************************/
 }
 
 if (!module.parent) {
-  main(argv)
-    .then(() => {
-      console.log("");
-      console.log("Execution successful.");
-      console.log("");
+  program.parse(process.argv);
+  const options = program.opts();
+  main(options)
+    .then((result) => {
+      if (!(typeof result === "string" || result instanceof Buffer)) {
+        process.exit(1);
+      }
+      process.stdout.write(result);
       process.exit(0);
     })
-    .catch((error) => {
-      console.error(error);
-      console.log("");
+    .catch(() => {
       process.exit(1);
     });
 } else {
+  // if importing in another script
   module.exports = main;
 }
