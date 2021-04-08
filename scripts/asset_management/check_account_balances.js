7f13899e6fb80dbe8805312b99fdf459a719915f
The Architect <email@example.com>
2021-04-07 17:53:33 -0700

    check account balances script

--- scripts/asset_management/check_account_balances.js
+++ scripts/asset_management/check_account_balances.js
@@ -1,67 +1,61 @@
 #!/usr/bin/env node
-/**
- * Command to run script:
- *
- * $ HARDHAT_NETWORK=localhost node scripts/1_deployments.js
- *
- * You can modify the script to handle command-line args and retrieve them
- * through the `argv` object.  Values are passed like so:
- *
- * $ HARDHAT_NETWORK=localhost node scripts/1_deployments.js --arg1=val1 --arg2=val2
- *
- * Remember, you should have started the forked mainnet locally in another terminal:
- *
- * $ MNEMONIC='' yarn fork:mainnet
- */
-const { argv } = require("yargs");
 const hre = require("hardhat");
-const { ethers, network } = hre;
+const { network, ethers } = hre;
+const { program } = require("commander");
 const { commify, formatUnits } = require("../../utils/helpers");
-const {
-  getAccountManager,
-  getStrategyAccountInfo,
-  getStablecoins,
-} = require("./utils");
+const { getStrategyAccountInfo } = require("./utils");
 const { console } = require("./utils");
 
+program.requiredOption(
+  "-t, --tokenAddresses <items>",
+  "comma separated list of token addresses",
+  commaSeparatedList,
+  [
+    "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490",
+  ]
+);
+
 // eslint-disable-next-line no-unused-vars
-async function main(argv) {
-  await hre.run("compile");
+async function checkBalances(addresses) {
   const NETWORK_NAME = network.name.toUpperCase();
-  console.log("");
-  console.log(`${NETWORK_NAME} selected`);
-  console.log("");
-
-  const [deployer] = await ethers.getSigners();
-  console.log("Deployer address:", deployer.address);
-
-  const accountManager = await getAccountManager(NETWORK_NAME);
-  console.logAddress("AccountManager", accountManager.address);
-
   const [, accountAddress] = await getStrategyAccountInfo(NETWORK_NAME);
-
-  const stablecoins = await getStablecoins(NETWORK_NAME);
-
-  const daiToken = stablecoins["DAI"];
-  const accDaiBal = await daiToken.balanceOf(accountAddress);
-  console.log(`DAI Balance: ${commify(formatUnits(accDaiBal, 18))}`);
-
-  const usdcToken = stablecoins["USDC"];
-  const accUsdcBal = await usdcToken.balanceOf(accountAddress);
-  console.log(`USDC Balance: ${commify(formatUnits(accUsdcBal, 6))}`);
-
-  const usdtToken = stablecoins["USDT"];
-  const accUsdtBal = await usdtToken.balanceOf(accountAddress);
-  console.log(`USDT Balance: ${commify(formatUnits(accUsdtBal, 6))}`);
+  for (let i = 0; i < addresses.length; i++) {
+    const token = await ethers.getContractAt("IDetailedERC20", addresses[i]);
+    const sym = await token.symbol();
+    const balance = await token.balanceOf(accountAddress);
+    const decimals = await token.decimals();
+    console.log(
+      `${sym} Balance: ${commify(formatUnits(balance, decimals))}, ${balance}`
+    );
+  }
 }
 
+// eslint-disable-next-line no-unused-vars
+function commaSeparatedList(value, dummyPrevious) {
+  return value.split(",");
+}
+
+async function main(options) {
+  await checkBalances(options.tokenAddresses);
+}
 if (!module.parent) {
-  main(argv)
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
+  program.parse(process.argv);
+  const options = program.opts();
+  main(options)
+    .then((result) => {
+      if (!(typeof result === "string" || result instanceof Buffer)) {
+        process.exit(1);
+      }
+      process.stdout.write(result);
+      process.exit(0);
+    })
+    .catch(() => {
       process.exit(1);
     });
 } else {
-  module.exports = main;
+  // if importing in another script
+  module.exports = checkBalances;
 }
