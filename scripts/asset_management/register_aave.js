e4bb49d4962fd05a62b6ca81ff9c0bf8eae77215
opz <will.shahda@gmail.com>
2021-04-08 22:07:44 -0400

    Refactor Aave register script

--- scripts/asset_management/register_aave.js
+++ scripts/asset_management/register_aave.js
@@ -1,86 +1,68 @@
 #!/usr/bin/env node
-/**
- * Command to run script:
- *
- * $ HARDHAT_NETWORK=localhost node scripts/1_deployments.js
- *
- * You can modify the script to handle command-line args and retrieve them
- * through the `argv` object.  Values are passed like so:
- *
- * $ HARDHAT_NETWORK=localhost node scripts/1_deployments.js --arg1=val1 --arg2=val2
- *
- * Remember, you should have started the forked mainnet locally in another terminal:
- *
- * $ MNEMONIC='' yarn fork:mainnet
- */
-const { argv } = require("yargs");
-const hre = require("hardhat");
-const { ethers, network } = require("hardhat");
-const {
-  getStrategyAccountInfo,
-  getTvlManager,
-  getAddressRegistry,
-} = require("./utils");
-const { bytes32 } = require("../../utils/helpers");
+const { network } = require("hardhat");
+const { program } = require("commander");
+const registerAllocation = require("./register_allocation");
+const { getStrategyAccountInfo } = require("./utils");
 
-// eslint-disable-next-line no-unused-vars
-async function main(argv) {
-  await hre.run("compile");
+const aTokens = {
+  aDAI: {
+    address: "0x028171bCA77440897B824Ca71D1c56caC55b68A3",
+    symbol: "DAI",
+    decimals: 18,
+  },
+  aUSDC: {
+    address: "0xBcca60bB61934080951369a648Fb03DF4F96263C",
+    symbol: "USDC",
+    decimals: 6,
+  },
+  aUSDT: {
+    address: "0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811",
+    symbol: "USDT",
+    decimals: 6,
+  },
+};
+
+program.requiredOption("-a, --atoken <string>", "aToken symbol", "aDAI");
+
+async function registerAave(aTokenSymbol) {
   const networkName = network.name.toUpperCase();
-  console.log("");
-  console.log(`${networkName} selected`);
-  console.log("");
-
-  const [deployer] = await ethers.getSigners();
-  console.log("Deployer address:", deployer.address);
-
-  const tvlManager = await getTvlManager(networkName);
-
-  // Aave interest-bearing DAI token
-  const ADAI_ADDRESS = "0x028171bCA77440897B824Ca71D1c56caC55b68A3";
-
-  const addressRegistry = await getAddressRegistry(networkName);
-  const aavePeripheryAddress = await addressRegistry.getAddress(
-    bytes32("aavePeriphery")
-  );
 
   const [, accountAddress] = await getStrategyAccountInfo(networkName);
 
-  console.log("");
-  console.log("Register aave allocation for strategy account ...");
-  console.log("");
-
-  const AavePeriphery = await ethers.getContractFactory("AavePeriphery");
-  const calldataForDai = AavePeriphery.interface.encodeFunctionData(
-    "getUnderlyerBalance(address,address)",
-    [accountAddress, ADAI_ADDRESS]
+  const allocationId = await registerAllocation(
+    aTokens[aTokenSymbol].address,
+    "AavePeriphery",
+    "getUnderlyerBalance",
+    aTokens[aTokenSymbol].symbol,
+    aTokens[aTokenSymbol].decimals,
+    [accountAddress, aTokens[aTokenSymbol].address]
   );
 
-  const data = [aavePeripheryAddress, calldataForDai];
-  let trx = await tvlManager.addAssetAllocation(data, "DAI", 18);
-  await trx.wait();
-  const allocationId = await tvlManager.generateDataHash(data);
-  console.log("Allocation ID:", allocationId);
+  return allocationId;
+}
 
-  console.log("... done.");
-  console.log("");
+async function main(options) {
+  const allocationId = await registerAave(options.atoken);
 
-  /****************************************/
+  return allocationId;
 }
 
 if (!module.parent) {
-  main(argv)
-    .then(() => {
-      console.log("");
-      console.log("Registration successful.");
-      console.log("");
+  program.parse(process.argv);
+  const options = program.opts();
+  main(options)
+    .then((result) => {
+      if (!(typeof result === "string" || result instanceof Buffer)) {
+        process.exit(1);
+      }
+      process.stdout.write(result);
       process.exit(0);
     })
-    .catch((error) => {
-      console.error(error);
-      console.log("");
+    .catch((e) => {
+      process.stdout.write(e.message);
       process.exit(1);
     });
 } else {
-  module.exports = main;
+  // if importing in another script
+  module.exports = registerAave;
 }
