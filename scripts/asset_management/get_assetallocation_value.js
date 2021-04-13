f1ae14b65e8b93a86edc11078902e4746bb23228
Chan-Ho Suh <chanhosuh@gmail.com>
2021-04-12 21:21:06 -0400

    Switch from decimal.js to bignumber.js

--- scripts/asset_management/get_assetallocation_value.js
+++ scripts/asset_management/get_assetallocation_value.js
@@ -1,16 +1,16 @@
 const hre = require("hardhat");
 const { ethers } = hre;
 const { BigNumber } = ethers;
-const { Decimal } = require("decimal.js");
+const BN = require("bignumber.js");
 const coingecko = require("./coingecko");
 
 const priceTotalValue = (allocations, quote, data) => {
   let totalValue = allocations.reduce((acc, t) => {
     const val = data[t.symbol].quote[quote].price;
-    const coins = new Decimal(t.balance.toString()).div(10 ** t.decimals);
-    return acc.add(coins.mul(val));
-  }, new Decimal(0));
-  totalValue = totalValue.mul(1e8).toFixed(0);
+    const coins = new BN(t.balance.toString()).div(10 ** t.decimals);
+    return acc.plus(coins.times(val));
+  }, new BN(0));
+  totalValue = totalValue.times(1e8).toFixed(0);
   return BigNumber.from(totalValue);
 };
 
