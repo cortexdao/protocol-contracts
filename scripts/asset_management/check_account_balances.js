55d0cb5072fe6957fcefdd2360d653ec7a4d4d0f
Chan-Ho Suh <chanhosuh@gmail.com>
2021-06-24 11:36:49 -0400

    Fix comments in script

--- scripts/asset_management/check_account_balances.js
+++ scripts/asset_management/check_account_balances.js
@@ -16,14 +16,14 @@ program.requiredOption(
     "0x6B175474E89094C44Da98b954EedeAC495271d0F", //DAI
     "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", //USDC
     "0xdAC17F958D2ee523a2206206994597C13D831ec7", //USDT
-    "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490", //3Crv
-    "0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A", //3Crv lp token
+    "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490", //3Crv LP token
+    "0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A", //3Crv gauge
     "0x3041CbD36888bECc7bbCBc0045E3B1f144466f5f", //USDC/USDT Pair LP
     "0x028171bCA77440897B824Ca71D1c56caC55b68A3", //ADAI
   ]
 );
 
-const invalidERC20s = ["0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A"];
+const invalidERC20s = ["0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A"]; // 3Crv gauge
 
 // eslint-disable-next-line no-unused-vars
 async function checkBalances(addresses) {
@@ -69,6 +69,7 @@ async function main(options) {
     }
   }
 }
+
 if (!module.parent) {
   program.parse(process.argv);
   const options = program.opts();
