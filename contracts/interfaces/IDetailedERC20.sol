9a1b4473350080fe8789435d52a9d333e1504082
Chan-Ho Suh <chanhosuh@gmail.com>
2021-07-23 17:16:06 -0400

    Interfaces

--- contracts/interfaces/IDetailedERC20.sol
+++ contracts/interfaces/IDetailedERC20.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: BUSDL-1.1
 pragma solidity 0.6.11;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import {
+    IERC20
+} from "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
 
 interface IDetailedERC20 is IERC20 {
     function name() external view returns (string memory);
--- contracts/interfaces/IMintableERC20.sol
+++ contracts/interfaces/IMintableERC20.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BUSDL-1.1
 pragma solidity 0.6.11;
 
-import "./IDetailedERC20.sol";
-import "./IMintable.sol";
+import {IDetailedERC20} from "./IDetailedERC20.sol";
+import {IMintable} from "./IMintable.sol";
 
 // solhint-disable no-empty-blocks
 interface IMintableERC20 is IDetailedERC20, IMintable {
