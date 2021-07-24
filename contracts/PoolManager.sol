7ac6f97ab1b2724f9383e70a682515bd5c8a0566
opz <will.shahda@gmail.com>
2021-07-24 04:40:11 -0400

    Implement rebalance function for pool reserves

--- contracts/PoolManager.sol
+++ contracts/PoolManager.sol
@@ -119,6 +119,30 @@ contract PoolManager is
     // solhint-disable-next-line no-empty-blocks
     function initializeUpgrade() external virtual onlyAdmin {}
 
+    function rebalanceReserves(bytes32[] calldata poolIds)
+        external
+        override
+        nonReentrant
+        onlyLpRole
+    {
+        address lpSafeAddress = addressRegistry.lpSafeAddress();
+        require(lpSafeAddress != address(0), "INVALID_LP_SAFE");
+
+        PoolAmount[] memory rebalanceAmounts = new PoolAmount[](poolIds.length);
+
+        for (uint256 i = 0; i < poolIds.length; i++) {
+            rebalanceAmounts[i] = PoolAmount(
+                poolIds[i],
+                PoolTokenV2(addressRegistry.getAddress(poolIds[i]))
+                    .getReserveTopUpValue()
+            );
+        }
+
+        (PoolTokenV2[] memory pools, int256[] memory amounts) =
+            _getPoolsAndAmounts(rebalanceAmounts);
+        _rebalance(lpSafeAddress, pools, amounts);
+    }
+
     /**
      * @notice Funds LP Safe account and register an asset allocation
      * @dev only callable with LpRole. Also registers the pool underlyer for the account being funded
@@ -138,7 +162,7 @@ contract PoolManager is
     {
         address lpSafeAddress = addressRegistry.lpSafeAddress();
         require(lpSafeAddress != address(0), "INVALID_LP_SAFE");
-        (PoolTokenV2[] memory pools, uint256[] memory amounts) =
+        (PoolTokenV2[] memory pools, int256[] memory amounts) =
             _getPoolsAndAmounts(poolAmounts);
         _fund(lpSafeAddress, pools, amounts);
         _registerPoolUnderlyers(lpSafeAddress, pools);
@@ -163,7 +187,7 @@ contract PoolManager is
     {
         address lpSafeAddress = addressRegistry.lpSafeAddress();
         require(lpSafeAddress != address(0), "INVALID_LP_SAFE");
-        (PoolTokenV2[] memory pools, uint256[] memory amounts) =
+        (PoolTokenV2[] memory pools, int256[] memory amounts) =
             _getPoolsAndAmounts(poolAmounts);
         _checkManagerAllowances(lpSafeAddress, pools, amounts);
         _withdraw(lpSafeAddress, pools, amounts);
@@ -239,14 +263,14 @@ contract PoolManager is
     function _fund(
         address account,
         PoolTokenV2[] memory pools,
-        uint256[] memory amounts
+        int256[] memory amounts
     ) internal {
         MetaPoolToken mApt = MetaPoolToken(addressRegistry.mAptAddress());
         uint256[] memory mintAmounts = new uint256[](pools.length);
         for (uint256 i = 0; i < pools.length; i++) {
             PoolTokenV2 pool = pools[i];
-            uint256 poolAmount = amounts[i];
-            require(poolAmount > 0, "INVALID_AMOUNT");
+            require(amounts[i] > 0, "INVALID_AMOUNT");
+            uint256 poolAmount = uint256(amounts[i]);
             IDetailedERC20 underlyer = pool.underlyer();
 
             uint256 tokenPrice = pool.getUnderlyerPrice();
@@ -278,14 +302,14 @@ contract PoolManager is
     function _withdraw(
         address account,
         PoolTokenV2[] memory pools,
-        uint256[] memory amounts
+        int256[] memory amounts
     ) internal {
         MetaPoolToken mApt = MetaPoolToken(addressRegistry.mAptAddress());
         uint256[] memory burnAmounts = new uint256[](pools.length);
         for (uint256 i = 0; i < pools.length; i++) {
             PoolTokenV2 pool = pools[i];
-            uint256 amountToSend = amounts[i];
-            require(amountToSend > 0, "INVALID_AMOUNT");
+            require(amounts[i] > 0, "INVALID_AMOUNT");
+            uint256 amountToSend = uint256(amounts[i]);
             IDetailedERC20 underlyer = pool.underlyer();
 
             uint256 tokenPrice = pool.getUnderlyerPrice();
@@ -307,13 +331,110 @@ contract PoolManager is
         }
     }
 
+    function _rebalance(
+        address account,
+        PoolTokenV2[] memory pools,
+        int256[] memory amounts
+    ) internal {
+        MetaPoolToken mApt = MetaPoolToken(addressRegistry.mAptAddress());
+
+        int256[] memory mAptDeltas = _calculateMaptDeltas(mApt, pools, amounts);
+
+        _transferBetweenAccountAndPools(account, pools, amounts);
+
+        // MUST do the actual minting after calculating *all* mint amounts,
+        // otherwise due to Chainlink not updating during a transaction,
+        // the totalSupply will change while TVL doesn't.
+        //
+        // Using the pre-mint TVL and totalSupply gives the same answer
+        // as using post-mint values.
+        _rebalanceMapt(mApt, pools, mAptDeltas);
+    }
+
+    function _transferBetweenAccountAndPools(
+        address account,
+        PoolTokenV2[] memory pools,
+        int256[] memory amounts
+    ) internal {
+        require(account != address(0), "INVALID_ADDRESS");
+        require(pools.length == amounts.length, "LENGTHS_MUST_MATCH");
+
+        for (uint256 i = 0; i < pools.length; i++) {
+            require(amounts[i] != 0, "INVALID_AMOUNT");
+
+            IDetailedERC20 underlyer = pools[i].underlyer();
+
+            if (amounts[i] < 0) {
+                underlyer.safeTransferFrom(
+                    address(pools[i]),
+                    account,
+                    uint256(-amounts[i])
+                );
+            } else if (amounts[i] > 0) {
+                underlyer.safeTransferFrom(
+                    account,
+                    address(pools[i]),
+                    uint256(amounts[i])
+                );
+            }
+        }
+    }
+
+    function _rebalanceMapt(
+        MetaPoolToken mApt,
+        PoolTokenV2[] memory pools,
+        int256[] memory mAptDeltas
+    ) internal {
+        require(pools.length == mAptDeltas.length, "LENGTHS_MUST_MATCH");
+
+        for (uint256 i = 0; i < pools.length; i++) {
+            require(mAptDeltas[i] != 0, "INVALID_AMOUNT");
+
+            if (mAptDeltas[i] < 0) {
+                mApt.mint(address(pools[i]), uint256(-mAptDeltas[i]));
+            } else if (mAptDeltas[i] > 0) {
+                mApt.burn(address(pools[i]), uint256(mAptDeltas[i]));
+            }
+        }
+    }
+
+    function _calculateMaptDeltas(
+        MetaPoolToken mApt,
+        PoolTokenV2[] memory pools,
+        int256[] memory amounts
+    ) internal view returns (int256[] memory) {
+        require(pools.length == amounts.length, "LENGTHS_MUST_MATCH");
+
+        int256[] memory mAptDeltas = new int256[](pools.length);
+
+        for (uint256 i = 0; i < pools.length; i++) {
+            require(amounts[i] != 0, "INVALID_AMOUNT");
+
+            IDetailedERC20 underlyer = pools[i].underlyer();
+            uint256 tokenPrice = pools[i].getUnderlyerPrice();
+            uint8 decimals = underlyer.decimals();
+
+            uint256 mAptDelta =
+                mApt.calculateMintAmount(
+                    amounts[i] < 0 ? uint256(-amounts[i]) : uint256(amounts[i]),
+                    tokenPrice,
+                    decimals
+                );
+            mAptDeltas[i] = amounts[i] < 0
+                ? -int256(mAptDelta)
+                : int256(mAptDelta);
+        }
+
+        return mAptDeltas;
+    }
+
     function _getPoolsAndAmounts(ILpSafeFunder.PoolAmount[] memory poolAmounts)
         internal
         view
-        returns (PoolTokenV2[] memory, uint256[] memory)
+        returns (PoolTokenV2[] memory, int256[] memory)
     {
         PoolTokenV2[] memory pools = new PoolTokenV2[](poolAmounts.length);
-        uint256[] memory amounts = new uint256[](poolAmounts.length);
+        int256[] memory amounts = new int256[](poolAmounts.length);
         for (uint256 i = 0; i < poolAmounts.length; i++) {
             amounts[i] = poolAmounts[i].amount;
             pools[i] = PoolTokenV2(
@@ -332,12 +453,13 @@ contract PoolManager is
     function _checkManagerAllowances(
         address account,
         PoolTokenV2[] memory pools,
-        uint256[] memory amounts
+        int256[] memory amounts
     ) internal view {
         for (uint256 i = 0; i < pools.length; i++) {
             IDetailedERC20 underlyer = pools[i].underlyer();
             uint256 allowance = underlyer.allowance(account, address(this));
-            require(amounts[i] <= allowance, "INSUFFICIENT_ALLOWANCE");
+            require(amounts[i] > 0, "INVALID_AMOUNT");
+            require(uint256(amounts[i]) <= allowance, "INSUFFICIENT_ALLOWANCE");
         }
     }
 }
