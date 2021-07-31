35d6fa6ba92bba163dcbe87fa7e3592a37f2094a
Chan-Ho Suh <chanhosuh@gmail.com>
2021-07-31 12:56:38 -0400

    Start unit tests for Erc20Allocation

---
+++ test-unit/Erc20Allocation.js
@@ -0,0 +1,117 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+const { artifacts, ethers, waffle } = hre;
+const timeMachine = require("ganache-time-traveler");
+const { FAKE_ADDRESS, ANOTHER_FAKE_ADDRESS } = require("../utils/helpers");
+const { deployMockContract } = waffle;
+
+const IDetailedERC20 = artifacts.readArtifactSync("IDetailedERC20");
+
+describe.only("Contract: Erc20Allocation", () => {
+  // signers
+  let deployer;
+  let user;
+
+  // contract factories
+  let Erc20Allocation;
+
+  // deployed contracts
+  let erc20Allocation;
+
+  // use EVM snapshots for test isolation
+  let snapshotId;
+
+  const token_0 = {
+    token: FAKE_ADDRESS,
+    symbol: "TOKEN",
+    decimals: 6,
+  };
+  const token_1 = {
+    token: ANOTHER_FAKE_ADDRESS,
+    symbol: "ANOTHER_TOKEN",
+    decimals: 18,
+  };
+
+  beforeEach(async () => {
+    let snapshot = await timeMachine.takeSnapshot();
+    snapshotId = snapshot["result"];
+  });
+
+  afterEach(async () => {
+    await timeMachine.revertToSnapshot(snapshotId);
+  });
+
+  before(async () => {
+    [deployer, user] = await ethers.getSigners();
+    Erc20Allocation = await ethers.getContractFactory("Erc20Allocation");
+    erc20Allocation = await Erc20Allocation.deploy();
+  });
+
+  it("addToken", async () => {
+    await erc20Allocation["addToken(address,string,uint8)"](
+      token_0.token,
+      token_0.symbol,
+      token_0.decimals
+    );
+    let result = await erc20Allocation.tokens();
+    // have to check in this cumbersome manner rather than a deep
+    // equal, because Ethers returns each struct as an *array*
+    // with struct fields set as properties
+    expect(result[0].token).to.equal(token_0.token);
+    expect(result[0].symbol).to.equal(token_0.symbol);
+    expect(result[0].decimals).to.equal(token_0.decimals);
+  });
+
+  describe("", () => {
+    let tokenMock_0;
+    let tokenMock_1;
+
+    const token_0 = {
+      symbol: "TOKEN",
+      decimals: 6,
+    };
+    const token_1 = {
+      symbol: "ANOTHER_TOKEN",
+      decimals: 18,
+    };
+
+    beforeEach("", async () => {
+      tokenMock_0 = await deployMockContract(deployer, IDetailedERC20.abi);
+      token_0.token = tokenMock_0.address;
+      tokenMock_1 = await deployMockContract(deployer, IDetailedERC20.abi);
+      token_1.token = tokenMock_1.address;
+
+      await tokenMock_0.mock.symbol.returns(token_0.symbol);
+      await tokenMock_0.mock.decimals.returns(token_0.decimals);
+      await tokenMock_1.mock.symbol.returns(token_1.symbol);
+      await tokenMock_1.mock.decimals.returns(token_1.decimals);
+
+      await tokenMock_0.mock.balanceOf.withArgs(user.address).returns(42);
+
+      await erc20Allocation["addToken(address,string,uint8)"](
+        token_0.token,
+        token_0.symbol,
+        token_0.decimals
+      );
+      await erc20Allocation["addToken(address,string,uint8)"](
+        token_1.token,
+        token_1.symbol,
+        token_1.decimals
+      );
+    });
+
+    it("symbolOf", async () => {
+      expect(await erc20Allocation.symbolOf(0)).to.equal(token_0.symbol);
+      expect(await erc20Allocation.symbolOf(1)).to.equal(token_1.symbol);
+    });
+
+    it("decimalsOf", async () => {
+      expect(await erc20Allocation.decimalsOf(0)).to.equal(token_0.decimals);
+      expect(await erc20Allocation.decimalsOf(1)).to.equal(token_1.decimals);
+    });
+
+    it("balanceOf", async () => {
+      expect(await erc20Allocation.balanceOf(user.address, 0)).to.equal(42);
+    });
+  });
+});
