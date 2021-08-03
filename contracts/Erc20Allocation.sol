2af5074c5c3a65e1d9bf1c8ffb4decd7c9cba244
Chan-Ho Suh <chanhosuh@gmail.com>
2021-08-02 22:45:22 -0400

    Add AccessControl roles for Erc20Allocation

--- contracts/Erc20Allocation.sol
+++ contracts/Erc20Allocation.sol
@@ -2,37 +2,63 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 import {EnumerableSet} from "@openzeppelin/contracts/utils/EnumerableSet.sol";
 import {AssetAllocationBase} from "./AssetAllocationBase.sol";
+import {IAddressRegistryV2} from "./interfaces/IAddressRegistryV2.sol";
 import {IDetailedERC20} from "./interfaces/IDetailedERC20.sol";
+import {AccessControl} from "./utils/AccessControl.sol";
 
-contract Erc20Allocation is AssetAllocationBase {
+contract Erc20Allocation is AssetAllocationBase, AccessControl {
+    using Address for address;
     using EnumerableSet for EnumerableSet.AddressSet;
 
     EnumerableSet.AddressSet private _tokenAddresses;
     mapping(address => TokenData) private _tokenToData;
 
-    function addToken(address token) external {
-        string memory symbol = IDetailedERC20(token).symbol();
-        uint8 decimals = IDetailedERC20(token).decimals();
-        addToken(token, symbol, decimals);
+    constructor(address addressRegistry_) public {
+        require(addressRegistry_.isContract(), "INVALID_ADDRESS_REGISTRY");
+        IAddressRegistryV2 addressRegistry =
+            IAddressRegistryV2(addressRegistry_);
+        _setupRole(
+            DEFAULT_ADMIN_ROLE,
+            addressRegistry.getAddress("emergencySafe")
+        );
+        _setupRole(CONTRACT_ROLE, addressRegistry.tvlManagerAddress());
     }
 
-    function addToken(address token, string memory symbol) external {
+    function addToken(address token) external onlyContractRole {
+        string memory symbol = IDetailedERC20(token).symbol();
         uint8 decimals = IDetailedERC20(token).decimals();
-        addToken(token, symbol, decimals);
+        _addToken(token, symbol, decimals);
+    }
+
+    function addToken(address token, string memory symbol)
+        external
+        onlyContractRole
+    {
+        uint8 decimals = IDetailedERC20(token).decimals();
+        _addToken(token, symbol, decimals);
     }
 
     function addToken(
         address token,
         string memory symbol,
         uint8 decimals
-    ) public {
+    ) external onlyContractRole {
+        _addToken(token, symbol, decimals);
+    }
+
+    function _addToken(
+        address token,
+        string memory symbol,
+        uint8 decimals
+    ) internal {
         _tokenAddresses.add(token);
         _tokenToData[token] = TokenData(token, symbol, decimals);
     }
 
-    function removeToken(address token) external {
+    function removeToken(address token) external onlyContractRole {
         _tokenAddresses.remove(token);
         delete _tokenToData[token];
     }
