ee317cb47296b83d8ebd35ce792cc2f8ae37128e
Chan-Ho Suh <chanhosuh@gmail.com>
2021-08-05 22:44:11 -0400

    Delete V1 contracts, substitute Curve allocation
    Allocation impl for Curve is tentative, not even POC, in order
    to get the tests to pass with V2 contracts.

---
+++ contracts/allocations/CurveAllocation.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: BUSDL-1.1
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ImmutableAssetAllocation} from "../ImmutableAssetAllocation.sol";
+import {IStableSwap, ILiquidityGauge} from "./Curve.sol";
+
+/**
+ * @title Periphery Contract for the Curve 3pool
+ * @author APY.Finance
+ * @notice This contract enables the APY.Finance system to retrieve the balance
+ * of an underlyer of a Curve LP token. The balance is used as part
+ * of the Chainlink computation of the deployed TVL.  The primary
+ * `getUnderlyerBalance` function is invoked indirectly when a
+ * Chainlink node calls `balanceOf` on the APYAssetAllocationRegistry.
+ */
+contract CurveAllocationBase {
+    using SafeMath for uint256;
+
+    /**
+     * @notice Returns the balance of an underlying token represented by
+     * an account's LP token balance.
+     * @param stableSwap the liquidity pool comprised of multiple underlyers
+     * @param gauge the staking contract for the LP tokens
+     * @param lpToken the LP token representing the share of the pool
+     * @param coin the index indicating which underlyer
+     * @return balance
+     */
+    function getUnderlyerBalance(
+        address account,
+        IStableSwap stableSwap,
+        ILiquidityGauge gauge,
+        IERC20 lpToken,
+        uint256 coin
+    ) public view returns (uint256 balance) {
+        require(address(stableSwap) != address(0), "INVALID_STABLESWAP");
+        require(address(gauge) != address(0), "INVALID_GAUGE");
+        require(address(lpToken) != address(0), "INVALID_LP_TOKEN");
+
+        uint256 poolBalance = getPoolBalance(stableSwap, coin);
+        (uint256 lpTokenBalance, uint256 lpTokenSupply) =
+            getLpTokenShare(account, stableSwap, gauge, lpToken);
+
+        balance = lpTokenBalance.mul(poolBalance).div(lpTokenSupply);
+    }
+
+    function getPoolBalance(IStableSwap stableSwap, uint256 coin)
+        public
+        view
+        returns (uint256)
+    {
+        require(address(stableSwap) != address(0), "INVALID_STABLESWAP");
+        return stableSwap.balances(coin);
+    }
+
+    function getLpTokenShare(
+        address account,
+        IStableSwap stableSwap,
+        ILiquidityGauge gauge,
+        IERC20 lpToken
+    ) public view returns (uint256 balance, uint256 totalSupply) {
+        require(address(stableSwap) != address(0), "INVALID_STABLESWAP");
+        require(address(gauge) != address(0), "INVALID_GAUGE");
+        require(address(lpToken) != address(0), "INVALID_LP_TOKEN");
+
+        totalSupply = lpToken.totalSupply();
+        balance = lpToken.balanceOf(account);
+        balance = balance.add(gauge.balanceOf(account));
+    }
+}
+
+contract Curve3PoolAllocation is CurveAllocationBase, ImmutableAssetAllocation {
+    // Curve 3Pool Mainnet addresses:
+    address public constant STABLE_SWAP_ADDRESS =
+        0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
+    address public constant LP_TOKEN_ADDRESS =
+        0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490;
+    address public constant LIQUIDITY_GAUGE_ADDRESS =
+        0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A;
+
+    address public constant DAI_ADDRESS =
+        0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant USDC_ADDRESS =
+        0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant USDT_ADDRESS =
+        0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    constructor() public ImmutableAssetAllocation(_getTokenData()) {} // solhint-disable-line no-empty-blocks
+
+    function _getTokenData() internal returns (TokenData[] memory) {
+        TokenData[] memory tokens = new TokenData[](3);
+        tokens[0] = TokenData(DAI_ADDRESS, "DAI", 18);
+        tokens[1] = TokenData(USDC_ADDRESS, "USDC", 6);
+        tokens[2] = TokenData(USDT_ADDRESS, "USDT", 6);
+        return tokens;
+    }
+
+    function balanceOf(address account, uint8 tokenIndex)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return
+            super.getUnderlyerBalance(
+                account,
+                IStableSwap(STABLE_SWAP_ADDRESS),
+                ILiquidityGauge(LIQUIDITY_GAUGE_ADDRESS),
+                IERC20(LP_TOKEN_ADDRESS),
+                uint256(tokenIndex)
+            );
+    }
+}
