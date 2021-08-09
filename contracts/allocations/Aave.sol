e1958ff8ca334dc4a82b53c0cd9c0303c6d33b98
Chan-Ho Suh <chanhosuh@gmail.com>
2021-08-09 14:36:32 -0400

    Start Aave allocation contracts

--- contracts/allocations/Aave.sol
+++ contracts/allocations/Aave.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: BUSDL-1.1
 pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
 
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ImmutableAssetAllocation} from "../ImmutableAssetAllocation.sol";
 
 /**
  * @notice the lending pool contract
@@ -63,8 +65,10 @@ interface IAaveLendingPool {
  * `getUnderlyerBalance` function is invoked indirectly when a
  * Chainlink node calls `balanceOf` on the APYAssetAllocationRegistry.
  */
-contract AavePeriphery {
+contract AaveAllocationBase {
     using SafeMath for uint256;
+    address public constant LENDING_POOL_ADDRESS =
+        0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
 
     /**
      * @notice Returns the balance of an underlying token represented by
@@ -82,5 +86,33 @@ contract AavePeriphery {
         require(address(aToken) != address(0), "INVALID_AAVE_TOKEN");
 
         return aToken.balanceOf(account);
+    }
+}
+
+contract AaveDaiAllocation is AaveAllocationBase, ImmutableAssetAllocation {
+    address public constant ADAI_ADDRESS =
+        0x028171bCA77440897B824Ca71D1c56caC55b68A3;
+    address public constant AUSDC_ADDRESS =
+        0xBcca60bB61934080951369a648Fb03DF4F96263C;
+    address public constant AUSDT_ADDRESS =
+        0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811;
+
+    constructor() public ImmutableAssetAllocation(_getTokenData()) {} // solhint-disable-line no-empty-blocks
+
+    function _getTokenData() internal pure returns (TokenData[] memory) {
+        TokenData[] memory tokens = new TokenData[](3);
+        tokens[0] = TokenData(ADAI_ADDRESS, "DAI", 18);
+        tokens[1] = TokenData(AUSDC_ADDRESS, "USDC", 6);
+        tokens[2] = TokenData(AUSDT_ADDRESS, "USDT", 6);
+        return tokens;
+    }
+
+    function balanceOf(address account, uint8 tokenIndex)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        return IERC20(addressOf(tokenIndex)).balanceOf(account);
     }
 }
