96e6631bb60d3ce4deae84bbbc864f2f1e992ed4
Chan-Ho Suh <chanhosuh@gmail.com>
2021-08-13 13:30:21 -0400

    Unwrap underlyer for IronBank

--- contracts/allocations/curve/pools/ironbank.sol
+++ contracts/allocations/curve/pools/ironbank.sol
@@ -12,22 +12,18 @@ import {
     ILiquidityGauge
 } from "contracts/allocations/curve/interfaces/ILiquidityGauge.sol";
 import {CurveAllocationBase} from "contracts/allocations/curve/Curve.sol";
+import {
+    CTokenInterface
+} from "contracts/allocations/curve/interfaces/CTokenInterface.sol";
+import {Curve3PoolUnderlyerConstants} from "./3pool.sol";
 
-contract CurveIronBankConstants {
+contract CurveIronBankConstants is Curve3PoolUnderlyerConstants {
     address public constant STABLE_SWAP_ADDRESS =
         0x2dded6Da1BF5DBdF597C45fcFaa3194e53EcfeAF;
     address public constant LP_TOKEN_ADDRESS =
         0x5282a4eF67D9C33135340fB3289cc1711c13638C;
     address public constant LIQUIDITY_GAUGE_ADDRESS =
         0xF5194c3325202F456c95c1Cf0cA36f8475C1949F;
-
-    // underlyers
-    address public constant CYDAI_ADDRESS =
-        0x8e595470Ed749b85C6F7669de83EAe304C2ec68F;
-    address public constant CYUSDC_ADDRESS =
-        0x76Eb2FE28b36B3ee97F3Adae0C69606eeDB2A37c;
-    address public constant CYUSDT_ADDRESS =
-        0x48759F220ED983dB51fA7A8C0D2AAb8f3ce4166a;
 }
 
 contract CurveIronBankAllocation is
@@ -41,7 +37,7 @@ contract CurveIronBankAllocation is
         override
         returns (uint256)
     {
-        return
+        uint256 cyBalance =
             super.getUnderlyerBalance(
                 account,
                 IStableSwap(STABLE_SWAP_ADDRESS),
@@ -49,6 +45,17 @@ contract CurveIronBankAllocation is
                 IERC20(LP_TOKEN_ADDRESS),
                 uint256(tokenIndex)
             );
+        return unwrapBalance(cyBalance, tokenIndex);
+    }
+
+    function unwrapBalance(uint256 balance, uint8 tokenIndex)
+        public
+        view
+        returns (uint256)
+    {
+        IStableSwap pool = IStableSwap(STABLE_SWAP_ADDRESS);
+        CTokenInterface cyToken = CTokenInterface(pool.coins(tokenIndex));
+        return balance.mul(cyToken.exchangeRateStored());
     }
 
     function _getTokenData()
@@ -58,9 +65,9 @@ contract CurveIronBankAllocation is
         returns (TokenData[] memory)
     {
         TokenData[] memory tokens = new TokenData[](3);
-        tokens[0] = TokenData(CYDAI_ADDRESS, "cyDAI", 8);
-        tokens[1] = TokenData(CYUSDC_ADDRESS, "cyUSDC", 8);
-        tokens[2] = TokenData(CYUSDT_ADDRESS, "cyUSDT", 8);
+        tokens[0] = TokenData(DAI_ADDRESS, "DAI", 18);
+        tokens[1] = TokenData(USDC_ADDRESS, "USDC", 6);
+        tokens[2] = TokenData(USDT_ADDRESS, "USDT", 6);
         return tokens;
     }
 }
