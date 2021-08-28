be49dcaec4a6aefa60bbf909f462271aaed76513
The Architect <email@example.com>
2021-08-28 15:18:32 -0700

    WIP Template Method

--- contracts/protocols/curve/zaps/CurveBasePool.sol
+++ contracts/protocols/curve/zaps/CurveBasePool.sol
@@ -7,12 +7,7 @@ import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {IZap} from "contracts/interfaces/IZap.sol";
 import {IAssetAllocation} from "contracts/interfaces/IAssetAllocation.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {
-    IStableSwap
-} from "contracts/protocols/curve/interfaces/IStableSwap.sol";
-import {
-    ILiquidityGauge
-} from "contracts/protocols/curve/interfaces/ILiquidityGauge.sol";
+import {ILiquidityGauge} from "contracts/protocols/curve/interfaces/ILiquidityGauge.sol";
 import {IDetailedERC20} from "contracts/interfaces/IDetailedERC20.sol";
 
 abstract contract CurveBasePool is IZap {
@@ -21,27 +16,40 @@ abstract contract CurveBasePool is IZap {
     address public constant CRV_ADDRESS =
         0xD533a949740bb3306d119CC777fa900bA034cd52;
 
-    function SWAP_ADDRESS() external view virtual returns (address);
+    function SWAP_ADDRESS() external pure virtual returns (address);
 
-    function GAUGE_ADDRESS() external view virtual returns (address);
+    function GAUGE_ADDRESS() external pure virtual returns (address);
 
-    function LP_ADDRESS() external view virtual returns (address);
+    function LP_ADDRESS() external pure virtual returns (address);
 
-    function ALLOCATION_ADDRESS() external view virtual returns (address);
+    function ALLOCATION_ADDRESSES()
+        external
+        pure
+        virtual
+        returns (address[] memory);
 
-    function _DENOMINATOR() external view virtual returns (uint256);
+    function _DENOMINATOR() external pure virtual returns (uint256);
 
-    function _SLIPPAGE() external view virtual returns (uint256);
+    function _SLIPPAGE() external pure virtual returns (uint256);
 
-    function N_COINS() external view virtual returns (uint256);
+    function N_COINS() external pure virtual returns (uint256);
 
-    function NUM_ALLOCATIONS() external view virtual returns (uint256);
+    function NUM_ALLOCATIONS() external pure virtual returns (uint256);
 
-    function NUM_ERC20_ALLOCATIONS() external view virtual returns (uint256);
+    function NUM_ERC20_ALLOCATIONS() external pure virtual returns (uint256);
+
+    function _getVirtualPrice() internal virtual returns (uint256);
+
+    function _getCoinAtIndex(uint256 i) internal virtual returns (address);
+
+    function _addLiquidity(uint256[] calldata amounts_, uint256 minAmount)
+        internal
+        virtual;
+
+    function _removeLiquidity(uint256 lpBalance) internal virtual;
 
     /// @param amounts array of underlyer amounts
     function deployLiquidity(uint256[] calldata amounts) external override {
-        IStableSwap stableSwap = IStableSwap(this.SWAP_ADDRESS());
         uint256 totalAmount = 0;
         uint256[3] memory amounts_;
         for (uint256 i = 0; i < amounts_.length; i++) {
@@ -49,21 +57,19 @@ abstract contract CurveBasePool is IZap {
             amounts_[i] = amounts[i];
         }
 
-        uint256 v = totalAmount.mul(1e18).div(stableSwap.get_virtual_price());
-        uint256 minAmount =
-            v.mul(this._DENOMINATOR().sub(this._SLIPPAGE())).div(
-                this._DENOMINATOR()
-            );
+        uint256 v = totalAmount.mul(1e18).div(_getVirtualPrice());
+        uint256 minAmount = v
+            .mul(this._DENOMINATOR().sub(this._SLIPPAGE()))
+            .div(this._DENOMINATOR());
 
         for (uint256 i = 0; i < this.N_COINS(); i++) {
             if (amounts_[i] == 0) continue;
 
-            address underlyerAddress =
-                IStableSwap(this.SWAP_ADDRESS()).coins(i);
+            address underlyerAddress = _getCoinAtIndex(i);
             IERC20(underlyerAddress).approve(this.SWAP_ADDRESS(), 0);
             IERC20(underlyerAddress).approve(this.SWAP_ADDRESS(), amounts_[i]);
         }
-        stableSwap.add_liquidity(amounts_, minAmount);
+        _addLiquidity(amounts, minAmount);
 
         ILiquidityGauge liquidityGauge = ILiquidityGauge(this.GAUGE_ADDRESS());
 
@@ -76,14 +82,8 @@ abstract contract CurveBasePool is IZap {
     function unwindLiquidity(uint256 amount) external override {
         ILiquidityGauge liquidityGauge = ILiquidityGauge(this.GAUGE_ADDRESS());
         liquidityGauge.withdraw(amount);
-
         uint256 lpBalance = IERC20(this.LP_ADDRESS()).balanceOf(address(this));
-
-        IStableSwap stableSwap = IStableSwap(this.SWAP_ADDRESS());
-        stableSwap.remove_liquidity(
-            lpBalance,
-            [uint256(0), uint256(0), uint256(0)]
-        );
+        _removeLiquidity(lpBalance);
     }
 
     function sortedSymbols() public view override returns (string[] memory) {
@@ -91,8 +91,7 @@ abstract contract CurveBasePool is IZap {
         // so we have to hardcode the number here
         string[] memory symbols = new string[](this.N_COINS());
         for (uint256 i = 0; i < symbols.length; i++) {
-            address underlyerAddress =
-                IStableSwap(this.SWAP_ADDRESS()).coins(i);
+            address underlyerAddress = _getCoinAtIndex(i);
             symbols[i] = IDetailedERC20(underlyerAddress).symbol();
         }
         return symbols;
--- contracts/protocols/curve/zaps/pools/3pool.sol
+++ contracts/protocols/curve/zaps/pools/3pool.sol
@@ -4,6 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IAssetAllocation} from "contracts/interfaces/IAssetAllocation.sol";
+import {IStableSwap} from "contracts/protocols/curve/interfaces/IStableSwap.sol";
 import {Curve3PoolConstants} from "contracts/protocols/curve/allocations/pools/3pool.sol";
 import {CurveBasePool} from "contracts/protocols/curve/zaps/CurveBasePool.sol";
 
@@ -12,7 +13,7 @@ contract Curve3PoolZap is CurveBasePool, Curve3PoolConstants {
     address public constant override GAUGE_ADDRESS = LIQUIDITY_GAUGE_ADDRESS;
     address public constant override LP_ADDRESS = LP_TOKEN_ADDRESS;
 
-    address public constant override ALLOCATION_ADDRESS = address(0);
+    address[] public constant override ALLOCATION_ADDRESSES = [address(0)];
 
     uint256 public constant override _DENOMINATOR = 10000;
     uint256 public constant override _SLIPPAGE = 100;
@@ -21,6 +22,28 @@ contract Curve3PoolZap is CurveBasePool, Curve3PoolConstants {
     uint256 public constant override NUM_ALLOCATIONS = 1;
     uint256 public constant override NUM_ERC20_ALLOCATIONS = 1;
 
+    function _getVirtualPrice() internal override returns (uint256) {
+        return IStableSwap(STABLE_SWAP_ADDRESS).get_virtual_price();
+    }
+
+    function _getCoinAtIndex(uint256 i) internal override returns (address) {
+        return IStableSwap(STABLE_SWAP_ADDRESS).coins(i);
+    }
+
+    function _addLiquidity(uint256[] calldata amounts, uint256 minAmount)
+        internal
+        override
+    {
+        IStableSwap(STABLE_SWAP_ADDRESS).add_liquidity(amounts, minAmount);
+    }
+
+    function _removeLiquidity(uint256 lpBalance) internal override {
+        IStableSwap(STABLE_SWAP_ADDRESS).remove_liquidity(
+            lpBalance,
+            [uint256(0), uint256(0), uint256(0)]
+        );
+    }
+
     function assetAllocations()
         public
         view
@@ -28,7 +51,7 @@ contract Curve3PoolZap is CurveBasePool, Curve3PoolConstants {
         returns (IAssetAllocation[] memory)
     {
         IAssetAllocation[] memory allocations = new IAssetAllocation[](1);
-        allocations[0] = IAssetAllocation(ALLOCATION_ADDRESS);
+        allocations[0] = IAssetAllocation(ALLOCATION_ADDRESSES[0]);
         return allocations;
     }
 
