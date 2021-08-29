4c7cd9650d25b8546b495209249e56523b30bd15
Chan-Ho Suh <chanhosuh@gmail.com>
2021-08-29 18:12:05 -0400

    Add pool deploys

---
+++ contracts/AlphaDeployer.sol
@@ -0,0 +1,186 @@
+// SPDX-License-Identifier: BUSDL-1.1
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+import {Ownable} from "contracts/common/Imports.sol";
+import {Address} from "contracts/libraries/Imports.sol";
+
+/** @dev
+# Alpha Deployment
+
+## Deployment order of contracts
+
+The address registry needs multiple addresses registered
+to setup the roles for access control in the contract
+constructors:
+
+MetaPoolToken
+
+- emergencySafe (emergency role, default admin role)
+- lpSafe (LP role)
+
+PoolTokenV2
+
+- emergencySafe (emergency role, default admin role)
+- adminSafe (admin role)
+- mApt (contract role)
+
+Erc20Allocation
+
+- emergencySafe (default admin role)
+- lpSafe (LP role)
+- mApt (contract role)
+
+TvlManager
+
+- emergencySafe (emergency role, default admin role)
+- lpSafe (LP role)
+
+OracleAdapter
+
+- emergencySafe (emergency role, default admin role)
+- adminSafe (admin role)
+- tvlManager (contract role)
+- mApt (contract role)
+
+Note the order of dependencies: a contract requires contracts
+above it in the list to be deployed first. Thus we need
+to deploy in the order given, starting with the Safes.
+
+Other steps:
+- LP Safe must approve mAPT for each pool underlyer
+*/
+/* solhint-disable func-name-mixedcase, no-empty-blocks */
+contract AlphaDeployer is Ownable {
+    using Address for address;
+
+    address public constant DAI_ADDRESS =
+        0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant USDC_ADDRESS =
+        0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant USDT_ADDRESS =
+        0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    address public constant POOL_PROXY_ADMIN = 0x0;
+    address public constant DAI_POOL_PROXY = 0x0;
+    address public constant USDC_POOL_PROXY = 0x0;
+    address public constant USDT_POOL_PROXY = 0x0;
+
+    address public addressRegistry;
+
+    constructor(address addressRegistry_) public {
+        addressRegistry = addressRegistry_;
+    }
+
+    function deploy_0_Safes() external onlyOwner {}
+
+    function deploy_1_MetaPoolToken() external onlyOwner {
+        address proxyAdmin = new ProxyAdmin();
+        address logic = new MetaPoolToken(addressRegistry);
+        address proxy =
+            new MetaPoolTokenProxy(logic, proxyAdmin, addressRegistry);
+
+        Ownable(proxyAdmin).transferOwnership(msg.sender);
+        Ownable(logic).transferOwnership(msg.sender); // unnecessary
+        Ownable(proxy).transferOwnership(msg.sender);
+
+        addressRegistry.functionDelegateCall(
+            abi.encodeWithSignature(
+                "registerAddress(bytes32,address)",
+                "mApt",
+                proxy
+            )
+        );
+    }
+
+    function deploy_2_DemoPools() external onlyOwner {
+        /* complete proxy deploy for the demo pools */
+
+        address proxyAdmin = new ProxyAdmin();
+        address logicV1 = new PoolToken();
+        address logicV2 = new PoolTokenV2();
+
+        bytes initData =
+            abi.encodeWithSignature(
+                "initializeUpgrade(address)",
+                addressRegistry
+            );
+
+        address daiProxy =
+            new PoolTokenProxy(
+                logicV1,
+                proxyAdmin,
+                DAI_ADDRESS,
+                0xCAFECAFECAFECAFECAFECAFECAFECAFECAFECAFE
+            );
+        ProxyAdmin(proxyAdmin).upgradeAndCall(daiProxy, logicV2, initData);
+
+        address usdcProxy =
+            new PoolTokenProxy(
+                logicV1,
+                proxyAdmin,
+                USDC_ADDRESS,
+                0xCAFECAFECAFECAFECAFECAFECAFECAFECAFECAFE
+            );
+        ProxyAdmin(proxyAdmin).upgradeAndCall(usdcProxy, logicV2, initData);
+
+        address usdtProxy =
+            new PoolTokenProxy(
+                logicV1,
+                proxyAdmin,
+                USDT_ADDRESS,
+                0xCAFECAFECAFECAFECAFECAFECAFECAFECAFECAFE
+            );
+        ProxyAdmin(proxyAdmin).upgradeAndCall(usdtProxy, logicV2, initData);
+
+        Ownable(proxyAdmin).transferOwnership(msg.sender);
+        Ownable(daiProxy).transferOwnership(msg.sender);
+        Ownable(usdcProxy).transferOwnership(msg.sender);
+        Ownable(usdtProxy).transferOwnership(msg.sender);
+    }
+
+    function deploy_3_Erc20Allocation() external onlyOwner {}
+
+    function deploy_4_TvlManager() external onlyOwner {}
+
+    function deploy_5_OracleAdapter() external onlyOwner {}
+
+    function deploy_6_PoolTokenV2_upgrade() external onlyOwner {
+        /* upgrade from v1 to v2 */
+
+        address logicV2 = new PoolTokenV2();
+        bytes initData =
+            abi.encodeWithSignature(
+                "initializeUpgrade(address)",
+                addressRegistry
+            );
+
+        bytes daiUpgradeData =
+            abi.encodeWithSignature(
+                "upgradeAndCall(address,address,bytes)",
+                DAI_POOL_PROXY,
+                logicV2,
+                initData
+            );
+        POOL_PROXY_ADMIN.functionDelegateCall(daiUpgradeData);
+
+        bytes usdcUpgradeData =
+            abi.encodeWithSignature(
+                "upgradeAndCall(address,address,bytes)",
+                USDC_POOL_PROXY,
+                logicV2,
+                initData
+            );
+        POOL_PROXY_ADMIN.functionDelegateCall(usdcUpgradeData);
+
+        bytes usdtUpgradeData =
+            abi.encodeWithSignature(
+                "upgradeAndCall(address,address,bytes)",
+                USDT_POOL_PROXY,
+                logicV2,
+                initData
+            );
+        POOL_PROXY_ADMIN.functionDelegateCall(usdtUpgradeData);
+    }
+}
+/* solhint-enable func-name-mixedcase */
