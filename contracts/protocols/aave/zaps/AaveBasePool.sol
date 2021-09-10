54bd995fde316aca4af7a57bfd77ba283dc17a9a
opz <will.shahda@gmail.com>
2021-09-09 21:22:57 -0400

    Fix syntax errors

--- contracts/protocols/aave/zaps/AaveBasePool.sol
+++ contracts/protocols/aave/zaps/AaveBasePool.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import {SafeMath} from "contracts/libraries/Imports.sol";
 import {IZap} from "contracts/lpaccount/Imports.sol";
-import {DetailedERC20} from "contracts/common/Imports.sol";
+import {IDetailedERC20} from "contracts/common/Imports.sol";
 
 abstract contract AaveBasePool is IZap {
     using SafeMath for uint256;
@@ -15,33 +15,29 @@ abstract contract AaveBasePool is IZap {
     // TODO: think about including the AToken address to conserve gas
     // TODO: consider using IDetailedERC20 as the type instead of address for underlyer
 
-    constructor(
-        address underlyerAddress,
-        address lendingAddress,
-    ) public {
+    constructor(address underlyerAddress, address lendingAddress) public {
         UNDERLYER_ADDRESS = underlyerAddress;
         LENDING_ADDRESS = lendingAddress;
     }
 
-    function _deposit(uint256 amount)
-        internal
-        virtual;
+    function _deposit(uint256 amount) internal virtual;
 
     function _withdraw(uint256 lpBalance) internal virtual;
 
     /// @param amounts array of underlyer amounts
     function deployLiquidity(uint256[] calldata amounts) external override {
-        _addLiquidity(amounts[0]);
+        _deposit(amounts[0]);
     }
 
     /// @param amount LP token amount
     function unwindLiquidity(uint256 amount) external override {
-        _removeLiquidity(amount);
+        _withdraw(amount);
     }
 
     function sortedSymbols() public view override returns (string[] memory) {
         // so we have to hardcode the number here
-        string[1] memory symbols = [IDetailedERC20(underlyerAddress).symbol()];
+        string[] memory symbols = new string[](1);
+        symbols[0] = IDetailedERC20(UNDERLYER_ADDRESS).symbol();
         return symbols;
     }
 }
