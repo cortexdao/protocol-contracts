a8637cad96f27f44d59e8b11113daebbf4000341
opz <will.shahda@gmail.com>
2021-09-20 20:47:01 -0400

    Remove stkAAVE reward token from test

--- test-integration/CurveZaps.js
+++ test-integration/CurveZaps.js
@@ -136,7 +136,6 @@ describe("Curve Zaps", () => {
       gaugeInterface: "ILiquidityGauge",
       numberOfCoins: 2,
       whaleAddress: WHALE_POOLS["ADAI"],
-      rewardToken: "0x4da27a545c0c5B758a6BA100e3a049001de870f5",
     },
     {
       contractName: "SusdV2Zap",
