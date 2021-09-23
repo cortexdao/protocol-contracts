06fdece067808f26179d759a2f44de3f1eaa2dcf
opz <will.shahda@gmail.com>
2021-09-23 03:54:52 -0400

    Depositor contract for busdv2 metapool

--- contracts/protocols/curve/usdp/Allocation.sol
+++ contracts/protocols/curve/usdp/Allocation.sol
@@ -2,26 +2,13 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-import {IERC20} from "contracts/common/Imports.sol";
-import {SafeMath} from "contracts/libraries/Imports.sol";
-import {ImmutableAssetAllocation} from "contracts/tvl/Imports.sol";
-
 import {
-    ILiquidityGauge
-} from "contracts/protocols/curve/common/interfaces/Imports.sol";
-
-import {
-    IMetaPool,
     MetaPoolAllocationBase
 } from "contracts/protocols/curve/metapool/Imports.sol";
 
 import {CurveUsdpConstants} from "./Constants.sol";
 
-contract CurveUsdpAllocation is
-    MetaPoolAllocationBase,
-    ImmutableAssetAllocation,
-    CurveUsdpConstants
-{
+contract CurveUsdpAllocation is MetaPoolAllocationBase, CurveUsdpConstants {
     constructor(address curve3PoolAllocation_)
         public
         MetaPoolAllocationBase(curve3PoolAllocation_)
@@ -36,9 +23,9 @@ contract CurveUsdpAllocation is
         return
             super.getUnderlyerBalance(
                 account,
-                IMetaPool(META_POOL_ADDRESS),
-                ILiquidityGauge(LIQUIDITY_GAUGE_ADDRESS),
-                IERC20(LP_TOKEN_ADDRESS),
+                META_POOL,
+                LIQUIDITY_GAUGE,
+                LP_TOKEN,
                 uint256(tokenIndex)
             );
     }
@@ -49,11 +36,6 @@ contract CurveUsdpAllocation is
         override
         returns (TokenData[] memory)
     {
-        TokenData[] memory tokens = new TokenData[](4);
-        tokens[0] = TokenData(PRIMARY_UNDERLYER_ADDRESS, "USDP", 18);
-        tokens[1] = TokenData(DAI_ADDRESS, "DAI", 18);
-        tokens[2] = TokenData(USDC_ADDRESS, "USDC", 6);
-        tokens[3] = TokenData(USDT_ADDRESS, "USDT", 6);
-        return tokens;
+        return _getBasePoolTokenData(address(PRIMARY_UNDERLYER), "USDP", 18);
     }
 }
