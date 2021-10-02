a4b1f53ab649041bb1d90bc1caba6ea655d6ecc6
Chan-Ho Suh <chanhosuh@gmail.com>
2021-10-02 00:42:07 -0400

    Check-in prod deploy script

--- scripts/alpha/demopools_upgrade.js
+++ scripts/alpha/demopools_upgrade.js
@@ -67,40 +67,60 @@ async function main(argv) {
   let maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
   const maxPriorityFeePerGas = parseInt(2.5e9);
 
-  const PoolTokenV2 = await ethers.getContractFactory("PoolTokenV2", deployer);
-  // Initialize logic storage to block possible attack vector:
-  // attacker may control and selfdestruct the logic contract
-  // if more powerful functionality is added later
-  const poolTokenV2 = await PoolTokenV2.connect(safeSigner).deploy({
-    maxFeePerGas,
-    maxPriorityFeePerGas,
-  });
+  //const PoolTokenV2 = await ethers.getContractFactory("PoolTokenV2", deployer);
+  //// Initialize logic storage to block possible attack vector:
+  //// attacker may control and selfdestruct the logic contract
+  //// if more powerful functionality is added later
+  //const poolTokenV2 = await PoolTokenV2.connect(safeSigner).deploy({
+  //  maxFeePerGas,
+  //  maxPriorityFeePerGas,
+  //});
 
-  const deploy_data = {};
-  deploy_data["PoolTokenV2"] = poolTokenV2.address;
-  updateDeployJsons(networkName, deploy_data);
-  const poolTokenV2Address = poolTokenV2.address;
+  //const deploy_data = {};
+  //deploy_data["PoolTokenV2"] = poolTokenV2.address;
+  //updateDeployJsons(networkName, deploy_data);
+  //const poolTokenV2Address = poolTokenV2.address;
+  const poolTokenV2Address = "0x712441D70782e284E46FEe73382EF70B2760326B";
 
-  console.log("USER ACTION REQUIRED");
-  console.log("Go to the Gnosis Safe Web App to confirm the transaction");
-  await poolTokenV2.deployed();
-  console.log("Deployed.");
+  // console.log("USER ACTION REQUIRED");
+  // console.log("Go to the Gnosis Safe Web App to confirm the transaction");
+  // await poolTokenV2.deployed();
+  // console.log("Deployed.");
 
-  feeData = await ethers.provider.getFeeData();
-  maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
+  // const safeTxHash = poolTokenV2.deployTransaction.hash;
+  // const safeTxHash =
+  //   "0x5cc53ed4b4cfafce16d4454254a5e17e3d18f6ccfd9f5e20f3fba91dece068c2";
+  // const txDetails = await service.getSafeTxDetails(safeTxHash);
+  // const txHash = txDetails.transactionHash;
 
-  let tx = await poolTokenV2
-    .connect(safeSigner)
-    .initialize(
-      "0x792da6df6bbdcc84c23235a6bef43921d81169b7",
-      "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-      "0xcafecafecafecafecafecafecafecafecafecafe",
-      { maxFeePerGas, maxPriorityFeePerGas }
-    );
-  console.log("USER ACTION REQUIRED");
-  console.log("Go to the Gnosis Safe Web App to confirm the transaction");
-  await tx.wait();
-  console.log("Initialized.");
+  // if (["KOVAN", "MAINNET"].includes(networkName)) {
+  //   console.log("Verifying on Etherscan ...");
+  //   await ethers.provider.waitForTransaction(txHash, 5); // wait for Etherscan to catch up
+  //   await hre.run("verify:verify", {
+  //     address: poolTokenV2Address,
+  //   });
+  //   console.log("");
+  // }
+
+  // feeData = await ethers.provider.getFeeData();
+  // maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
+
+  // const poolTokenV2 = await ethers.getContractAt(
+  //   "PoolTokenV2",
+  //   poolTokenV2Address
+  // );
+  // let tx = await poolTokenV2
+  //   .connect(safeSigner)
+  //   .initialize(
+  //     "0x792da6df6bbdcc84c23235a6bef43921d81169b7",
+  //     "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+  //     "0xcafecafecafecafecafecafecafecafecafecafe",
+  //     { maxFeePerGas, maxPriorityFeePerGas }
+  //   );
+  // console.log("USER ACTION REQUIRED");
+  // console.log("Go to the Gnosis Safe Web App to confirm the transaction");
+  // await tx.wait();
+  // console.log("Initialized.");
 
   const proxyAdmin = await ethers.getContractAt(
     "ProxyAdmin",
@@ -116,21 +136,21 @@ async function main(argv) {
     addressRegistryAddress
   );
 
-  feeData = await ethers.provider.getFeeData();
-  maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
-
-  const daiPoolAddress = await addressRegistry.getAddress(
-    bytes32("daiDemoPool")
-  );
-  tx = await proxyAdmin
-    .connect(safeSigner)
-    .upgrade(daiPoolAddress, poolTokenV2Address, {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-    });
-  console.log("USER ACTION REQUIRED");
-  console.log("Go to the Gnosis Safe Web App to confirm the transaction");
-  await tx.wait();
+  //   feeData = await ethers.provider.getFeeData();
+  //   maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
+  //
+  //   const daiPoolAddress = await addressRegistry.getAddress(
+  //     bytes32("daiDemoPool")
+  //   );
+  //   tx = await proxyAdmin
+  //     .connect(safeSigner)
+  //     .upgrade(daiPoolAddress, poolTokenV2Address, {
+  //       maxFeePerGas,
+  //       maxPriorityFeePerGas,
+  //     });
+  //   console.log("USER ACTION REQUIRED");
+  //   console.log("Go to the Gnosis Safe Web App to confirm the transaction");
+  //   await tx.wait();
 
   feeData = await ethers.provider.getFeeData();
   maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
@@ -138,7 +158,7 @@ async function main(argv) {
   const usdcPoolAddress = await addressRegistry.getAddress(
     bytes32("usdcDemoPool")
   );
-  tx = await proxyAdmin
+  const tx = await proxyAdmin
     .connect(safeSigner)
     .upgrade(usdcPoolAddress, poolTokenV2Address, {
       maxFeePerGas,
@@ -148,34 +168,22 @@ async function main(argv) {
   console.log("Go to the Gnosis Safe Web App to confirm the transaction");
   await tx.wait();
 
-  feeData = await ethers.provider.getFeeData();
-  maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
-
-  const usdtPoolAddress = await addressRegistry.getAddress(
-    bytes32("usdtDemoPool")
-  );
-  tx = await proxyAdmin
-    .connect(safeSigner)
-    .upgrade(usdtPoolAddress, poolTokenV2Address, {
-      maxFeePerGas,
-      maxPriorityFeePerGas,
-    });
-  console.log("USER ACTION REQUIRED");
-  console.log("Go to the Gnosis Safe Web App to confirm the transaction");
-  await tx.wait();
-
-  const safeTxHash = poolTokenV2.deployTransaction.hash;
-  const txDetails = await service.getSafeTxDetails(safeTxHash);
-  const txHash = txDetails.transactionHash;
-
-  if (["KOVAN", "MAINNET"].includes(networkName)) {
-    console.log("Verifying on Etherscan ...");
-    await ethers.provider.waitForTransaction(txHash, 5); // wait for Etherscan to catch up
-    await hre.run("verify:verify", {
-      address: poolTokenV2.address,
-    });
-    console.log("");
-  }
+  //   feeData = await ethers.provider.getFeeData();
+  //   maxFeePerGas = feeData.maxFeePerGas.mul(85).div(100);
+  //
+  //   const usdtPoolAddress = await addressRegistry.getAddress(
+  //     bytes32("usdtDemoPool")
+  //   );
+  //   tx = await proxyAdmin
+  //     .connect(safeSigner)
+  //     .upgrade(usdtPoolAddress, poolTokenV2Address, {
+  //       maxFeePerGas,
+  //       maxPriorityFeePerGas,
+  //     });
+  //   console.log("USER ACTION REQUIRED");
+  //   console.log("Go to the Gnosis Safe Web App to confirm the transaction");
+  //   await tx.wait();
+  //
 }
 
 if (!module.parent) {
