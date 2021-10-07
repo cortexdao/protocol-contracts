180b2300a4ead4844cccd8c25a2230de4697d923
opz <will.shahda@gmail.com>
2021-10-06 21:25:32 -0400

    E2e tests for deploying liquidity for each token

--- test-e2e/funding.js
+++ test-e2e/funding.js
@@ -203,7 +203,11 @@ describe("Funding scenarios", () => {
       const prevPoolMapt = await metaPoolToken.balanceOf(daiPool.address);
       expect(prevPoolMapt).to.equal(0);
 
-      const pools = [bytes32("daiPool")];
+      const pools = [
+        bytes32("daiPool"),
+        bytes32("usdcPool"),
+        bytes32("usdtPool"),
+      ];
       await metaPoolToken.connect(lpSafe).fundLpAccount(pools);
 
       const newPoolBalance = await daiToken.balanceOf(daiPool.address);
@@ -242,124 +246,162 @@ describe("Funding scenarios", () => {
       const poolTests = [
         {
           name: "curve-3pool",
-          amounts: [tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 3,
+          decimals: [18, 6, 6],
+          ignoreIndexes: [],
           gaugeAddress: "0xbFcF63294aD7105dEa65aA58F8AE5BE2D9d0952A",
         },
         {
           name: "curve-aave",
-          amounts: [tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 3,
+          decimals: [18, 6, 6],
+          ignoreIndexes: [],
           gaugeAddress: "0xd662908ADA2Ea1916B3318327A97eB18aD588b5d",
         },
         {
           name: "curve-alusd",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0x9582C4ADACB3BCE56Fea3e590F05c3ca2fb9C477",
         },
         {
           name: "curve-busdv2",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0xd4B22fEdcA85E684919955061fDf353b9d38389b",
         },
         {
           name: "curve-compound",
-          amounts: [tokenAmountToBigNumber("1000", 18), 0],
+          nCoins: 2,
+          decimals: [18, 6],
+          ignoreIndexes: [],
           gaugeAddress: "0x7ca5b0a2910B33e9759DC7dDB0413949071D7575",
         },
         {
           name: "curve-frax",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0x72E158d38dbd50A483501c24f792bDAAA3e7D55C",
         },
         {
           name: "curve-ironbank",
-          amounts: [tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 3,
+          decimals: [18, 6, 6],
           gaugeAddress: "0xF5194c3325202F456c95c1Cf0cA36f8475C1949F",
         },
         {
           name: "curve-lusd",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0x9B8519A9a00100720CCdC8a120fBeD319cA47a14",
         },
         {
           name: "curve-musd",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0x5f626c30EC1215f4EdCc9982265E8b1F411D1352",
         },
         //{
         //  name: "curve-saave",
-        //  amounts: [tokenAmountToBigNumber("1000", 18), 0],
+        //  nCoins: 3,
+        //  decimals: [18, 18],
+        //  ignoreIndexes: [1],
         //  gaugeAddress: "0x462253b8F74B72304c145DB0e4Eebd326B22ca39",
         //},
         {
           name: "curve-susdv2",
-          amounts: [tokenAmountToBigNumber("1000", 18), 0, 0, 0],
+          nCoins: 4,
+          decimals: [18, 6, 6, 18],
+          ignoreIndexes: [3],
           gaugeAddress: "0xA90996896660DEcC6E997655E065b23788857849",
         },
         {
           name: "curve-usdn",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0xF98450B5602fa59CC66e1379DFfB6FDDc724CfC4",
         },
         {
           name: "curve-usdp",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0x055be5DDB7A925BfEF3417FC157f53CA77cA7222",
         },
         {
           name: "curve-ust",
-          amounts: [0, tokenAmountToBigNumber("1000", 18), 0, 0],
+          nCoins: 4,
+          decimals: [18, 18, 6, 6],
+          ignoreIndexes: [0],
           gaugeAddress: "0x3B7020743Bc2A4ca9EaF9D0722d42E20d6935855",
         },
       ];
 
-      poolTests.forEach(({ name, amounts, gaugeAddress }) => {
-        describe(name, () => {
-          let subsuiteSnapshotId;
+      poolTests.forEach(
+        ({ name, nCoins, decimals, ignoreIndexes, gaugeAddress }) => {
+          describe(name, () => {
+            let subsuiteSnapshotId;
 
-          before(async () => {
-            const snapshot = await timeMachine.takeSnapshot();
-            subsuiteSnapshotId = snapshot["result"];
-          });
-
-          after(async () => {
-            await timeMachine.revertToSnapshot(subsuiteSnapshotId);
-          });
-
-          it("Should deploy to the pool", async () => {
-            //const amounts = [tokenAmountToBigNumber("1000", 18), 0, 0];
-
-            await lpAccount.connect(lpSafe).deployStrategy(name, amounts);
-          });
-
-          describe("Should unwind each token from the pool", () => {
-            let testSnapshotId;
-
-            beforeEach(async () => {
+            before(async () => {
               const snapshot = await timeMachine.takeSnapshot();
-              testSnapshotId = snapshot["result"];
+              subsuiteSnapshotId = snapshot["result"];
             });
 
-            afterEach(async () => {
-              await timeMachine.revertToSnapshot(testSnapshotId);
+            after(async () => {
+              await timeMachine.revertToSnapshot(subsuiteSnapshotId);
             });
 
-            _.range(amounts.length).forEach((index) => {
-              it(`token ${index}`, async () => {
-                const gauge = await ethers.getContractAt(
-                  "ILiquidityGauge",
-                  gaugeAddress
-                );
+            _.range(nCoins).forEach((index) => {
+              if (!_.includes(ignoreIndexes, index)) {
+                it(`Should deploy to token ${index} the pool`, async () => {
+                  //const amounts = [tokenAmountToBigNumber("1000", 18), 0, 0];
 
-                const amount = await gauge.balanceOf(lpAccount.address);
+                  const amounts = _.times(nCoins, _.constant(0));
+                  amounts[index] = tokenAmountToBigNumber(
+                    "1000",
+                    decimals[index]
+                  );
+                  await lpAccount.connect(lpSafe).deployStrategy(name, amounts);
+                });
+              }
+            });
 
-                await lpAccount
-                  .connect(lpSafe)
-                  .unwindStrategy(name, amount, index);
+            describe("Should unwind each token from the pool", () => {
+              let testSnapshotId;
+
+              beforeEach(async () => {
+                const snapshot = await timeMachine.takeSnapshot();
+                testSnapshotId = snapshot["result"];
+              });
+
+              afterEach(async () => {
+                await timeMachine.revertToSnapshot(testSnapshotId);
+              });
+
+              _.range(nCoins).forEach((index) => {
+                it(`token ${index}`, async () => {
+                  const gauge = await ethers.getContractAt(
+                    "ILiquidityGauge",
+                    gaugeAddress
+                  );
+
+                  const amount = await gauge.balanceOf(lpAccount.address);
+
+                  await lpAccount
+                    .connect(lpSafe)
+                    .unwindStrategy(name, amount, index);
+                });
               });
             });
           });
-        });
-      });
+        }
+      );
     });
   });
 });
