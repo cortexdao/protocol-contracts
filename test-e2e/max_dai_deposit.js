173a4434d444008b849bbaeb09e1cf049d74c7d5
Chan-Ho Suh <chanhosuh@gmail.com>
2021-11-03 17:37:45 -0400

    Comment-out USDN from e2e tests

---
+++ test-e2e/max_dai_deposit.js
@@ -0,0 +1,42 @@
+const hre = require("hardhat");
+const {ethers} = hre;
+const {tokenAmountToBigNumber, impersonateAccount, getStablecoinAddress, forciblySendEth} = require("../utils/helpers")
+
+async function main() {
+  const [deployer] = await ethers.getSigners()
+
+  const lpSafe = await impersonateAccount("0x5b79121EA6dC2395B8046eCDCE14D66c2bF221B0")
+  const lpAccount = await ethers.getContractAt("LpAccount", "0x026544DACFAfC3422A0219cb6Be03ecB5D99a771", lpSafe)
+  await forciblySendEth(
+    lpSafe, 
+    tokenAmountToBigNumber(10),
+    deployer
+  )
+
+
+  const DAI_ADDRESS = getStablecoinAddress("DAI", "MAINNET")
+  const daiToken = await ethers.getContractAt("IDetailedERC20", DAI_ADDRESS)
+  const daiBalance = await daiToken.balanceOf(lpAccount.address)
+  console.log("DAI balance: %s", await daiToken.balanceOf(lpAccount.address))
+
+  const name = "curve-saave"
+
+  const amounts = [daiBalance, 0]
+  await lpAccount.deployStrategy(name, amounts)
+
+  console.log("New DAI balance: %s", await daiToken.balanceOf(lpAccount.address))
+}
+
+if (!module.parent) {
+  main()
+    .then(() => {
+      process.exit(0);
+    })
+    .catch((error) => {
+      console.error(error);
+      console.log("");
+      process.exit(1);
+    });
+} else {
+  module.exports = main;
+}
