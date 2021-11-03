eb41809602fe7b7025f70908798305f3510ff815
Chan-Ho Suh <chanhosuh@gmail.com>
2021-11-03 15:27:10 -0400

    Fix linting errors

--- contracts/lpaccount/IStableSwap3Pool.sol
+++ contracts/lpaccount/IStableSwap3Pool.sol
@@ -5,18 +5,19 @@ pragma solidity 0.6.11;
  * @notice the stablecoin pool contract
  */
 interface IStableSwap3Pool {
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy // solhint-disable-line func-param-name-mixedcase
+    ) external;
+
     function coins(uint256 coin) external view returns (address);
 
+    // solhint-disable-next-line func-name-mixedcase
     function get_dy(
         int128 i,
         int128 j,
         uint256 dx
     ) external view returns (uint256);
-
-    function exchange(
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 min_dy
-    ) external;
 }
