cc7d55a7a2eb468341bec35469629aeb071bc74d
Chan-Ho Suh <chanhosuh@gmail.com>
2021-11-14 02:38:15 -0500

    Fix shadowing issue in fix

--- contracts/lpaccount/LpAccountTempStorageFix.sol
+++ contracts/lpaccount/LpAccountTempStorageFix.sol
@@ -57,7 +57,7 @@ contract LpAccountTempStorageFix is
         IStableSwap3Pool(0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7);
     uint256 private constant _DEFAULT_LOCK_PERIOD = 135;
 
-    address public proxyAdmin;
+    address internal _dummySlot;
     IAddressRegistryV2 public addressRegistry;
     uint256 public lockPeriod;
 
