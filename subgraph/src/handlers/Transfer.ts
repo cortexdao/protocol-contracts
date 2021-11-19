1789bf642e957dbe49dfe8ece29e85369079aa4a
Chan-Ho Suh <chanhosuh@gmail.com>
2021-11-19 14:33:00 -0500

    Fix transfer event handler for V1

--- subgraph/src/handlers/Transfer.ts
+++ subgraph/src/handlers/Transfer.ts
@@ -1,84 +1,103 @@
 import {
-  PoolTokenV2,
-  Transfer as TransferEvent,
+    PoolTokenV2,
+    Transfer as TransferEvent,
 } from "../../generated/DAI_PoolToken/PoolTokenV2";
 import { IDetailedERC20 } from "../../generated/DAI_PoolToken/IDetailedERC20";
+import { AggregatorV3Interface } from "../../generated/DAI_PoolToken/AggregatorV3Interface";
 import { Transfer, User, Pool } from "../../generated/schema";
-import { BigInt } from "@graphprotocol/graph-ts";
+import { Address, BigInt, dataSource } from "@graphprotocol/graph-ts";
+import {
+    getEthUsdAggregator,
+    getPriceFromAgg,
+    getStableUsdAggregator,
+} from "../utils/chainlink";
 
 export function handleTransfer(event: TransferEvent): void {
-  const poolAddress = event.address;
-  const toAddress = event.params.to;
-  const fromAddress = event.params.from;
+    const poolAddress = event.address;
+    const toAddress = event.params.to;
+    const fromAddress = event.params.from;
 
-  let transfer = new Transfer(
-    fromAddress.toHexString() +
-      toAddress.toHexString() +
-      event.block.timestamp.toString() +
-      event.logIndex.toString() +
-      event.transaction.hash.toHexString()
-  );
+    let transfer = new Transfer(
+        fromAddress.toHexString() +
+            toAddress.toHexString() +
+            event.block.timestamp.toString() +
+            event.logIndex.toString() +
+            event.transaction.hash.toHexString()
+    );
 
-  transfer.poolAddress = poolAddress;
-  transfer.from = fromAddress;
-  transfer.to = toAddress;
-  transfer.value = event.params.value;
-  transfer.save();
+    transfer.poolAddress = poolAddress;
+    transfer.from = fromAddress;
+    transfer.to = toAddress;
+    transfer.value = event.params.value;
+    transfer.save();
 
-  const contract = PoolTokenV2.bind(poolAddress);
-  const underlyer = IDetailedERC20.bind(contract.underlyer());
+    const poolToken = PoolTokenV2.bind(poolAddress);
+    const underlyer = IDetailedERC20.bind(poolToken.underlyer());
+    const totalValueResult = poolToken.try_getPoolTotalValue();
+    let version = 2;
+    if (totalValueResult.reverted) {
+        version = 1;
+    }
 
-  const priceResult = contract.try_getUnderlyerPrice();
-  let price = BigInt.fromI32(0);
-  if (!priceResult.reverted) {
-    price = priceResult.value;
-  }
-  const totalValueResult = contract.try_getPoolTotalValue();
-  let totalValue = BigInt.fromI32(0);
-  if (!totalValueResult.reverted) {
-    totalValue = totalValueResult.value;
-  }
+    const symbol = underlyer.symbol();
+    const decimals = underlyer.decimals() as u8;
+    const underlyerPriceAgg = getStableUsdAggregator(
+        dataSource.network(),
+        symbol,
+        version
+    );
+    const ethUsdAgg: AggregatorV3Interface = getEthUsdAggregator(
+        dataSource.network()
+    );
 
-  const poolId = poolAddress.toHexString();
-  const pool = Pool.load(poolId) || new Pool(poolId);
-  pool.underlyerPrice = price;
-  pool.underlyerSymbol = underlyer.symbol();
-  pool.underlyerDecimals = underlyer.decimals();
-  pool.underlyerBalance = underlyer.balanceOf(poolAddress);
-  pool.totalValue = totalValue;
-  pool.address = poolAddress;
-  pool.aptSupply = contract.totalSupply();
-  pool.save();
+    // Ensure underlyer is priced in USD as in V1 it is priced in ETH
+    let underlyerPrice = getPriceFromAgg(underlyerPriceAgg);
+    if (underlyerPriceAgg.decimals() == 18) {
+        const ethUsdPrice = getPriceFromAgg(ethUsdAgg);
+        underlyerPrice = underlyerPrice
+            .times(ethUsdPrice)
+            .div(BigInt.fromI32(10).pow(18 as u8));
+    } else if (underlyerPriceAgg.decimals() != 8) {
+        throw new Error("Price aggregator decimals must be 18 or 8.");
+    }
 
-  const toUserId = toAddress.toHexString() + poolAddress.toHexString();
-  const toUser = User.load(toUserId) || new User(toUserId);
-  toUser.poolAddress = poolAddress;
-  toUser.address = toAddress;
+    let totalValue = BigInt.fromI32(0);
+    // In V2, the total value in USD is retrieved by `getPoolTotalValue`.
+    // In V1, the total value is just the USD value of the underlyer
+    // balance of the pool.
+    if (version == 2) {
+        totalValue = totalValueResult.value;
+    } else {
+        const underlyerBalance = underlyer.balanceOf(poolAddress);
+        totalValue = underlyerBalance
+            .times(underlyerPrice)
+            .div(BigInt.fromI32(10).pow(decimals));
+    }
 
-  let balance = contract.balanceOf(toAddress);
-  let result = contract.try_getAPTValue(balance);
-  let ethValue = BigInt.fromI32(0);
-  if (!result.reverted) {
-    ethValue = result.value;
-  }
+    const poolId = poolAddress.toHexString();
+    const pool = Pool.load(poolId) || new Pool(poolId);
+    pool.underlyerPrice = underlyerPrice;
+    pool.underlyerSymbol = underlyer.symbol();
+    pool.underlyerDecimals = underlyer.decimals();
+    pool.underlyerBalance = underlyer.balanceOf(poolAddress);
+    pool.totalValue = totalValue;
+    pool.address = poolAddress;
+    pool.aptSupply = poolToken.totalSupply();
+    pool.save();
 
-  toUser.accountBalance = balance;
-  toUser.accountValue = ethValue;
-  toUser.save();
+    const toUserId = toAddress.toHexString() + poolAddress.toHexString();
+    const toUser = User.load(toUserId) || new User(toUserId);
+    toUser.poolAddress = poolAddress;
+    toUser.address = toAddress;
+    let balance = poolToken.balanceOf(toAddress);
+    toUser.accountBalance = balance;
+    toUser.save();
 
-  const fromUserId = fromAddress.toHexString() + poolAddress.toHexString();
-  const fromUser = User.load(fromUserId) || new User(fromUserId);
-  fromUser.poolAddress = poolAddress;
-  fromUser.address = fromAddress;
-
-  balance = contract.balanceOf(fromAddress);
-  result = contract.try_getAPTValue(balance);
-  ethValue = BigInt.fromI32(0);
-  if (!result.reverted) {
-    ethValue = result.value;
-  }
-
-  fromUser.accountBalance = balance;
-  fromUser.accountValue = ethValue;
-  fromUser.save();
+    const fromUserId = fromAddress.toHexString() + poolAddress.toHexString();
+    const fromUser = User.load(fromUserId) || new User(fromUserId);
+    fromUser.poolAddress = poolAddress;
+    fromUser.address = fromAddress;
+    balance = poolToken.balanceOf(fromAddress);
+    fromUser.accountBalance = balance;
+    fromUser.save();
 }
