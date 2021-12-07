b25ef133b8b695fbf9d4cc20c6fdbb8d4c0724d7
opz <will.shahda@gmail.com>
2021-12-06 23:03:19 -0500

    Use newer block for curve-ousd tests

--- test-integration/CurveMetaPoolZaps-NewBlock.js
+++ test-integration/CurveMetaPoolZaps-NewBlock.js
@@ -15,13 +15,17 @@ const { WHALE_POOLS } = require("../utils/constants");
 
 const CRV_ADDRESS = "0xD533a949740bb3306d119CC777fa900bA034cd52";
 
+const pinnedBlock = 13755900;
+const defaultPinnedBlock = hre.config.networks.hardhat.forking.blockNumber;
+const forkingUrl = hre.config.networks.hardhat.forking.url;
+
 /* ************************ */
 /* set DEBUG log level here */
 /* ************************ */
 console.debugging = false;
 /* ************************ */
 
-describe("Curve MetaPool Zaps - LP Account integration", () => {
+describe("Curve MetaPool Zaps (Updated block) - LP Account integration", () => {
   /* signers */
   let deployer;
   let emergencySafe;
@@ -41,86 +45,15 @@ describe("Curve MetaPool Zaps - LP Account integration", () => {
 
   const CurveMetaPoolZaps = [
     {
-      contractName: "CurveAlusdZap",
-      swapAddress: "0x43b4FdFD4Ff969587185cDB6f0BD875c5Fc83f8c",
+      contractName: "CurveOusdZap",
+      swapAddress: "0x87650D7bbfC3A9F10587d7778206671719d9910D",
       swapInterface: "IMetaPool",
-      lpTokenAddress: "0x43b4FdFD4Ff969587185cDB6f0BD875c5Fc83f8c",
-      gaugeAddress: "0x9582C4ADACB3BCE56Fea3e590F05c3ca2fb9C477",
+      lpTokenAddress: "0x87650D7bbfC3A9F10587d7778206671719d9910D",
+      gaugeAddress: "0x25f0cE4E2F8dbA112D9b115710AC297F816087CD",
       gaugeInterface: "ILiquidityGauge",
       numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["ALUSD"],
-      rewardToken: "0xdBdb4d16EdA451D0503b854CF79D55697F90c8DF",
-    },
-    {
-      contractName: "CurveBusdv2Zap",
-      swapAddress: "0x4807862AA8b2bF68830e4C8dc86D0e9A998e085a",
-      swapInterface: "IMetaPool",
-      lpTokenAddress: "0x4807862AA8b2bF68830e4C8dc86D0e9A998e085a",
-      gaugeAddress: "0xd4B22fEdcA85E684919955061fDf353b9d38389b",
-      gaugeInterface: "ILiquidityGauge",
-      numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["BUSD"],
-    },
-    {
-      contractName: "CurveFraxZap",
-      swapAddress: "0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B",
-      swapInterface: "IMetaPool",
-      lpTokenAddress: "0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B",
-      gaugeAddress: "0x72E158d38dbd50A483501c24f792bDAAA3e7D55C",
-      gaugeInterface: "ILiquidityGauge",
-      numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["FRAX"],
-      rewardToken: "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0",
-    },
-    {
-      contractName: "CurveLusdZap",
-      swapAddress: "0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA",
-      swapInterface: "IMetaPool",
-      lpTokenAddress: "0xEd279fDD11cA84bEef15AF5D39BB4d4bEE23F0cA",
-      gaugeAddress: "0x9B8519A9a00100720CCdC8a120fBeD319cA47a14",
-      gaugeInterface: "ILiquidityGauge",
-      numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["LUSD"],
-    },
-    {
-      contractName: "CurveMusdZap",
-      swapAddress: "0x8474DdbE98F5aA3179B3B3F5942D724aFcdec9f6",
-      swapInterface: "IMetaPool",
-      lpTokenAddress: "0x1AEf73d49Dedc4b1778d0706583995958Dc862e6",
-      gaugeAddress: "0x5f626c30EC1215f4EdCc9982265E8b1F411D1352",
-      gaugeInterface: "ILiquidityGauge",
-      numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["MUSD"],
-    },
-    // {
-    //   contractName: "CurveUsdnZap",
-    //   swapAddress: "0x0f9cb53Ebe405d49A0bbdBD291A65Ff571bC83e1",
-    //   swapInterface: "IMetaPool",
-    //   lpTokenAddress: "0x4f3E8F405CF5aFC05D68142F3783bDfE13811522",
-    //   gaugeAddress: "0xF98450B5602fa59CC66e1379DFfB6FDDc724CfC4",
-    //   gaugeInterface: "ILiquidityGauge",
-    //   numberOfCoins: 4,
-    //   whaleAddress: WHALE_POOLS["USDN"],
-    // },
-    {
-      contractName: "CurveUsdpZap",
-      swapAddress: "0x42d7025938bEc20B69cBae5A77421082407f053A",
-      swapInterface: "IMetaPool",
-      lpTokenAddress: "0x7Eb40E450b9655f4B3cC4259BCC731c63ff55ae6",
-      gaugeAddress: "0x055be5DDB7A925BfEF3417FC157f53CA77cA7222",
-      gaugeInterface: "ILiquidityGauge",
-      numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["USDP"],
-    },
-    {
-      contractName: "CurveUstZap",
-      swapAddress: "0x890f4e345B1dAED0367A877a1612f86A1f86985f",
-      swapInterface: "IMetaPool",
-      lpTokenAddress: "0x94e131324b6054c0D789b190b2dAC504e4361b53",
-      gaugeAddress: "0x3B7020743Bc2A4ca9EaF9D0722d42E20d6935855",
-      gaugeInterface: "ILiquidityGauge",
-      numberOfCoins: 4,
-      whaleAddress: WHALE_POOLS["UST"],
+      whaleAddress: WHALE_POOLS["OUSD"],
+      rewardToken: "0x8207c1FfC5B6804F6024322CcF34F29c3541Ae26",
     },
   ];
 
@@ -161,6 +94,28 @@ describe("Curve MetaPool Zaps - LP Account integration", () => {
 
   afterEach(async () => {
     await timeMachine.revertToSnapshot(snapshotId);
+  });
+
+  before("Use pinned block for new zaps", async () => {
+    await hre.network.provider.send("hardhat_reset", [
+      {
+        forking: {
+          jsonRpcUrl: forkingUrl,
+          blockNumber: pinnedBlock,
+        },
+      },
+    ]);
+  });
+
+  after("Reset pinned block", async () => {
+    await hre.network.provider.send("hardhat_reset", [
+      {
+        forking: {
+          jsonRpcUrl: forkingUrl,
+          blockNumber: defaultPinnedBlock,
+        },
+      },
+    ]);
   });
 
   before("Setup mock address registry", async () => {
