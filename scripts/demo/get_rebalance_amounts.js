0b4bbfd4725d2ff95b0b79974cec42de10eb915c
opz <will.shahda@gmail.com>
2021-12-30 01:57:22 -0500

    Show what to unwind for reserve pool rebalance

--- scripts/demo/get_rebalance_amounts.js
+++ scripts/demo/get_rebalance_amounts.js
@@ -2,6 +2,7 @@ const {
   impersonateAdminSafe,
   getPoolIds,
   getRegisteredContract,
+  getStablecoin,
   unlockOracleAdapter,
 } = require("../frontend/utils");
 const { formatUnits } = require("../../utils/helpers");
@@ -28,14 +29,34 @@ async function main() {
   amountsFormatted[1] = formatUnits(amounts[1][1], 6);
   amountsFormatted[2] = formatUnits(amounts[1][2], 6);
 
-  return amountsFormatted;
+  const lpAccount = await getRegisteredContract("lpAccount");
+
+  const dai = await getStablecoin("DAI");
+  const daiBalance = await dai.balanceOf(lpAccount.address);
+  const daiToUnwind = amounts[1][0].sub(daiBalance);
+
+  const usdc = await getStablecoin("USDC");
+  const usdcBalance = await usdc.balanceOf(lpAccount.address);
+  const usdcToUnwind = amounts[1][1].sub(usdcBalance);
+
+  const usdt = await getStablecoin("USDT");
+  const usdtBalance = await usdt.balanceOf(lpAccount.address);
+  const usdtToUnwind = amounts[1][2].sub(usdtBalance);
+
+  let toUnwindFormatted = [];
+  toUnwindFormatted[0] = formatUnits(daiToUnwind, 18);
+  toUnwindFormatted[1] = formatUnits(usdcToUnwind, 6);
+  toUnwindFormatted[2] = formatUnits(usdtToUnwind, 6);
+
+  return { amountsFormatted, toUnwindFormatted };
 }
 
 if (!module.parent) {
   main()
-    .then((amounts) => {
+    .then((amountsFormatted, toUnwindFormatted) => {
       console.log("");
-      console.log(amounts);
+      console.log(amountsFormatted);
+      console.log(toUnwindFormatted);
       console.log("");
       process.exit(0);
     })
