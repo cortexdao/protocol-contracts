0dbad323ce359bb2f2f208206d243ab8f626eb30
Chan-Ho Suh <chanhosuh@gmail.com>
2022-01-17 11:51:54 -0500

    Polish off rinkeby deploy script

--- scripts/rinkeby/voting_escrow_deploy.js
+++ scripts/rinkeby/voting_escrow_deploy.js
@@ -14,6 +14,8 @@ const NAME = "Boost-Locked APY";
 const SYMBOL = "blAPY";
 const VERSION = "1.0.0";
 
+const ADMIN_SAFE_ADDRESS = "0xacC66a1bD538cfCBB801FC047f41A3FC0AECf87a";
+
 // eslint-disable-next-line no-unused-vars
 async function main(argv) {
   await hre.run("compile");
@@ -30,7 +32,7 @@ async function main(argv) {
   console.log("Deployer address:", deployer.address);
   console.log("");
 
-  let balance =
+  const balance =
     (await ethers.provider.getBalance(deployer.address)).toString() / 1e18;
   console.log("ETH balance (deployer): %s", balance);
   console.log("");
@@ -46,7 +48,7 @@ async function main(argv) {
     contractName,
     deployer
   );
-  let votingEscrow = await contractFactory.deploy(
+  const votingEscrow = await contractFactory.deploy(
     APY_ADDRESS,
     NAME,
     SYMBOL,
@@ -55,15 +57,27 @@ async function main(argv) {
   console.log("Contract address: %s", votingEscrow.address);
 
   console.log("Waiting for 5 confirmations ...");
-  await ethers.provider.waitForTransaction(
-    votingEscrow.deployTransaction.hash,
-    5
-  );
-  console.log("Verifying on Etherscan ...");
-  await hre.run("verify:verify", {
-    address: votingEscrow.address,
-    constructorArguments: [APY_ADDRESS, NAME, SYMBOL, VERSION],
-  });
+  await votingEscrow.deployTransaction.wait(5);
+
+  console.log("Commit transfer ownership ...");
+  let tx = await votingEscrow
+    .connect(deployer)
+    .commit_transfer_ownership(ADMIN_SAFE_ADDRESS);
+  console.log("Waiting for 5 confirmations ...");
+  await tx.wait(5);
+
+  console.log("Apply transfer ownership ...");
+  tx = await votingEscrow.connect(deployer).apply_transfer_ownership();
+  console.log("Waiting for 5 confirmations ...");
+  await tx.wait(5);
+  console.log("Done.");
+
+  console.log("Verify manually on the etherscan website.");
+  // can't verify vyper contracts through hardhat; must do through
+  // Etherscan website using this bytecode for the constructor args:
+  //
+  // vyper version 0.2.4
+  // 000000000000000000000000a41d3d461b8a9f1e9f92d2b040495ce21cfed548000000000000000000000000000000000000000000000000000000000000008000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000010426f6f73742d4c6f636b656420415059000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005626c4150590000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000005312e302e30000000000000000000000000000000000000000000000000000000
 }
 
 if (!module.parent) {
