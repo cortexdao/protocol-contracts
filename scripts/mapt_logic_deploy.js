00a5096c74bb0f3cc8dacc73ca52e0bdd39a993a
Chan-Ho Suh <chanhosuh@gmail.com>
2022-01-21 11:21:22 -0500

    Cleanup mAPT deploy script

--- scripts/mapt_logic_deploy.js
+++ scripts/mapt_logic_deploy.js
@@ -5,17 +5,25 @@
  * $ HARDHAT_NETWORK=<network name> node scripts/<script filename> --arg1=val1 --arg2=val2
  */
 require("dotenv").config();
-const { argv } = require("yargs").option("compile", {
-  type: "boolean",
-  default: true,
-  description: "Compile contract using `compile:one`",
-});
+const { argv } = require("yargs")
+  .option("compile", {
+    type: "boolean",
+    default: true,
+    description: "Compile contract using `compile:one`",
+  })
+  .option("maxFeePerGas", {
+    type: "number",
+    description: "Gas price in gwei; omitting uses default Ethers logic",
+  })
+  .option("maxPriorityFeePerGas", {
+    type: "number",
+    description: "Gas price in gwei; omitting uses default Ethers logic",
+  });
 const hre = require("hardhat");
 const { ethers, network } = require("hardhat");
-const {
-  waitForSafeTxReceipt,
-  getAdminSafeSigner,
-} = require("../utils/helpers");
+const { getMaxFee, getMaxPriorityFee } = require("../utils/helpers");
+
+const ADDRESS_REGISTRY_ADDRESS = "0x7ec81b7035e91f8435bdeb2787dcbd51116ad303";
 
 // eslint-disable-next-line no-unused-vars
 async function main(argv) {
@@ -27,7 +35,14 @@ async function main(argv) {
   if (!process.env.SAFE_OWNER_KEY) {
     throw new Error("Must set SAFE_OWNER_KEY env var.");
   }
-  const safeSigner = await getAdminSafeSigner(networkName);
+  const safeOwner = new ethers.Wallet(
+    process.env.SAFE_OWNER_KEY,
+    ethers.provider
+  );
+  console.log("Deployer: %s", safeOwner.address);
+  const balance =
+    (await ethers.provider.getBalance(safeOwner.address)).toString() / 1e18;
+  console.log("ETH balance (Safe owner): %s", balance);
 
   const contractName = "MetaPoolTokenV2";
   console.log(`${contractName} deploy`);
@@ -39,24 +54,32 @@ async function main(argv) {
     await hre.run("compile:one", { contractName });
   }
 
+  const maxFeePerGas = await getMaxFee(argv.maxFeePerGas);
+  const maxPriorityFeePerGas = await getMaxPriorityFee(
+    argv.maxPriorityFeePerGas
+  );
+
   console.log("Deploying ... ");
   console.log("");
 
   const contractFactory = await ethers.getContractFactory(contractName);
-  let apy = await contractFactory.connect(safeSigner).deploy();
-  const receipt = await waitForSafeTxReceipt(
-    apy.deployTransaction,
-    safeSigner.service
-  );
-  const contractAddress = receipt.contractAddress;
-  if (!contractAddress) {
-    throw new Error("Contract address is missing.");
-  }
-  console.log("Contract address: %s", contractAddress);
+  const logicV2 = await contractFactory
+    .connect(safeOwner)
+    .deploy({ maxFeePerGas, maxPriorityFeePerGas });
+  await logicV2.deployTransaction.wait(5);
+
+  console.log("Contract address: %s", logicV2.address);
+
+  await logicV2
+    .connect(safeOwner)
+    .initialize(ADDRESS_REGISTRY_ADDRESS, {
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+    });
 
   console.log("Verifying on Etherscan ...");
   await hre.run("verify:verify", {
-    address: contractAddress,
+    address: logicV2.address,
   });
 }
 
