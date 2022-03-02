70409faa82fad008597d1687d2753ad8767a4aaf
Chan-Ho Suh <chanhosuh@gmail.com>
2022-03-02 15:16:33 -0500

    Add Curve allocation

---
+++ contracts/protocols/curve/ust-wormhole/Allocation.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: BUSDL-1.1
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+import {
+    MetaPoolAllocationBase
+} from "contracts/protocols/curve/metapool/Imports.sol";
+
+import {CurveUstWormholeConstants} from "./Constants.sol";
+
+contract CurveUstWormholeAllocation is
+    MetaPoolAllocationBase,
+    CurveUstWormholeConstants
+{
+    constructor(address curve3PoolAllocation_)
+        public
+        MetaPoolAllocationBase(curve3PoolAllocation_)
+    {} // solhint-disable-line no-empty-blocks
+
+    function balanceOf(address account, uint8 tokenIndex)
+        public
+        view
+        override
+        returns (uint256)
+    {
+        return
+            super.getUnderlyerBalance(
+                account,
+                META_POOL,
+                LIQUIDITY_GAUGE,
+                LP_TOKEN,
+                uint256(tokenIndex)
+            );
+    }
+
+    function _getTokenData()
+        internal
+        pure
+        override
+        returns (TokenData[] memory)
+    {
+        return _getBasePoolTokenData(address(PRIMARY_UNDERLYER), "UST", 6);
+    }
+}
