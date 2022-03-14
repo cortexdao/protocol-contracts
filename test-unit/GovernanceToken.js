7b956d6bfde625f00eba044f4dde5cbb45ccd8fb
Chan-Ho Suh <chanhosuh@gmail.com>
2022-03-14 19:15:59 -0400

    Start V2; update test deploy; cleanups

--- test-unit/GovernanceToken.js
+++ test-unit/GovernanceToken.js
@@ -8,12 +8,14 @@ const { ZERO_ADDRESS } = require("@openzeppelin/test-helpers/src/constants");
 const ProxyAdmin = artifacts.require("ProxyAdmin");
 const GovernanceTokenProxy = artifacts.require("GovernanceTokenProxy");
 const GovernanceToken = artifacts.require("GovernanceToken");
+const GovernanceTokenV2 = artifacts.require("GovernanceTokenV2");
 
 contract("GovernanceToken Unit Test", async (accounts) => {
   const [owner, instanceAdmin, randomUser] = accounts;
 
   let proxyAdmin;
   let logic;
+  let logicV2;
   let proxy;
   let instance;
 
@@ -34,6 +36,7 @@ contract("GovernanceToken Unit Test", async (accounts) => {
   before(async () => {
     proxyAdmin = await ProxyAdmin.new({ from: owner });
     logic = await GovernanceToken.new({ from: owner });
+    logicV2 = await GovernanceToken.new({ from: owner });
     proxy = await GovernanceTokenProxy.new(
       logic.address,
       proxyAdmin.address,
@@ -42,66 +45,69 @@ contract("GovernanceToken Unit Test", async (accounts) => {
         from: owner,
       }
     );
-    instance = await GovernanceToken.at(proxy.address);
+    await proxyAdmin.upgrade(proxy.address, logicV2.address, { from: owner });
+    instance = await GovernanceTokenV2.at(proxy.address);
   });
 
-  describe("Test Constructor", async () => {
-    it("Test params invalid admin", async () => {
+  describe("Constructor", async () => {
+    it("Revert on invalid admin address", async () => {
       await expectRevert.unspecified(
         GovernanceTokenProxy.new(logic.address, ZERO_ADDRESS, totalSupply, {
           from: owner,
         })
       );
     });
-
-    it("Test totalSupply is set", async () => {
-      expect(await instance.totalSupply.call()).to.bignumber.equal(totalSupply);
-    });
-
-    it("Test owner has total supply", async () => {
-      expect(await instance.balanceOf.call(owner)).to.bignumber.equal(
-        await instance.totalSupply.call()
-      );
-    });
   });
 
-  describe("Test Defaults", async () => {
-    it("Test Owner", async () => {
-      assert.equal(await instance.owner.call(), owner);
+  describe("initialize", async () => {
+    it("Owner set correctly", async () => {
+      assert.equal(await instance.owner(), owner);
     });
 
-    it("Test Pool Token Name", async () => {
-      assert.equal(await instance.name.call(), "APY Governance Token");
+    it("Name set correctly", async () => {
+      assert.equal(await instance.name(), "APY Governance Token");
     });
 
-    it("Test Pool Symbol", async () => {
-      assert.equal(await instance.symbol.call(), "APY");
+    it("Symbol set correctly", async () => {
+      assert.equal(await instance.symbol(), "APY");
     });
 
-    it("Test Pool Decimals", async () => {
-      assert.equal(await instance.decimals.call(), 18);
+    it("Decimals set correctly", async () => {
+      assert.equal(await instance.decimals(), 18);
     });
 
-    it("Test sending Ether", async () => {
+    it("Revert on sent ETH", async () => {
       await expectRevert(instance.send(10), "DONT_SEND_ETHER");
     });
-  });
 
-  describe("Test setAdminAdddress", async () => {
-    it("Test setAdminAddress pass", async () => {
-      await instance.setAdminAddress(instanceAdmin, { from: owner });
-      assert.equal(await instance.proxyAdmin.call(), instanceAdmin);
+    it("totalSupply is set correctly", async () => {
+      expect(await instance.totalSupply()).to.bignumber.equal(totalSupply);
     });
 
-    it("Test setAdminAddress invalid admin", async () => {
-      await expectRevert.unspecified(
-        instance.setAdminAddress(ZERO_ADDRESS, { from: owner })
+    it("Owner has total supply", async () => {
+      expect(await instance.balanceOf(owner)).to.bignumber.equal(
+        await instance.totalSupply()
+      );
+    });
+  });
+
+  describe("setAdminAdddress", async () => {
+    it("Owner can set", async () => {
+      await instance.setAdminAddress(instanceAdmin, { from: owner });
+      assert.equal(await instance.proxyAdmin(), instanceAdmin);
+    });
+
+    it("Revert on invalid admin address", async () => {
+      await expectRevert(
+        instance.setAdminAddress(ZERO_ADDRESS, { from: owner }),
+        "INVALID_ADMIN"
       );
     });
 
-    it("Test setAdminAddress fail", async () => {
-      await expectRevert.unspecified(
-        instance.setAdminAddress(instanceAdmin, { from: randomUser })
+    it("Unpermissioned cannot set", async () => {
+      await expectRevert(
+        instance.setAdminAddress(instanceAdmin, { from: randomUser }),
+        "Ownable: caller is not the owner"
       );
     });
   });
