b607331325cdfc92ba398b1cb6e25447ad836e2a
Chan-Ho Suh <chanhosuh@gmail.com>
2022-03-15 11:37:05 -0400

    Add interface

---
+++ contracts/rewards/ITimeLocked.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BUSDL-1.1
+pragma solidity 0.6.11;
+pragma experimental ABIEncoderV2;
+
+interface ITimeLocked {
+    event LockerAdded(address);
+    event LockerRemoved(address);
+
+    function addLocker(address locker) external;
+
+    function removeLocker(address locker) external;
+
+    function setLockEnd(uint256 lockEnd) external;
+
+    function lockAmount(address account, uint256 amount) external;
+
+    function lockEnd() external view returns (uint256);
+
+    function unlockedAmount(address account) external view returns (uint256);
+}
