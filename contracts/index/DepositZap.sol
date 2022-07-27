475ba78a3cc132b0ec5f6dc773a6d9569f1d8ba4
Chan-Ho Suh <chanhosuh@gmail.com>
2022-07-27 10:57:40 -0400

    Add natspecs; clean up comments

--- contracts/index/DepositZap.sol
+++ contracts/index/DepositZap.sol
@@ -37,25 +37,42 @@ contract DepositZap {
         underlyers[2] = USDT;
     }
 
+    /**
+     * @notice deposit 3Pool underlyer amount for index shares
+     * @dev user must approve deposit zap for underlyer to transfer
+     * @param amount the amount of 3Pool underlyer
+     * @param index the index specifying 3Pool underlyer
+     */
     function deposit(uint256 amount, uint8 index) external {
+        require(index < 3, "INVALID_INDEX");
+
+        // user must have approved zap for 3Pool underlyer
         _addLiquidityOneCoin(amount, index, 0);
+
         uint256 lpAmount = IDetailedERC20(CURVE_3CRV).balanceOf(address(this));
+
+        // must approve index token so we can deposit 3CRV
         IDetailedERC20(CURVE_3CRV).safeApprove(indexToken, 0);
         IDetailedERC20(CURVE_3CRV).safeApprove(indexToken, lpAmount);
+
         IERC4626(indexToken).deposit(lpAmount, msg.sender);
     }
 
+    /**
+     * @notice redeem index shares for specified 3Pool underlyer
+     * @dev user must approve deposit zap for index token to burn
+     * @param shares the amount of index token shares
+     * @param index the index specifying 3Pool underlyer
+     */
     function redeem(uint256 shares, uint8 index) external {
         require(index < 3, "INVALID_INDEX");
 
-        // redeem index tokens for 3CRV; user must have approved the zap
+        // user must have approved zap for index tokens
         IERC4626(indexToken).redeem(shares, address(this), msg.sender);
 
-        // pull out of 3Pool into underlyer (redeem 3CRV)
         uint256 lpAmount = IDetailedERC20(CURVE_3CRV).balanceOf(address(this));
         ICurve3Pool(CURVE_3POOL).remove_liquidity_one_coin(lpAmount, index, 0);
 
-        // transfer underlyer balance to user
         address token = underlyers[index];
         uint256 tokenBalance = IDetailedERC20(token).balanceOf(address(this));
         IDetailedERC20(token).safeTransfer(msg.sender, tokenBalance);
