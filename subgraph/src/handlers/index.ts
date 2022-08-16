451ddd3c1beda83bf1c74f900f206930bfceab99
Chan-Ho Suh <chanhosuh@gmail.com>
2022-08-16 17:35:12 -0400

    Remove deprecated subgraph dir

--- subgraph/src/handlers/index.ts
+++
@@ -1,4 +0,0 @@
-export { handleDepositedAPT } from "./DepositedAPT";
-export { handleRedeemedAPT } from "./RedeemedAPT";
-export { handleTransfer } from "./Transfer";
-export { handleClaimed } from "./Claimed";
